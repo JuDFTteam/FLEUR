ee9e0dfe34b3e84b569e31ccf53b18bfbc89dea1
Gregor Michalicek <g.michalicek@fz-juelich.de>
2016-06-29 11:36:21 +0200

    Added some copyright notices to files in cdn subdirecory

--- cdn/m_perp.f90
+++ cdn/m_perp.f90
@@ -1,3 +1,9 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+
 MODULE m_m_perp
 CONTAINS 
   SUBROUTINE m_perp(atoms,itype,noco,vr0, chmom,qa21,alphdiff)
