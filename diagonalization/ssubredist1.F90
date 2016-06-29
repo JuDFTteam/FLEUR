0292b39eb22aebd8cc138c58e321c790da21dfb9
Gregor Michalicek <g.michalicek@fz-juelich.de>
2016-06-29 13:47:24 +0200

    Added copyright notices to all files in diagonalization subdirectory

--- diagonalization/ssubredist1.F90
+++ diagonalization/ssubredist1.F90
@@ -1,3 +1,9 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+
 !DEC$ FREEFORM
 SUBROUTINE subredist1(achi,n,asca,lda,SUB_COMM,nprow,npcol,iam,ierr,nb)
 use m_juDFT
--- diagonalization/ssubredist2.F90
+++ diagonalization/ssubredist2.F90
@@ -1,3 +1,9 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+
 !DEC$ FREEFORM
 subroutine subredist2(achi,n,m,asca,lda,SUB_COMM,nprow,npcol,&
                        iam,ierr,nb)
