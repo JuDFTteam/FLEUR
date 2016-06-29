20369cec732520d7e584475912f2f047ea93e08b
Gregor Michalicek <g.michalicek@fz-juelich.de>
2016-06-29 18:30:23 +0200

    Added copyright notices to all files in the wannier subdirectory

--- wannier/wann_dipole2.f
+++ wannier/wann_dipole2.f
@@ -1,3 +1,9 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+
 c********************************************************
 c     Calculate the electric polarization.
 c     Frank Freimuth, October 2006
