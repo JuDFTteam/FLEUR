cc954abd6c3a4a1b369af142650c2b4baf24ea2f
Daniel Wortmann <d.wortmann@fz-juelich.de>
2016-07-13 15:44:39 +0200

    Put argument handling into seperate module

---
+++ juDFT/args.F90
@@ -0,0 +1,37 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+MODULE m_juDFT_args
+!This subroutine allows to query for command line arguments
+!The following command line arguments are known to FLEUR
+! eig66_io.F90: -da, -mem, -mpi, -hdf                     
+!                        -->  choose the storage of eigenvalues&vectors
+! eigen_diag.F90: -lapack, -lapack2, -elpa, -magma, -scalapack
+!                        --> choose diagonalization sheme 
+! time.F90: -debugtime   --> write out the start/stop of all timers to STDOUT
+! fleur_init.F90,set_inp.F90: --xmlInput
+!                        --> choose xmlInput format
+
+PUBLIC
+CONTAINS
+  FUNCTION juDFT_was_argument(arg) RESULT(OK)
+    IMPLICIT NONE
+    CHARACTER(len=*),INTENT(IN)::arg
+    LOGICAL ok
+
+    INTEGER:: i
+    CHARACTER(LEN=30)::str
+    ok=.FALSE.
+    DO i=1,COMMAND_ARGUMENT_COUNT()
+       CALL GET_COMMAND_ARGUMENT(i,str)
+       IF(ADJUSTL(str)==ADJUSTL(arg)) ok=.TRUE.
+    ENDDO
+    IF (ok) RETURN
+    !Test for environment variable as well
+    CALL GET_ENVIRONMENT_VARIABLE("juDFT",str,status=i)
+    IF (i==0) ok=INDEX(str,ADJUSTL(arg))>0
+
+  END FUNCTION juDFT_was_argument
+END MODULE m_juDFT_args
