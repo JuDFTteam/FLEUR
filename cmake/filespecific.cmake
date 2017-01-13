ed7960b8ed843ee2ea061f26eefa83afa9e87349
Daniel Wortmann <d.wortmann@fz-juelich.de>
2017-01-13 12:16:56 +0100

    Added a file in cmake process to include file specific compiler options This is currently used to switch off optimization for some files

---
+++ cmake/filespecific.cmake
@@ -0,0 +1,10 @@
+#This file contains specific compiler flags for
+#individual files and compilers
+#E.G. it is used to switch of optimization for some files
+#The compiler flags are added at the end and hence can be used
+#to overwrite previous settings
+
+if (${CMAKE_Fortran_COMPILER_ID} MATCHES "Intel")
+   set_source_files_properties(io/eig66_mpi.F90 PROPERTIES COMPILE_FLAGS -O0)
+endif()
+
