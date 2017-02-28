09629253d8c43c17da1f9a4d001133651b4a4816
Daniel Wortmann <d.wortmann@fz-juelich.de>
2017-02-28 15:13:10 +0100

    Modifications to re-enable MAGMA interface

---
+++ cmake/tests/test_MAGMA.cmake
@@ -0,0 +1,10 @@
+#First check if we can compile with ELPA
+try_compile(FLEUR_USE_MAGMA ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/tests/test_MAGMA.f90
+LINK_LIBRARIES ${FLEUR_LIBRARIES}
+            )
+
+message("MAGMA Library found:${FLEUR_USE_MAGMA}")
+
+if (FLEUR_USE_MAGMA)
+   set(FLEUR_MPI_DEFINITIONS ${FLEUR_MPI_DEFINITIONS} "CPP_MAGMA")
+endif() 
