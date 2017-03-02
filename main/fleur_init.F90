8b972f9244a410d752cdf5a285a7e16623850e68
Daniel Wortmann <d.wortmann@fz-juelich.de>
2017-03-02 11:12:41 +0100

    Added a checks module to collect different routines to verify input

--- main/fleur_init.F90
+++ main/fleur_init.F90
@@ -27,6 +27,7 @@
           USE m_winpXML
           USE m_setupMPI
           USE m_cdn_io
+          USE m_checks
 #ifdef CPP_MPI
           USE m_mpi_bc_all,  ONLY : mpi_bc_all
 #endif
@@ -72,6 +73,8 @@
           CHARACTER(LEN=20)             :: filename
           REAL                          :: a1(3),a2(3),a3(3)
           REAL                          :: scale, dtild
+          
+          CALL check_command_line()
 #ifdef CPP_MPI
           INCLUDE 'mpif.h'
           INTEGER ierr(3)
