5053f7da6b92a9e1b4369dc5a7c38c511f17fae2
Daniel Wortmann <d.wortmann@fz-juelich.de>
2017-03-16 14:02:05 +0100

    Added script for buildbot

---
+++ cmake/buildbot.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#This skript is used by buildbot to set up the environment on different machines
+# is loads modules and sets environment variables by sourcing the corresponding 
+# files in the machines directory and then calls the configuration script with
+# the correct machine name
+
+# the environment variable BUILDSLAVE_MACHINE must be set correctly for this to work
+
+m=$BUILDSLAVE_MACHINE
+
+if [[ $m =~ "JURECA" ]]
+then
+    source cmake/machines/JURECA/intelsource.sh
+    configure.sh JURECA
+    exit
+fi
+
+if [[ $m =~ "JURECA-GPU" ]]
+then
+    source cmake/machines/JURECA/pgisource.sh
+    configure.sh JURECA
+    exit
+fi
+
+if [[ $m =~ "JURECA-GCC" ]]
+then
+    source cmake/machines/JURECA/gccsource.sh
+    configure.sh AUTO
+    exit
+fi
+
+configure.sh $m
+
+
