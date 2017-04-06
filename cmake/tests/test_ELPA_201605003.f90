9e6eb61620eb3833c9bcd6613da9ea8be3dc77e0
Daniel Wortmann <d.wortmann@fz-juelich.de>
2017-04-06 10:33:06 +0200

    Automatic determination of ELPA version

---
+++ cmake/tests/test_ELPA_201605003.f90
@@ -0,0 +1,7 @@
+      program test
+      use elpa1
+      integer:: ierr,mpi_subcom, myrowblacs, mycolblacs
+      integer:: mpi_comm_rows,mpi_comm_cols,m,nb,mycolssca,ok
+      real :: bsca(10,10)
+      ok=CHOLESKY_real (m,bsca,SIZE(bsca,1),nb,mycolssca,mpi_comm_rows,mpi_comm_cols,.false.)      
+      end
--- cmake/tests/test_ELPA_OLD.f90
+++ cmake/tests/test_ELPA_OLD.f90
@@ -2,4 +2,6 @@
       use elpa1
       integer:: ierr,mpi_subcom, myrowblacs, mycolblacs
       integer:: mpi_comm_rows,mpi_comm_cols
+      call get_elpa_row_col_comms(mpi_subcom, myrowblacs, mycolblacs,        &
+     &     mpi_comm_rows, mpi_comm_cols)
       end
