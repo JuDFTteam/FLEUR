68b460f1065b4728da7f52c96554bf868694eb77
Gregor Michalicek <g.michalicek@fz-juelich.de>
2017-08-18 11:54:33 +0200

    Add Wannier90 library availability test to configure script
    Note: The test does not yet search for such a library.

---
+++ cmake/tests/test_Wannier90.cmake
@@ -0,0 +1,12 @@
+#first try if Wannier90 already works
+try_compile(FLEUR_USE_WANN ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/tests/test_Wannier90.f90
+	    LINK_LIBRARIES ${FLEUR_LIBRARIES}
+            )
+
+message("Wannier90 1.2 Library found:${FLEUR_USE_WANN}")
+
+if (FLEUR_USE_WANN)
+   message("Wannier90 1.2 Library found:${FLEUR_USE_WANN}")
+   set(FLEUR_DEFINITIONS ${FLEUR_DEFINITIONS} "CPP_WANN") 
+   set(FLEUR_MPI_DEFINITIONS ${FLEUR_MPI_DEFINITIONS} "CPP_WANN")
+endif()
