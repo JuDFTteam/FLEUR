528946be3606951b5abe38ad89304d5a1aaf1e55
Gregor Michalicek <g.michalicek@fz-juelich.de>
2017-08-18 15:04:01 +0200

    Replaced CPP_WANN by CPP_WANN4 or CPP_WANN5 at several places
    As some parts of the Wannier code rely on JPHs modified Wannier90 libraries
    wannier4.a and wannier5.a I now differentiate between them and the official
    version. wannier4.a and wannier5.a are only needed for certain code parts.

--- cmake/tests/test_Wannier90.cmake
+++ cmake/tests/test_Wannier90.cmake
@@ -6,7 +6,6 @@ try_compile(FLEUR_USE_WANN ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/tests/t
 message("Wannier90 1.2 Library found:${FLEUR_USE_WANN}")
 
 if (FLEUR_USE_WANN)
-   message("Wannier90 1.2 Library found:${FLEUR_USE_WANN}")
    set(FLEUR_DEFINITIONS ${FLEUR_DEFINITIONS} "CPP_WANN") 
    set(FLEUR_MPI_DEFINITIONS ${FLEUR_MPI_DEFINITIONS} "CPP_WANN")
 endif()
