6daac5c70926cda443597e5290082891f5f5d61d
Uliana Alekseeva <u.alekseeva@fz-juelich.de>
2017-09-15 14:43:58 +0200

    small fixes in cmake files for GPU

--- cmake/machines/JURECA/JURECAGPU.cmake
+++ cmake/machines/JURECA/JURECAGPU.cmake
@@ -3,7 +3,7 @@ set(CMAKE_Fortran_COMPILER mpif90)
 set(CMAKE_C_COMPILER mpicc)
 #Add include pathes
 #set(CMAKE_C_FLAGS " -I$ENV{XML2_ROOT}/include")
-set(FLEUR_LIBRARIES "-lstdc++;-L$ENV{MKLROOT}/lib/intel64;-lmkl_scalapack_lp64;-lmkl_intel_lp64;-lmkl_pgi_thread;-lmkl_core;-lmkl_blacs_intelmpi_lp64"")
+set(FLEUR_LIBRARIES "-lstdc++;-L$ENV{MKLROOT}/lib/intel64;-lmkl_scalapack_lp64;-lmkl_intel_lp64;-lmkl_pgi_thread;-lmkl_core;-lmkl_blacs_intelmpi_lp64")
 if (ENV{MAGMA_ROOT})
 set(FLEUR_Fortran_FLAGS " -I$ENV{MAGMA_ROOT}/include")
 set(FLEUR_LIBRARIES "${FLEUR_LIBARIES};-L$ENV{MAGMA_ROOT}/lib;-lmagma")
