3b8deae3829cae8bbfeedafdc246ae052b6f2842
Daniel Wortmann <d.wortmann@fz-juelich.de>
2017-09-19 10:39:22 +0200

    Fix of CI

--- cmake/machines/GITLAB.cmake
+++ cmake/machines/GITLAB.cmake
@@ -4,4 +4,4 @@ set(CMAKE_Fortran_COMPILER mpif90)
 #Add include pathes
 #set(FLEUR_Fortran_FLAGS "")
 #Add linker stuff
-set(FLEUR_LIBRARIES ${FLEUR_LIBRARIES} "/usr/lib/x86_64-linux-gnu/libxml2.so;/usr/lib/x86_64-linux-gnu/libscalapack-openmpi.so;/usr/lib/x86_64-linux-gnu/libblacsF77init-openmpi.so;/usr/lib/x86_64-linux-gnu/libblacs-openmpi.so;/usr/lib/liblapack.so;/usr/lib/libblas.so")
+set(FLEUR_LIBRARIES ${FLEUR_LIBRARIES} "-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi;-llapack;-lblas")
