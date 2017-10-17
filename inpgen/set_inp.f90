aae4bf9285d0d67beed72a1e5ddebf41de9f9b11
Gregor Michalicek <g.michalicek@fz-juelich.de>
2017-10-17 17:23:32 +0200

    Add missing initialization of kpts%numSpecialPoints to inpgen/set_inp.f90
    This seems to be the Valgrind complaint for inpgen for the moment.

--- inpgen/set_inp.f90
+++ inpgen/set_inp.f90
@@ -162,6 +162,7 @@
       sliceplot%e1s = 0.0 ; sliceplot%e2s = 0.0 ; banddos%e1_dos = 0.5 ; banddos%e2_dos = -0.5 ; input%tkb = 0.001
       banddos%sig_dos = 0.015 ; vacuum%tworkf = 0.0 ; scale = 1.0 ; scpos = 1.0 
       zc = 0.0 ; vacuum%locx(:) = 0.0 ;  vacuum%locy(:) = 0.0
+      kpts%numSpecialPoints = 0
 
 !+odim
       oneD%odd%mb = 0 ; oneD%odd%M = 0 ; oneD%odd%m_cyl = 0 ; oneD%odd%chi = 0 ; oneD%odd%rot = 0
