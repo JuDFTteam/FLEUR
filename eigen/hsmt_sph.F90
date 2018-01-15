JuDFTteam/FLEUR

commit b065f991a342dcf579472ba392dbc888b4f0fc10

Temporarily comment out explicit SIMD instructions in eigen/hsmt_sph.F90
The OpenMP SIMD instructions made problems on ifort 12 and ifort 15. We
have to introduce a filter to exclude these compilers from respective
code sections.

The instructions were introduced in commit 97ef6a4d58e9a5ba6158c81648298f803e197a7e.

eigen/hsmt_sph.F90
@@ -255,7 +255,7 @@ CONTAINS
                 DO nn = n0,n1
                    tnn = tpi_const*atoms%taual(:,nn)
                    !--->             set up phase factors
-                   !$OMP SIMD PRIVATE(tmp1,tmp2,tmp3,tmp) 
+                   !!$OMP SIMD PRIVATE(tmp1,tmp2,tmp3,tmp) 
                    DO kj = 1,kjmax
                       !rph(kj,1) = rph(kj,1) +&
                       !     COS(DOT_PRODUCT(ski-(/lapw%k1(kj,jintsp),lapw%k2(kj,jintsp),lapw%k3(kj,jintsp)/)+qssbtj,tnn))
@@ -268,7 +268,7 @@ CONTAINS
                    IF (.NOT.sym%invs) THEN
                       !--->                if the system does not posses inversion symmetry
                       !--->                the complex part of the exponential is needed.
-                      !$OMP SIMD PRIVATE(tmp1,tmp2,tmp3,tmp) 
+                      !!$OMP SIMD PRIVATE(tmp1,tmp2,tmp3,tmp) 
                       DO kj = 1,kjmax
                       !   cph(kj,1) = cph(kj,1) +&
                       !        SIN(DOT_PRODUCT((/lapw%k1(kj,jintsp),lapw%k2(kj,jintsp),lapw%k3(kj,jintsp)/)+qssbtj-ski,tnn))

commit 9ce3d954666b36ac5b39d662377975230a3f9347

Make CI work again
The problem was due to several variables not being declared as private in
an OpenMP loop. The bug was introduced in commit b065f991a342dcf579472ba392dbc888b4f0fc10.
Sorry for that! ;)

eigen/hsmt_sph.F90
@@ -170,6 +170,7 @@ CONTAINS
     !$OMP PRIVATE(w1,apw_lo1,apw_lo2,ddnln,elall,fct,ij,apw1)&
     !$OMP PRIVATE(cross_k,dplegend,chi,chi11,chi21,chi22,nsp,chj)&
     !$OMP PRIVATE(isigma_x,isigma_y,isigma_z,j1,j2,chi11so,chi21so,chi22so)&
+    !$OMP PRIVATE(tmp1,tmp2,tmp3,tmp)&
     !$OMP PRIVATE(aawa,bbwa,capw1,ii) IF (.not.l_socfirst)
     !$     IF (l_socfirst) WRITE(*,*) "WARNING: first variation SOC does not work with OPENMP in hsmt_sph"
     !$     IF (l_socfirst) WRITE(*,*) "         switching off openmp parallelization"