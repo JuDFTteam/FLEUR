b0aec8598275092e1588b24aed23ec2f71a07eef
Daniel Wortmann <d.wortmann@fz-juelich.de>
2018-03-29 16:54:34 +0200

    Added file missing in 01b9d8b83e8ff6bd4b0bf7d7d79cf4834b9f9ea8

---
+++ mpi/mpi_dist_forcetheorem.F90
@@ -0,0 +1,44 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+
+MODULE m_mpi_dist_forcetheorem
+CONTAINS
+  SUBROUTINE mpi_dist_forcetheorem(mpi,forcetheo)
+    USE m_types
+    IMPLICIT NONE
+    TYPE(t_mpi),INTENT(in)::mpi
+    CLASS(t_forcetheo),ALLOCATABLE,INTENT(INOUT)::forcetheo
+
+    INTEGER::t,ierr
+#ifdef CPP_MPI
+    INCLUDE 'mpif.h'
+    
+    IF (mpi%irank==0) THEN
+       SELECT TYPE(forcetheo)
+       TYPE IS (t_forcetheo)
+          t=1
+       TYPE IS (t_forcetheo_mae)
+          t=2
+       END SELECT
+    ENDIF   
+    CALL MPI_BCAST(t,1,MPI_INTEGER,0,mpi%mpi_comm,ierr)
+    IF (mpi%irank.NE.0) THEN
+       SELECT CASE (t)
+       CASE (1)
+          ALLOCATE(t_forcetheo::forcetheo)
+       CASE(2)
+          ALLOCATE(t_forcetheo_mae::forcetheo)
+       END SELECT
+    END IF
+
+    !now we have the correct type, now we have to distribute the data
+    SELECT TYPE(forcetheo)
+    TYPE IS (t_forcetheo_mae)
+       CALL forcetheo%dist(mpi)
+    END SELECT
+#endif
+  END SUBROUTINE mpi_dist_forcetheorem
+END MODULE m_mpi_dist_forcetheorem
