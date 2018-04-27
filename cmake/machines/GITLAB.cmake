9ef34c81faff1a3c710d8d7290c263680241ce42
Daniel Wortmann <d.wortmann@fz-juelich.de>
2018-04-27 11:13:45 +0200

    Added link&compile options for locally installed libraries to CI script

--- cmake/machines/GITLAB.cmake
+++ cmake/machines/GITLAB.cmake
@@ -4,4 +4,4 @@ set(CMAKE_Fortran_COMPILER mpif90)
 #Add include pathes
 set(FLEUR_Fortran_FLAGS "-I/opt/include")
 #Add linker stuff
-set(FLEUR_LIBRARIES ${FLEUR_LIBRARIES} "-L/opt/lib;-lxcf03;-lxc;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi;-llapack;-lblas")
+set(FLEUR_LIBRARIES ${FLEUR_LIBRARIES} "-L/opt/lib;-lxcf03;-lxc;-lhdf5_fortran;-lhdf5;-ldl;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi;-llapack;-lblas")
