c98091706a0d14ef9a617c064f4ee59a5751b204
Daniel Wortmann <d.wortmann@fz-juelich.de>
2018-06-22 11:39:46 +0200

    Fixed external cmake dependency in configure.sh

--- external/cmake/install_cmake.sh
+++ external/cmake/install_cmake.sh
@@ -1,11 +1,12 @@
-version=3.11.4-`uname`-x86_64
+OS=`uname`
+version=3.11.4-${OS}-x86_64
 
 #Download if not present...
 if [ ! -r cmake-${version} ]
 then
     #Get the file with the code
     curl -LO "https://cmake.org/files/v3.11/cmake-${version}.tar.gz"
-    tar xzf cmake-${hdf5_version}.tar.gz
+    tar xzf cmake-${version}.tar.gz
 fi
 
 #add to path
