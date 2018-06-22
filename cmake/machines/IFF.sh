944dd1a20ce2308add16ea1c3bd547b4e9e5791d
Daniel Wortmann <d.wortmann@fz-juelich.de>
2018-06-22 16:16:00 +0200

    Added external dependency "chase" for configure.sh

--- cmake/machines/IFF.sh
+++ cmake/machines/IFF.sh
@@ -1,2 +1,6 @@
 export FC=${FC:=mpiifort}
+export CC=${CC:=mpiicc}
+export CXX=${CXX:=mpiicpc}
+
+
 FLEUR_LIBRARIES="-lxml2;-lmkl_scalapack_lp64;-lmkl_blacs_intelmpi_lp64;-mt_mpi;${FLEUR_LIBRARIES}"
