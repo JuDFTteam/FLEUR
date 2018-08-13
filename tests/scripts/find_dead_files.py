ad511e1dab773eb0cf22495483ad21b75273eb2d
Matthias Redies <m.redies@fz-juelich.de>
2018-08-13 15:38:13 +0200

    now it know about comments

--- tests/scripts/find_dead_files.py
+++ tests/scripts/find_dead_files.py
@@ -8,12 +8,20 @@ files.extend(glob("**/*.F90"))
 
 cmake_files = glob("**/CMakeLists.txt")
 
+def rm_comments(line):
+  comm_pos = line.find("#")
+  if comm_pos == -1:
+    return line
+  else:
+    return line[:comm_pos]
+
+
 def in_cmake(cmake_files, file):
   for cmf in cmake_files:
     with open(cmf) as f:
       lines = f.readlines()
       for l in lines:
-        if file in l:
+        if file in rm_comments(l):
           return True
   return False
 
