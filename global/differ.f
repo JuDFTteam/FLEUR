60c6df1f00d2464b7f53f63627c1bc4ea7aa92f3
Gregor Michalicek <g.michalicek@fz-juelich.de>
2018-09-10 18:27:04 +0200

    Increase precision of differ

--- global/differ.f
+++ global/differ.f
@@ -42,7 +42,7 @@ C     ..
       ierr = 0
       nqnt = fn - fl - 0.99e0
       n = msh
-      del = 5.e-5
+      del = 5.e-7
       emax = 2
       emin = -z*z/fn**2 - 10.0
       IF ((e.GE.emax) .OR. (e.LE.emin)) e = 0.5e0* (emax+emin)
