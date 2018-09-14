ee95a1bb08d28e59fa5543b319c4c033773c051b
Uliana Alekseeva <u.aleskeeva@fz-juelich.de>
2018-09-14 09:47:14 +0200

    PGI compiling added for CLAIX

--- cmake/machines/CLAIX.sh
+++ cmake/machines/CLAIX.sh
@@ -1,12 +1,12 @@
 #Configuration for CLAIX@RWTH; do a 'module load intelmpi' before
 
+  if  module list 2>&1 | grep -q intel
+  then
     if ! module list 2>&1| grep -q intelmpi
     then
       echo "Please use intelmpi, e.g. do a module switch openmpi intelmpi"
       exit	
     fi
-
-
     export FC=$MPIFC
     export CC=$MPICC
     #ELPA
@@ -16,5 +16,7 @@
       FLEUR_LIBDIR="$FLEUR_LIBDIR $ELPA_LIB"
       FLEUR_INCLUDEDIR="$FLEUR_INCLUDEDIR $ELPA_MODULES"
     fi
- 
-    
+  elif  module list 2>&1 | grep -q pgi
+  then
+    echo "found PGI compiler"
+  fi 
