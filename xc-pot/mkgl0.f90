e628c30485642f68052b7a4184ef61bba2f79c4a
Matthias Redies <m.redies@fz-juelich.de>
2019-01-04 15:03:48 +0100

    fix indentation

--- xc-pot/mkgl0.f90
+++ xc-pot/mkgl0.f90
@@ -1,4 +1,4 @@
-      MODULE m_mkgl0
+MODULE m_mkgl0
 !      ------------------------------------------------------------------
 !      make quantities for vxcallg. for paramag. case
 !        dens,drr,ddrr are charge density and its gradient for nonmag.
@@ -8,8 +8,8 @@
 !      gggr: grad(ro)*grad(agr),
 !      grgru,d: grad(ro)*grad(rou),for rod., gzgr: grad(zeta)*grad(ro).
 !      ------------------------------------------------------------------
-      CONTAINS
-      SUBROUTINE mkgl0(mshd,msh,jspd,jspins,rad, densi,drri,ddrri, grad)
+CONTAINS
+   SUBROUTINE mkgl0(mshd,msh,jspd,jspins,rad, densi,drri,ddrri, grad)
 
       USE m_types
       IMPLICIT NONE
@@ -18,11 +18,11 @@
       REAL,    INTENT (IN) :: rad(mshd),densi(mshd,jspd)
       REAL,    INTENT (IN) :: drri(mshd,jspd),ddrri(mshd,jspd)
       TYPE(t_gradients)::grad
-      
+
       REAL dagrr,dagrru,ddrr,ddrrd,ddrru,drr,drrd,drru,dzdr,ro,rod,rou,rv,spnf
       INTEGER i
 !     ------------------------------------------------------------------
-      
+
       spnf = 1./(3-jspins)
       IF (allocated(grad%sigma)) THEN
          DO i=1,msh
@@ -36,41 +36,41 @@
       ENDIF
       DO i = 1,msh
 
-          rv = rad(i)
-          ro =  spnf * (densi(i,1) + densi(i,jspins)) 
-          rou = spnf * densi(i,1)
-          rod = spnf * densi(i,jspins) 
+         rv = rad(i)
+         ro =  spnf * (densi(i,1) + densi(i,jspins))
+         rou = spnf * densi(i,1)
+         rod = spnf * densi(i,jspins)
 
-          drr =   spnf * (drri(i,1) + drri(i,jspins))
-          drru =  spnf * drri(i,1)
-          drrd =  spnf * drri(i,jspins)
-          ddrr =  spnf * (ddrri(i,1) + ddrri(i,jspins))
-          ddrru = spnf * ddrri(i,1)
-          ddrrd = spnf * ddrri(i,jspins)
+         drr =   spnf * (drri(i,1) + drri(i,jspins))
+         drru =  spnf * drri(i,1)
+         drrd =  spnf * drri(i,jspins)
+         ddrr =  spnf * (ddrri(i,1) + ddrri(i,jspins))
+         ddrru = spnf * ddrri(i,1)
+         ddrrd = spnf * ddrri(i,jspins)
 
-          grad%agrt(i) = abs(drr)
-          grad%agru(i) = abs(drru)
-          grad%agrd(i) = grad%agru(i)
+         grad%agrt(i) = abs(drr)
+         grad%agru(i) = abs(drru)
+         grad%agrd(i) = grad%agru(i)
 
-          dagrr = drr*ddrr/grad%agrt(i)
-          dagrru = drru*ddrru/grad%agru(i)
- 
-          grad%gggrt(i) = drr*dagrr
-          grad%gggru(i) = drru*dagrru
-          grad%gggrd(i) = grad%gggru(i)
+         dagrr = drr*ddrr/grad%agrt(i)
+         dagrru = drru*ddrru/grad%agru(i)
 
-          dzdr = ((drru-drrd)*ro- (rou-rod)*drr)/ro**2
+         grad%gggrt(i) = drr*dagrr
+         grad%gggru(i) = drru*dagrru
+         grad%gggrd(i) = grad%gggru(i)
 
-          grad%gzgr(i) = dzdr*drr
+         dzdr = ((drru-drrd)*ro- (rou-rod)*drr)/ro**2
 
-          grad%g2rt(i) = ddrr + 2*drr/rv
-          grad%g2ru(i) = ddrru + 2*drru/rv
-          grad%g2rd(i) = ddrrd + 2*drrd/rv
+         grad%gzgr(i) = dzdr*drr
 
-          grad%grgru(i) = drr*drru
-          grad%grgrd(i) = drr*drrd
+         grad%g2rt(i) = ddrr + 2*drr/rv
+         grad%g2ru(i) = ddrru + 2*drru/rv
+         grad%g2rd(i) = ddrrd + 2*drrd/rv
+
+         grad%grgru(i) = drr*drru
+         grad%grgrd(i) = drr*drrd
 
       ENDDO
 
-      END SUBROUTINE mkgl0
-      END MODULE m_mkgl0
+   END SUBROUTINE mkgl0
+END MODULE m_mkgl0
