e50528400d68446ad3de6b8628c17aea414db841
janssen <he.janssen@fz-juelich.de>
2019-02-22 09:20:43 +0100

    pre merge

--- ldahia/onsite21.f90
+++ ldahia/onsite21.f90
@@ -5,28 +5,26 @@ MODULE m_onsite21
 
    CONTAINS
 
-   SUBROUTINE onsite21(atoms,sym,ispin,jspins,noccbd,tetweights,wtkpt,eig,usdus,eigVecCoeffs,gOnsite)
+   SUBROUTINE onsite21(atoms,sym,jspins,noccbd,tetweights,wtkpt,eig,usdus,denCoeffsOffDiag,eigVecCoeffs,gOnsite)
 
       USE m_types
       USE m_differentiate
 
       IMPLICIT NONE
 
-      TYPE(t_atoms),          INTENT(IN)     :: atoms
-      TYPE(t_sym),            INTENT(IN)     :: sym
-      TYPE(t_eigVecCoeffs),   INTENT(IN)     :: eigVecCoeffs
-      TYPE(t_usdus),          INTENT(IN)     :: usdus
-      TYPE(t_greensf),        INTENT(INOUT)  :: gOnsite
+      TYPE(t_atoms),             INTENT(IN)     :: atoms
+      TYPE(t_sym),               INTENT(IN)     :: sym
+      TYPE(t_eigVecCoeffs),      INTENT(IN)     :: eigVecCoeffs
+      TYPE(t_denCoeffsOffDiag),  INTENT(IN)     :: denCoeffsOffDiag
+      TYPE(t_usdus),             INTENT(IN)     :: usdus
+      TYPE(t_greensf),           INTENT(INOUT)  :: gOnsite
 
+      INTEGER,                   INTENT(IN)     :: jspins
+      INTEGER,                   INTENT(IN)     :: noccbd 
 
-
-      INTEGER,                INTENT(IN)     :: ispin
-      INTEGER,                INTENT(IN)     :: jspins
-      INTEGER,                INTENT(IN)     :: noccbd 
-
-      REAL,                   INTENT(IN)     :: wtkpt
-      REAL,                   INTENT(IN)     :: tetweights(:,:)
-      REAL,                   INTENT(IN)     :: eig(noccbd)
+      REAL,                      INTENT(IN)     :: wtkpt
+      REAL,                      INTENT(IN)     :: tetweights(:,:)
+      REAL,                      INTENT(IN)     :: eig(noccbd)
 
 
 
