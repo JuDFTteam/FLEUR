f91a74b5f072be189494f6d2e07addf9092d0509
Gregor Michalicek <g.michalicek@fz-juelich.de>
2019-03-07 15:44:07 +0100

    Revert "remove unused A array"
    This reverts commit 289e15a4d893e1bf1c75c2c715e23fe9da2586ca

--- juDFT/stop.F90
+++ juDFT/stop.F90
@@ -234,6 +234,7 @@ CONTAINS
     INTEGER, OPTIONAL, INTENT(IN)  :: errorCode
     INTEGER :: error
     LOGICAL :: calltrace
+    LOGICAL,ALLOCATABLE::a(:)
 #ifdef CPP_MPI
     INTEGER :: ierr
 #endif
@@ -252,6 +253,7 @@ CONTAINS
 #elif (defined(CPP_AIX)&&!defined(__PGI))
        CALL xl__trbk()
 #endif
+       DEALLOCATE(a)!will generate an error that can be found by the compiler
     ENDIF
 
 #if defined(CPP_MPI)
