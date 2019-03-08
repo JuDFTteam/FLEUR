bc03d4708f5bde26cb76d964bcda35dfac9cac26
Matthias Redies <m.redies@fz-juelich.de>
2019-03-08 10:10:51 +0100

    added int2str and str2int

--- juDFT/string.f90
+++ juDFT/string.f90
@@ -21,4 +21,27 @@ contains
 
       output = input(front:back)
    end function strip
+
+   function str2int(str) result(int)
+      implicit none
+      character(len=*), intent(in) :: str
+      integer                      :: int
+      integer                      :: stat
+
+      read (str, *, iostat=stat) int
+      if (stat /= 0) then
+         write (*, *) "str reading failed", str
+         stop 9
+      endif
+   end function str2int
+
+   function int2str(num) result(ret_str)
+      implicit none
+      integer, intent(in)            :: num
+      character(len=:), allocatable  :: ret_str
+
+      allocate(character(100) :: ret_str)
+      write (ret_str,*) num
+      ret_str = strip(ret_str)
+   end function int2str
 end module m_juDFT_string
