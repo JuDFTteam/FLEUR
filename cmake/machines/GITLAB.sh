d8f4bdb769927ac3d2fe41857990801fbf9a10f3
Daniel Wortmann <d.wortmann@fz-juelich.de>
2019-03-28 12:13:48 +0100

    Fixes to compile within gitlab

--- cmake/machines/GITLAB.sh
+++ cmake/machines/GITLAB.sh
@@ -1,4 +1,4 @@
 #NOSHOW
 export FC=mpif90
 FLEUR_INCLUDEDIR="/opt/include"
-FLEUR_LIBRARIES="-L/opt/lib;-lxcf03;-lxc;-lhdf5_fortran;-lhdf5;-ldl;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi;-llapack;-lblas"
+FLEUR_LIBRARIES="-L/opt/lib;-lxcf03;-lxc;-ldl;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi;-llapack;-lblas"
