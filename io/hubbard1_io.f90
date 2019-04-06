bea774d4493af7e896fe79eb5de9dbd111a34d10
janssen <he.janssen@fz-juelich.de>
2019-04-06 10:00:31 +0200

    Added writing out of energy grid to hubbard 1 solver

--- io/hubbard1_io.f90
+++ io/hubbard1_io.f90
@@ -18,12 +18,13 @@ MODULE m_hubbard1_io
 
    USE m_juDFT
    USE m_types
+   USE m_constants
 
    IMPLICIT NONE
 
    CONTAINS
 
-   SUBROUTINE write_hubbard1_input(path,l,f0,f2,f4,f6,xi,bz,n_min,n_max,beta,mu)
+   SUBROUTINE write_hubbard1_input(path,l,f0,f2,f4,f6,xi,bz,n_min,n_max,beta,mu,ne,e_min,e_max,sigma)
 
       CHARACTER(len=*), INTENT(IN)  :: path
       INTEGER,          INTENT(IN)  :: l
@@ -33,7 +34,11 @@ MODULE m_hubbard1_io
       INTEGER,          INTENT(IN)  :: n_min,n_max
       REAL,             INTENT(IN)  :: beta
       REAL,             INTENT(IN)  :: mu 
-
+      INTEGER,          INTENT(IN)  :: ne
+      REAL,             INTENT(IN)  :: e_min
+      REAL,             INTENT(IN)  :: e_max
+      REAL,             INTENT(IN)  :: sigma 
+      
       CHARACTER(len=300) :: fname
       INTEGER :: info, io_error,io_unit
 
@@ -103,10 +108,10 @@ MODULE m_hubbard1_io
                            "#  Parameters for the frequency/energy axis"                ,&                   
                            "#**********************************************************"
       WRITE(io_unit,*) "real_freq_axis {"
-      WRITE(io_unit,*) "omegamin", -20.0
-      WRITE(io_unit,*) "omegamax", 20.0
-      WRITE(io_unit,*) "Nomega",    2000
-      WRITE(io_unit,*) "eps",      1.0e-2
+      WRITE(io_unit,*) "omegamin", e_min*hartree_to_ev_const
+      WRITE(io_unit,*) "omegamax", e_max*hartree_to_ev_const
+      WRITE(io_unit,*) "Nomega",   ne
+      WRITE(io_unit,*) "eps",      sigma
       WRITE(io_unit,*) "}"
 
       WRITE(io_unit,*) "matsub_freq_axis {"
@@ -153,38 +158,33 @@ MODULE m_hubbard1_io
 9020  FORMAT("Energy:"2f14.8)
    END SUBROUTINE write_onsite_gf
 
-   SUBROUTINE read_selfen(path,selfen_re,selfen_im,ne,l,e)
+   SUBROUTINE read_selfen(path,selfen,ne,matsize,e)
       
+      USE m_constants
       !This Subroutine reads in the self-energy
       !produced by the hubbard 1 solver
 
-      REAL, ALLOCATABLE,   INTENT(OUT) :: selfen_re(:,:,:)
-      REAL, ALLOCATABLE,   INTENT(OUT) :: selfen_im(:,:,:)
-      CHARACTER(len=*),    INTENT(IN)  :: path
-      INTEGER,             INTENT(IN)  :: ne
-      INTEGER,             INTENT(IN)  :: l
-      REAL, ALLOCATABLE,   INTENT(OUT) :: e(:)    
+      COMPLEX,          INTENT(OUT) :: selfen(:,:,:)
+      CHARACTER(len=*), INTENT(IN)  :: path
+      INTEGER,          INTENT(IN)  :: ne
+      INTEGER,          INTENT(IN)  :: matsize
+      REAL,             INTENT(OUT) :: e(:)    
       
       INTEGER io_error,io_unit
       INTEGER n,m,i
-      INTEGER matsize
+      REAL tmp(matsize,matsize)
       io_unit = 17
       !Open the selfenergy file
-      OPEN(unit=io_unit, file=TRIM(ADJUSTL(path)) // "se.atom", status="replace", action="read", iostat=io_error)
+      OPEN(unit=io_unit, file=TRIM(ADJUSTL(path)) // "se.atom", status="old", action="read", iostat=io_error)
 
       IF(io_error.NE.0) CALL juDFT_error("IO-Error in reading the self-energy", calledby="read_selfen")
-          
-      matsize = 2*(2*l+1)
-      ALLOCATE(selfen_re(ne,matsize,matsize))
-      ALLOCATE(selfen_im(ne,matsize,matsize))
-      ALLOCATE(e(ne))
       
       DO i = 1, ne
-
          READ(io_unit,9010) e(i)
-         READ(io_unit,9020) ((selfen_re(i,m,n), m= 1, matsize), n= 1, matsize)
-         READ(io_unit,9020) ((selfen_im(i,m,n), m= 1, matsize), n= 1, matsize)
-
+         READ(io_unit,9020) ((tmp(m,n), m= 1, matsize), n= 1, matsize)
+         selfen(i,1:matsize,1:matsize) = tmp(1:matsize,1:matsize)/hartree_to_ev_const
+         READ(io_unit,9020) ((tmp(m,n), m= 1, matsize), n= 1, matsize)
+         selfen(i,1:matsize,1:matsize) = selfen(i,1:matsize,1:matsize) + ImagUnit * tmp(1:matsize,1:matsize)/hartree_to_ev_const
       ENDDO
 
 9010  FORMAT(f10.5)
