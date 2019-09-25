25868dd7729008b9474fa21b9e13426f18250dba
Alexander Neukirchen <a.neukirchen@fz-juelich.de>
2019-09-25 12:04:30 +0200

    fix for last commit; derivatives are now a tad more general

--- vgen/pw_tofrom_grid.F90
+++ vgen/pw_tofrom_grid.F90
@@ -73,7 +73,7 @@ CONTAINS
     USE m_fft3dxc
     USE m_fft3d
     USE m_types
-    use m_constants
+    USE m_constants
     IMPLICIT NONE
     LOGICAL,INTENT(IN)            :: dograds
     INTEGER,INTENT(IN)            :: jspins
@@ -215,8 +215,21 @@ CONTAINS
              ENDDO !jdm
           ENDDO   !idm 
        END IF
-       CALL alloc_gradients(ifftxc3,jspins,grad)
- 
+
+       !!!!!!!THIS IS A QUICKFIX! TO BE REMOVED ASAP!!!!!!!!!
+       !A. Neukirchen 25.09.19
+       IF (allocated(grad%agrt)) THEN
+          DEALLOCATE(grad%agrt,grad%agru,grad%agrd)
+          DEALLOCATE(grad%g2ru,grad%g2rd,grad%gggrt)
+          DEALLOCATE(grad%gggru,grad%gzgr,grad%g2rt)
+          DEALLOCATE(grad%gggrd,grad%grgru,grad%grgrd)
+       ENDIF
+
+       ALLOCATE(grad%agrt(ifftxc3),grad%agru(ifftxc3),grad%agrd(ifftxc3))
+       ALLOCATE(grad%g2ru(ifftxc3),grad%g2rd(ifftxc3),grad%gggrt(ifftxc3))
+       ALLOCATE(grad%gggru(ifftxc3),grad%gzgr(ifftxc3),grad%g2rt(ifftxc3))
+       ALLOCATE(grad%gggrd(ifftxc3),grad%grgru(ifftxc3),grad%grgrd(ifftxc3))
+       !!!!!!!!!
        !
        !     calculate the quantities such as abs(grad(rho)),.. used in
        !     evaluating the gradient contributions to potential and energy.
