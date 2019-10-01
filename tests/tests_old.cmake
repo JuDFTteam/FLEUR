d275ba302ea199b9b72198dfb9ae6e1cd718e456
Daniel Wortmann <d.wortmann@fz-juelich.de>
2019-10-01 14:57:01 +0200

    Update of some tests, removal of many others

--- tests/tests_old.cmake
+++ tests/tests_old.cmake
@@ -1,11 +1,12 @@
 enable_testing()
 
-set(SerialParallelTests CuBulk CuBulkXML SiLOXML Fe_1l Fe_1lXML Fe-Atom CuBand CuBandXML CuDOS CuDOSXML
-Fe_bct Fe_bctXML PTO PTOXML Fe_1l_SOCXML PTO-SOC PTO-SOCXML Fe_bct_SOC Fe_bct_SOCXML Fe_fccXML
-GaAsMultiUForceXML SiFilmPlotXML SiFilmSlicePlotXML CoMCDXML Fe_Kerker Fe_bct_LOXML )#SiHybridGamma)
+#set(SerialParallelTests CuBulk CuBulkXML SiLOXML Fe_1l Fe_1lXML Fe-Atom CuBand CuBandXML CuDOS CuDOSXML
+#Fe_bct Fe_bctXML PTO PTOXML Fe_1l_SOCXML PTO-SOC PTO-SOCXML Fe_bct_SOC Fe_bct_SOCXML Fe_fccXML
+#GaAsMultiUForceXML SiFilmPlotXML SiFilmSlicePlotXML CoMCDXML Fe_Kerker Fe_bct_LOXML )#SiHybridGamma)
 
-set(SerialOnlyTests Fe_bct_LO Fe_fcc)# TiO2eels TiO2eelsXML)
-set(InpgenTests Si_plain Si_plain_explicit Si_full_para)# Si_kpt Si_kden Si_round_trip) 
+set(SerialParallelTests CuBulkXML SiLOXML Fe_1lXML CuBandXML CuDOSXML Fe_bctXML PTOXML Fe_1l_SOCXML PTO-SOCXML Fe_bct_SOCXML Fe_fccXML
+GaAsMultiUForceXML Fe_bct_LOXML )#SiHybridGamma)
+set(InpgenTests Si_plain Si_full_para)# Si_kpt Si_kden Si_round_trip)
 
 set(Testdirs ${SerialParallelTests} ${SerialOnlyTests})
 set(ParTestdirs ${SerialParallelTests})
@@ -20,7 +21,7 @@ endif()
 #The inpgen tests
 #if (${INPGEN})
 foreach(test ${InpTestdirs})
- add_test("INPGEN:${test}" ${CMAKE_CURRENT_SOURCE_DIR}/tests/test.pl "inpgen/${test}" "${CMAKE_BINARY_DIR}/inpgen")
+ add_test("INPGEN:${test}" ${CMAKE_CURRENT_SOURCE_DIR}/tests/test.pl "inpgen/${test}" "${CMAKE_BINARY_DIR}/inpgen2/inpgen2 -f inp_simple")
 endforeach(test)
 #endif()
 
@@ -54,4 +55,3 @@ if (${FLEUR_USE_MPI})
 ${test} "${CMAKE_BINARY_DIR}/fleur_MPI" "${mpi_exec}")
    endforeach(test)
 endif()
-
