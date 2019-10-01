46a99c957ac47d3475f9e382b98b56401502b7eb
Matthias Redies <m.redies@fz-juelich.de>
2019-10-01 13:05:19 +0200

    add helper functions to kpoint type

--- types/types_kpts.f90
+++ types/types_kpts.f90
@@ -34,6 +34,42 @@ MODULE m_types_kpts
       INTEGER, ALLOCATABLE           :: ntetra(:, :)
       REAL, ALLOCATABLE           :: voltet(:)
       REAL, ALLOCATABLE           :: sc_list(:, :) !list for all information about folding of bandstructure (need for unfoldBandKPTS)((k(x,y,z),K(x,y,z),m(g1,g2,g3)),(nkpt),k_original(x,y,z))
+   CONTAINS
+      procedure :: get_nk => kpts_get_nk
+      procedure :: to_first_bz => kpts_to_first_bz
+      procedure :: is_kpt => kpts_is_kpt
    ENDTYPE t_kpts
+contains
+   function kpts_get_nk(kpts, kpoint) result(idx)
+      ! get the index of a kpoint
+      implicit NONE
+      class(t_kpts), intent(in)    :: kpts
+      real, intent(in)            :: kpoint(3)
+      integer                     :: idx
 
+      DO idx = 1, kpts%nkptf
+         IF (all(abs(kpoint - kpts%bkf(:,idx)) < 1E-06)) THEN
+            return
+         END IF
+      END DO
+      idx = 0
+   end function kpts_get_nk
+
+   function kpts_to_first_bz(kpts, kpoint) result(out_point)
+      implicit NONE
+      class(t_kpts), intent(in)  :: kpts
+      real, intent(in)           :: kpoint(3)
+      real                       :: out_point(3)
+
+      out_point = kpoint - floor(kpoint)
+   end function kpts_to_first_bz
+
+   function kpts_is_kpt(kpts, kpoint) result(is_kpt)
+      implicit none
+      class(t_kpts), intent(in)  :: kpts
+      real, intent(in)           :: kpoint(3)
+      logical                    :: is_kpt
+
+      is_kpt = kpts%get_nk(kpoint) > 0
+   end function kpts_is_kpt
 END MODULE m_types_kpts
