87c5da16ddc112075d98fa78e72f60b2454c001e
Matthias Redies <m.redies@fz-juelich.de>
2019-10-02 11:39:38 +0200

    replace modulo1

--- hybrid/wavefproducts.F90
+++ hybrid/wavefproducts.F90
@@ -100,10 +100,10 @@ CONTAINS
 
       kqpthlp = kpts%bkf(:, nk) + kpts%bkf(:, iq)
       ! kqpt can lie outside the first BZ, transfer it back
-      kqpt = modulo1(kqpthlp, kpts%nkpt3)
+      kqpt = ktps%to_first_bz(kqpthlp)
       g_t(:) = nint(kqpt - kqpthlp)
-      ! determine number of kqpt
 
+      ! determine number of kqpt
       nkqpt = kpts%get_nk(kqpt)
       IF (.not. kpts%is_kpt(kqpt)) call juDFT_error('wavefproducts_inv5: k-point not found')
 
