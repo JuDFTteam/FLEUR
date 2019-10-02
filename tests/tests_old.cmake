89b239a54208df69fd4618df25fb0b9b4272e249
Matthias Redies <m.redies@fz-juelich.de>
2019-10-02 11:13:03 +0200

    my tests first

--- tests/tests_old.cmake
+++ tests/tests_old.cmake
@@ -1,10 +1,10 @@
 enable_testing()
 
-set(SerialParallelTests CuBulk CuBulkXML SiLOXML Fe_1l Fe_1lXML Fe-Atom CuBand
+set(SerialParallelTests SiHybridGamma SiHybridGammaNoInv SiHybrid8kpt_sym
+   SiHybrid8kpt_nosym CuBulk CuBulkXML SiLOXML Fe_1l Fe_1lXML Fe-Atom CuBand
    CuBandXML CuDOS CuDOSXML Fe_bct Fe_bctXML PTO PTOXML Fe_1l_SOCXML PTO-SOC
    PTO-SOCXML Fe_bct_SOC Fe_bct_SOCXML Fe_fccXML GaAsMultiUForceXML
-   SiFilmPlotXML SiFilmSlicePlotXML CoMCDXML Fe_Kerker Fe_bct_LOXML
-   SiHybridGamma SiHybridGammaNoInv SiHybrid8kpt_sym SiHybrid8kpt_nosym)
+   SiFilmPlotXML SiFilmSlicePlotXML CoMCDXML Fe_Kerker Fe_bct_LOXML   )
 
 set(SerialOnlyTests Fe_bct_LO Fe_fcc)# TiO2eels TiO2eelsXML)
 set(InpgenTests Si_plain Si_plain_explicit Si_full_para)# Si_kpt Si_kden Si_round_trip)
