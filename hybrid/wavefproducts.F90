adbb77c671f161ef6b2ff3036b0823b454667890
Matthias Redies <m.redies@fz-juelich.de>
2019-10-08 11:18:06 +0200

    delete old file

--- hybrid/wavefproducts.F90
+++
@@ -1,9 +0,0 @@
-!--------------------------------------------------------------------------------
-! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
-! This file is part of FLEUR and available as free software under the conditions
-! of the MIT license as expressed in the LICENSE file in more detail.
-!--------------------------------------------------------------------------------
-MODULE m_wavefproducts
-   USE m_wavefproducts_noinv
-   USE m_wavefproducts_inv
-END MODULE m_wavefproducts
