2c38b6a33540709b9f25f4374e5d16ff798a2ae2
Daniel Wortmann <d.wortmann@fz-juelich.de>
2019-10-18 15:00:17 +0200

    Updated fleurinput_read_xml to make arguments optional

--- fleurinput/fleurinput_read_xml.f90
+++ fleurinput/fleurinput_read_xml.f90
@@ -6,48 +6,48 @@ CONTAINS
        sliceplot,banddos,hybrid,oneD,coreSpecInput,wann,&
        xcpot,forcetheo_data,kpts,enparaXML)
     USE m_types_xml
-    
-    TYPE(t_cell),INTENT(OUT)::cell
-    TYPE(t_sym),INTENT(OUT)::sym
-    TYPE(t_atoms),INTENT(OUT)::atoms
-    TYPE(t_input),INTENT(OUT)::input 
-    TYPE(t_noco),INTENT(OUT)::noco
-    TYPE(t_vacuum),INTENT(OUT)::vacuum
-    TYPE(t_field),INTENT(OUT)::field
-    TYPE(t_sliceplot),INTENT(OUT)::sliceplot
-    TYPE(t_banddos),INTENT(OUT)::banddos
-    TYPE(t_hybrid),INTENT(OUT)::hybrid
-    TYPE(t_oneD),INTENT(OUT)::oneD
-    TYPE(t_coreSpecInput),INTENT(OUT)::coreSpecInput
-    TYPE(t_wann),INTENT(OUT)::wann
-    CLASS(t_xcpot),INTENT(OUT)::xcpot
-    TYPE(t_forcetheo_data),INTENT(OUT)::forcetheo_data
-    TYPE(t_enparaXML),INTENT(OUT)::enparaXML
-    TYPE(t_kpts),INTENT(OUT)::kpts
-    
+
+    TYPE(t_cell),INTENT(OUT),OPTIONAL::cell
+    TYPE(t_sym),INTENT(OUT),OPTIONAL::sym
+    TYPE(t_atoms),INTENT(OUT),OPTIONAL::atoms
+    TYPE(t_input),INTENT(OUT),OPTIONAL::input
+    TYPE(t_noco),INTENT(OUT),OPTIONAL::noco
+    TYPE(t_vacuum),INTENT(OUT),OPTIONAL::vacuum
+    TYPE(t_field),INTENT(OUT),OPTIONAL::field
+    TYPE(t_sliceplot),INTENT(OUT),OPTIONAL::sliceplot
+    TYPE(t_banddos),INTENT(OUT),OPTIONAL::banddos
+    TYPE(t_hybrid),INTENT(OUT),OPTIONAL::hybrid
+    TYPE(t_oneD),INTENT(OUT),OPTIONAL::oneD
+    TYPE(t_coreSpecInput),INTENT(OUT),OPTIONAL::coreSpecInput
+    TYPE(t_wann),INTENT(OUT),OPTIONAL::wann
+    CLASS(t_xcpot),INTENT(OUT),OPTIONAL::xcpot
+    TYPE(t_forcetheo_data),INTENT(OUT),OPTIONAL::forcetheo_data
+    TYPE(t_enparaXML),INTENT(OUT),OPTIONAL::enparaXML
+    TYPE(t_kpts),INTENT(OUT),OPTIONAL::kpts
+
     TYPE(t_xml)::xml
 
     !Call to init of xml type initialized XML reading and connects to inp.xml
     call xml%init()
-    
+
     !Now read from inp.xml for all datatypes
-    CALL cell%read_xml(xml)
-    CALL sym%read_xml(xml)
-    CALL atoms%read_xml(xml)
-    CALL input%read_xml(xml)
-    CALL noco%read_xml(xml)
-    CALL vacuum%read_xml(xml)
-    CALL field%read_xml(xml)
-    CALL sliceplot%read_xml(xml)
-    CALL banddos%read_xml(xml)
-    CALL hybrid%read_xml(xml)
-    CALL oneD%read_xml(xml)
-    CALL coreSpecInput%read_xml(xml)
-    CALL wann%read_xml(xml)
-    CALL xcpot%read_xml(xml)
-    CALL forcetheo_data%read_xml(xml)
-    CALL enparaXML%read_xml(xml)
-    CALL kpts%read_xml(xml)
+    if (present(cell)) call cell%read_xml(xml)
+    if (present(sym)) call sym%read_xml(xml)
+    if (present(atoms)) call atoms%read_xml(xml)
+    if (present(input)) call input%read_xml(xml)
+    if (present(noco)) call noco%read_xml(xml)
+    if (present(vacuum)) call vacuum%read_xml(xml)
+    if (present(field)) call field%read_xml(xml)
+    if (present(sliceplot)) call sliceplot%read_xml(xml)
+    if (present(banddos)) call banddos%read_xml(xml)
+    if (present(hybrid)) call hybrid%read_xml(xml)
+    if (present(oneD)) call oneD%read_xml(xml)
+    if (present(coreSpecInput)) call coreSpecInput%read_xml(xml)
+    if (present(wann)) call wann%read_xml(xml)
+    if (present(xcpot)) call xcpot%read_xml(xml)
+    if (present(forcetheo_data)) call forcetheo_data%read_xml(xml)
+    if (present(enparaXML)) call enparaXML%read_xml(xml)
+    if (present(kpts)) CALL kpts%read_xml(xml)
 
   END SUBROUTINE fleurinput_read_xml
 END MODULE m_fleurinput_read_xml
