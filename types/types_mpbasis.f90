e34de27a9e12926ce6c964f7f89bfa5973160a50
Matthias Redies <m.redies@fz-juelich.de>
2019-10-29 16:13:07 +0100

    now delete gptmd

--- types/types_mpbasis.f90
+++ types/types_mpbasis.f90
@@ -4,7 +4,6 @@ module m_types_mpbasis
    type t_mpbasis
       INTEGER, ALLOCATABLE   ::  gptm(:,:) ! (3, num_gpts)
       INTEGER, ALLOCATABLE   ::  ngptm(:)
-      INTEGER                ::  gptmd
    CONTAINS
       procedure :: num_gpts => mpbasis_num_gpts
    end type t_mpbasis
