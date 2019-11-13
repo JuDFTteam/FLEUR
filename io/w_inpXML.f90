d2b784a6163833c1de636066b1b5772171dd82cc
Robin Hilgers <r.hilgers@fz-juelich.de>
2019-11-13 10:39:49 +0100

    Added mtNocoPot switch as written out in inp.xml by default.

--- io/w_inpXML.f90
+++ io/w_inpXML.f90
@@ -202,9 +202,9 @@ SUBROUTINE w_inpXML(&
    END IF
 
    IF (l_nocoOpt.OR.l_explicit) THEN
-160   FORMAT('      <nocoParams l_ss="',l1,'" l_mperp="',l1,'" l_constr="',l1,&
+160   FORMAT('      <nocoParams l_ss="',l1,'" l_mperp="',l1,'" l_constr="',l1,'" l_mtNocoPot="',l1,&
            '" mix_b="',f0.8,'">')
-      WRITE (fileNum,160) noco%l_ss, noco%l_mperp, noco%l_constr, noco%mix_b
+      WRITE (fileNum,160) noco%l_ss, noco%l_mperp, noco%l_constr, noco%l_mtNocoPot ,noco%mix_b
       162 FORMAT('         <qss>',f0.10,' ',f0.10,' ',f0.10,'</qss>')
       WRITE(fileNum,162) noco%qss(1), noco%qss(2), noco%qss(3)
       WRITE (fileNum,'(a)') '      </nocoParams>'
--- main/fleur.F90
+++ main/fleur.F90
@@ -517,7 +517,7 @@ CONTAINS
     ! Test: Build a field, for which the theoretical divergence etc. are known and
     ! compare with the result of the routine.
 
-    CALL builddivtest(stars,atoms,sphhar,vacuum,sym,cell,1,testDen)
+    !CALL builddivtest(stars,atoms,sphhar,vacuum,sym,cell,1,testDen)
     !CALL makeBxc(stars,atoms,sphhar,vacuum,input,noco,vTot,testDen)
     !CALL matrixsplit(stars, atoms, sphhar, vacuum, input, noco, 1.0, inDen, dummyDen, testDen(1), testDen(2), testDen(3))
     !CALL checkplotinp()
@@ -525,12 +525,12 @@ CONTAINS
     !CALL savxsf(stars, atoms, sphhar, vacuum, input, oneD, sym, cell, noco, .FALSE., .FALSE., 'testDeny            ', testDen(2))
     !CALL savxsf(stars, atoms, sphhar, vacuum, input, oneD, sym, cell, noco, .FALSE., .FALSE., 'testDenz            ', testDen(3))
     !CALL sourcefree(mpi,dimension,field,stars,atoms,sphhar,vacuum,input,oneD,sym,cell,noco,testDen)
-    DO i=1,3
-       CALL testGrad(i)%init_potden_simple(stars%ng3,atoms%jmtd,sphhar%nlhd,atoms%ntype,atoms%n_u,1,.FALSE.,.FALSE.,POTDEN_TYPE_POTTOT,vacuum%nmzd,vacuum%nmzxyd,stars%ng2)
-       ALLOCATE(testGrad(i)%pw_w,mold=testGrad(i)%pw)
-    ENDDO
-    CALL divpotgrad(stars,atoms,sphhar,vacuum,sym,cell,noco,testDen(2),testGrad)
-    CALL savxsf(stars, atoms, sphhar, vacuum, input, oneD, sym, cell, noco, .FALSE., .FALSE., 'testGrad            ', testGrad(1), testGrad(1), testGrad(2), testGrad(3))
+    !DO i=1,3
+    !   CALL testGrad(i)%init_potden_simple(stars%ng3,atoms%jmtd,sphhar%nlhd,atoms%ntype,atoms%n_u,1,.FALSE.,.FALSE.,POTDEN_TYPE_POTTOT,vacuum%nmzd,vacuum%nmzxyd,stars%ng2)
+    !   ALLOCATE(testGrad(i)%pw_w,mold=testGrad(i)%pw)
+    !ENDDO
+    !CALL divpotgrad(stars,atoms,sphhar,vacuum,sym,cell,noco,testDen(2),testGrad)
+    !CALL savxsf(stars, atoms, sphhar, vacuum, input, oneD, sym, cell, noco, .FALSE., .FALSE., 'testGrad            ', testGrad(1), testGrad(1), testGrad(2), testGrad(3))
 
     CALL add_usage_data("Iterations",iter)
 
