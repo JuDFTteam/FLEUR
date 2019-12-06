47851ac7e0b6ff1677deec31a47642db7c3c8db3
Matthias Redies <m.redies@fz-juelich.de>
2019-12-06 13:12:10 +0100

    recomment IS part

--- hybrid/wavefproducts_noinv.f90
+++ hybrid/wavefproducts_noinv.f90
@@ -45,10 +45,10 @@ CONTAINS
       nkqpt = kpts%get_nk(kqpt)
       IF (.not. kpts%is_kpt(kqpt)) call juDFT_error('wavefproducts: k-point not found')
 
-      !call wavefproducts_noinv5_IS(bandi, bandf, bandoi, bandof, nk, iq, g_t,&
-      !                                   dimension, input, jsp, cell, atoms, mpbasis, hybrid,&
-      !                                   hybdat, kpts, lapw, sym, nbasm_mt, noco,&
-      !                                   nkqpt, cprod)
+      call wavefproducts_noinv5_IS(bandi, bandf, bandoi, bandof, nk, iq, g_t,&
+                                        dimension, input, jsp, cell, atoms, mpbasis, hybrid,&
+                                        hybdat, kpts, lapw, sym, nbasm_mt, noco,&
+                                        nkqpt, cprod)
 
       call wavefproducts_noinv_MT(bandi, bandf, bandoi, bandof, nk, iq, &
                                   dimension, atoms, mpbasis, hybrid, hybdat, kpts, &
