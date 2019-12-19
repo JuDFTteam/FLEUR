f8c9f17c226a53e54341449437d7837cf1234288
janssen <he.janssen@fz-juelich.de>
2019-12-19 14:47:31 +0100

    corrected LDA+U array dimension to atoms%n_u+atoms%n_hia and readded one LDA+U+SOC => complex eigenvalues condition at one place

--- main/fleur_init.F90
+++ main/fleur_init.F90
@@ -185,7 +185,8 @@ CONTAINS
     !Collect some usage info
     CALL add_usage_data("A-Types",atoms%ntype)
     CALL add_usage_data("Atoms",atoms%nat)
-    CALL add_usage_data("Real",sym%invs.AND..NOT.noco%l_noco)
+    CALL add_usage_data("Real",sym%invs.AND..NOT.noco%l_noco&
+                        .AND..NOT.(noco%l_soc.AND.atoms%n_u+atoms%n_hia.GT.0))
     CALL add_usage_data("Spins",input%jspins)
     CALL add_usage_data("Noco",noco%l_noco)
     CALL add_usage_data("SOC",noco%l_soc)
