eda2cd4efff52896b94dbe1a7d02ea5af4d37e62
Matthias Redies <m.redies@fz-juelich.de>
2020-01-27 11:40:22 +0100

    fix formating in symm_hf

--- hybrid/symm_hf.f90
+++ hybrid/symm_hf.f90
@@ -10,8 +10,8 @@
 !                                             M.Betzinger (09/07)     !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 MODULE m_symm_hf
-  use m_judft
-  USE m_types_hybdat
+   use m_judft
+   USE m_types_hybdat
 
    USE m_constants
    USE m_types
@@ -29,7 +29,7 @@ CONTAINS
       TYPE(t_kpts), INTENT(IN)    :: kpts
       INTEGER, INTENT(IN)    :: nk
       INTEGER, INTENT(INOUT) :: nsymop
-      INTEGER, INTENT(INOUT) :: rrot(:,:,:) ! 3,3,sym%nsym
+      INTEGER, INTENT(INOUT) :: rrot(:, :, :) ! 3,3,sym%nsym
       INTEGER, INTENT(INOUT) :: psym(:) ! Note: psym is only filled up to index nsymop
 
       INTEGER :: i
@@ -38,7 +38,7 @@ CONTAINS
       nsymop = 0
       ! calculate rotations in reciprocal space
       DO i = 1, sym%nsym
-         IF (i <= sym%nop) THEN
+         IF(i <= sym%nop) THEN
             rrot(:, :, i) = transpose(sym%mrot(:, :, sym%invtab(i)))
          ELSE
             rrot(:, :, i) = -rrot(:, :, i - sym%nop)
@@ -59,19 +59,19 @@ CONTAINS
          rotkpt = kpts%to_first_bz(rotkpt)
 
          !check if rotkpt is identical to bk(:,nk)
-         IF (maxval(abs(rotkpt - kpts%bkf(:, nk))) <= 1E-07) THEN
+         IF(maxval(abs(rotkpt - kpts%bkf(:, nk))) <= 1E-07) THEN
             nsymop = nsymop + 1
             psym(nsymop) = i
          END IF
       END DO
 
-      WRITE (6, '(A,i3)') ' nk', nk
-      WRITE (6, '(A,3f10.5)') ' kpts%bkf(:,nk):', kpts%bkf(:, nk)
-      WRITE (6, '(A,i3)') ' Number of elements in the little group:', nsymop
+      WRITE(6, '(A,i3)') ' nk', nk
+      WRITE(6, '(A,3f10.5)') ' kpts%bkf(:,nk):', kpts%bkf(:, nk)
+      WRITE(6, '(A,i3)') ' Number of elements in the little group:', nsymop
 
    END SUBROUTINE symm_hf_init
 
-   SUBROUTINE symm_hf(kpts, nk, sym, hybdat, eig_irr, input,atoms, mpdata, hybinp, cell, &
+   SUBROUTINE symm_hf(kpts, nk, sym, hybdat, eig_irr, input, atoms, mpdata, hybinp, cell, &
                       lapw, jsp, rrot, nsymop, psym, nkpt_EIBZ, n_q, parent, &
                       pointer_EIBZ, nsest, indx_sest)
 
@@ -98,7 +98,7 @@ CONTAINS
       INTEGER, INTENT(IN)              :: nsymop
 
 !     - arrays -
-      INTEGER, INTENT(IN)              :: rrot(:,:,:)
+      INTEGER, INTENT(IN)              :: rrot(:, :, :)
       INTEGER, INTENT(IN)              :: psym(:)
       INTEGER, INTENT(INOUT)           :: parent(kpts%nkptf)
       INTEGER, INTENT(INOUT)           :: nsest(hybdat%nbands(nk))
@@ -106,7 +106,7 @@ CONTAINS
       INTEGER, ALLOCATABLE, INTENT(INOUT) :: pointer_EIBZ(:)
       INTEGER, ALLOCATABLE, INTENT(INOUT) :: n_q(:)
 
-      REAL, INTENT(IN)                 :: eig_irr(:,:)
+      REAL, INTENT(IN)                 :: eig_irr(:, :)
 
 !     - local scalars -
       INTEGER                         :: ikpt, ikpt1, iop, isym, iisym, m
@@ -144,9 +144,8 @@ CONTAINS
       COMPLEX, ALLOCATABLE             :: rep_d(:, :, :)
       LOGICAL, ALLOCATABLE             :: symequivalent(:, :)
 
-      parent = 0; nsest = 0; indx_sest = 0; nkpt_EIBZ =0;
-
-      WRITE (6, '(A)') new_line('n')//new_line('n')//'### subroutine: symm ###'
+      parent = 0; nsest = 0; indx_sest = 0; nkpt_EIBZ = 0;
+      WRITE(6, '(A)') new_line('n')//new_line('n')//'### subroutine: symm ###'
 
       ! determine extented irreducible BZ of k ( EIBZ(k) ), i.e.
       ! those k-points, which can generate the whole BZ by
@@ -165,14 +164,14 @@ CONTAINS
 
             !determine number of rotkpt
             nrkpt = kpts%get_nk(rotkpt)
-            IF (nrkpt == 0) call judft_error('symm: Difference vector not found !')
+            IF(nrkpt == 0) call judft_error('symm: Difference vector not found !')
 
-            IF (list(nrkpt) /= 0) THEN
+            IF(list(nrkpt) /= 0) THEN
                list(nrkpt) = 0
                neqvkpt(ikpt) = neqvkpt(ikpt) + 1
                parent(nrkpt) = ikpt
             END IF
-            IF (all(list == 0)) EXIT
+            IF(all(list == 0)) EXIT
 
          END DO
       END DO
@@ -184,20 +183,20 @@ CONTAINS
       ! determine number of members in the EIBZ(k)
       ic = 0
       DO ikpt = 1, kpts%nkptf
-         IF (parent(ikpt) == ikpt) ic = ic + 1
+         IF(parent(ikpt) == ikpt) ic = ic + 1
       END DO
       nkpt_EIBZ = ic
 
       allocate(pointer_EIBZ(nkpt_EIBZ), source=0)
       ic = 0
       DO ikpt = 1, kpts%nkptf
-         IF (parent(ikpt) == ikpt) THEN
+         IF(parent(ikpt) == ikpt) THEN
             ic = ic + 1
             pointer_EIBZ(ic) = ikpt
          END IF
       END DO
 
-      WRITE (6, '(A,i5)') ' Number of k-points in the EIBZ', nkpt_EIBZ
+      WRITE(6, '(A,i5)') ' Number of k-points in the EIBZ', nkpt_EIBZ
 
       ! determine the factor n_q, that means the number of symmetrie operations of the little group of bk(:,nk)
       ! which keep q (in EIBZ) invariant
@@ -206,7 +205,7 @@ CONTAINS
       ic = 0
       n_q = 0
       DO ikpt = 1, kpts%nkptf
-         IF (parent(ikpt) == ikpt) THEN
+         IF(parent(ikpt) == ikpt) THEN
             ic = ic + 1
             DO iop = 1, nsymop
                isym = psym(iop)
@@ -216,13 +215,13 @@ CONTAINS
                rotkpt = kpts%to_first_bz(rotkpt)
 
                !check if rotkpt is identical to bk(:,ikpt)
-               IF (maxval(abs(rotkpt - kpts%bkf(:, ikpt))) <= 1E-06) THEN
+               IF(maxval(abs(rotkpt - kpts%bkf(:, ikpt))) <= 1E-06) THEN
                   n_q(ic) = n_q(ic) + 1
                END IF
             END DO
          END IF
       END DO
-      IF (ic /= nkpt_EIBZ) call judft_error('symm: failure EIBZ')
+      IF(ic /= nkpt_EIBZ) call judft_error('symm: failure EIBZ')
 
       ! calculate degeneracy:
       ! degenerat(i) = 1 state i  is not degenerat,
@@ -233,18 +232,18 @@ CONTAINS
 
       degenerat = 1
 
-      WRITE (6, '(A,f10.8)') ' Tolerance for determining degenerate states=', tolerance
+      WRITE(6, '(A,f10.8)') ' Tolerance for determining degenerate states=', tolerance
 
       DO i = 1, hybdat%nbands(nk)
          DO j = i + 1, hybdat%nbands(nk)
-            IF (abs(eig_irr(i, nk) - eig_irr(j, nk)) <= tolerance) THEN
+            IF(abs(eig_irr(i, nk) - eig_irr(j, nk)) <= tolerance) THEN
                degenerat(i) = degenerat(i) + 1
             END IF
          END DO
       END DO
 
       DO i = 1, hybdat%ne_eig(nk)
-         IF (degenerat(i) /= 1 .or. degenerat(i) /= 0) THEN
+         IF(degenerat(i) /= 1 .or. degenerat(i) /= 0) THEN
             degenerat(i + 1:i + degenerat(i) - 1) = 0
          END IF
       END DO
@@ -255,9 +254,9 @@ CONTAINS
       ! number of different degenerate bands/states
       nddb = count(degenerat >= 1)
 
-      WRITE (6, *) ' Degenerate states:'
+      WRITE(6, *) ' Degenerate states:'
       DO iband = 1, hybdat%nbands(nk)/5 + 1
-         WRITE (6, '(5i5)') degenerat(iband*5 - 4:min(iband*5, hybdat%nbands(nk)))
+         WRITE(6, '(5i5)') degenerat(iband*5 - 4:min(iband*5, hybdat%nbands(nk)))
       END DO
 
       ! read in cmt and z at current k-point (nk)
@@ -265,9 +264,9 @@ CONTAINS
       CALL read_cmt(cmt, nk)
       !CALL intgrf_init(atoms%ntype,atoms%jmtd,atoms%jri,atoms%dx,atoms%rmsh,hybdat%gridf)
 
-      IF (allocated(olapmt)) deallocate(olapmt)
+      IF(allocated(olapmt)) deallocate(olapmt)
       allocate(olapmt(maxval(mpdata%num_radfun_per_l), maxval(mpdata%num_radfun_per_l), 0:atoms%lmaxd, atoms%ntype), stat=ok)
-      IF (ok /= 0) call judft_error('symm: failure allocation olapmt')
+      IF(ok /= 0) call judft_error('symm: failure allocation olapmt')
       olapmt = 0
 
       DO itype = 1, atoms%ntype
@@ -276,16 +275,16 @@ CONTAINS
             DO n2 = 1, nn
                DO n1 = 1, nn
                   olapmt(n1, n2, l, itype) = intgrf( &
-                                hybdat%bas1(:, n1, l, itype)*hybdat%bas1(:, n2, l, itype)&
-                               + hybdat%bas2(:, n1, l, itype)*hybdat%bas2(:, n2, l, itype),&
-                                atoms, itype, hybdat%gridf)
+                                             hybdat%bas1(:, n1, l, itype)*hybdat%bas1(:, n2, l, itype) &
+                                             + hybdat%bas2(:, n1, l, itype)*hybdat%bas2(:, n2, l, itype), &
+                                             atoms, itype, hybdat%gridf)
                END DO
             END DO
          END DO
       END DO
 
       allocate(wavefolap(hybdat%nbands(nk), hybdat%nbands(nk)), carr(maxval(mpdata%num_radfun_per_l)), stat=ok)
-      IF (ok /= 0) call judft_error('symm: failure allocation wfolap/maxindx')
+      IF(ok /= 0) call judft_error('symm: failure allocation wfolap/maxindx')
       wavefolap = 0
 
       iatom = 0
@@ -297,12 +296,12 @@ CONTAINS
                DO M = -l, l
                   nn = mpdata%num_radfun_per_l(l, itype)
                   DO iband1 = 1, hybdat%nbands(nk)
-                     carr(:nn) = matmul(olapmt(:nn, :nn, l, itype),&
-                                         cmt(iband1, lm + 1:lm + nn, iatom))
+                     carr(:nn) = matmul(olapmt(:nn, :nn, l, itype), &
+                                        cmt(iband1, lm + 1:lm + nn, iatom))
                      DO iband2 = 1, iband1
-                        wavefolap(iband2, iband1)&
-                      = wavefolap(iband2, iband1)&
-                      + dot_product(cmt(iband2, lm + 1:lm + nn, iatom), carr(:nn))
+                        wavefolap(iband2, iband1) &
+                           = wavefolap(iband2, iband1) &
+                             + dot_product(cmt(iband2, lm + 1:lm + nn, iatom), carr(:nn))
                      END DO
                   END DO
                   lm = lm + nn
@@ -318,20 +317,20 @@ CONTAINS
       END DO
 
       allocate(symequivalent(nddb, nddb), stat=ok)
-      IF (ok /= 0) call judft_error('symm: failure allocation symequivalent')
+      IF(ok /= 0) call judft_error('symm: failure allocation symequivalent')
       symequivalent = .false.
       ic1 = 0
       DO iband1 = 1, hybdat%nbands(nk)
          ndb1 = degenerat(iband1)
-         IF (ndb1 == 0) CYCLE
+         IF(ndb1 == 0) CYCLE
          ic1 = ic1 + 1
          ic2 = 0
          DO iband2 = 1, hybdat%nbands(nk)
             ndb2 = degenerat(iband2)
-            IF (ndb2 == 0) CYCLE
+            IF(ndb2 == 0) CYCLE
             ic2 = ic2 + 1
-            IF (any(abs(wavefolap(iband1:iband1 + ndb1 - 1,&
-                                   iband2:iband2 + ndb2 - 1)) > 1E-9)) THEN
+            IF(any(abs(wavefolap(iband1:iband1 + ndb1 - 1, &
+                                 iband2:iband2 + ndb2 - 1)) > 1E-9)) THEN
 !                .and. ndb1 .eq. ndb2 ) THEN
                symequivalent(ic2, ic1) = .true.
             END IF
@@ -348,23 +347,23 @@ CONTAINS
       nsest = 0
       DO iband1 = 1, hybdat%nbands(nk)
          ndb1 = degenerat(iband1)
-         IF (ndb1 >= 1) ic1 = ic1 + 1
+         IF(ndb1 >= 1) ic1 = ic1 + 1
          i = 0
-         DO WHILE (degenerat(iband1 - i) == 0)
+         DO WHILE(degenerat(iband1 - i) == 0)
             i = i + 1
          END DO
          ndb1 = degenerat(iband1 - i)
          ic2 = 0
          DO iband2 = 1, hybdat%nbands(nk)
             ndb2 = degenerat(iband2)
-            IF (ndb2 >= 1) ic2 = ic2 + 1
+            IF(ndb2 >= 1) ic2 = ic2 + 1
             i = 0
-            DO WHILE (degenerat(iband2 - i) == 0)
+            DO WHILE(degenerat(iband2 - i) == 0)
                i = i + 1
             END DO
             ndb2 = degenerat(iband2 - i)
             ! only upper triangular part
-            IF (symequivalent(ic2, ic1) .and. iband2 <= iband1) THEN
+            IF(symequivalent(ic2, ic1) .and. iband2 <= iband1) THEN
 !            IF( ndb1 .ne. ndb2 ) call judft_error('symm_hf: failure symequivalent')
                nsest(iband1) = nsest(iband1) + 1
                indx_sest(nsest(iband1), iband1) = iband2
@@ -385,9 +384,9 @@ CONTAINS
 
       pi = pimach()
 
-      IF (hybdat%lmaxcd > atoms%lmaxd) STOP &
-       'symm_hf: The very impropable case that hybdat%lmaxcd > atoms%lmaxd occurs'
-
+      IF(hybdat%lmaxcd > atoms%lmaxd) then
+         call judft_error('symm_hf: The very impropable case that hybdat%lmaxcd > atoms%lmaxd occurs')
+      endif
       iatom = 0
       iatom0 = 0
       DO itype = 1, atoms%ntype
@@ -395,7 +394,7 @@ CONTAINS
             iatom = iatom + 1
             DO iop = 1, nsymop
                isym = psym(iop)
-               IF (isym <= sym%nop) THEN
+               IF(isym <= sym%nop) THEN
                   iisym = isym
                ELSE
                   iisym = isym - sym%nop
@@ -406,7 +405,7 @@ CONTAINS
                g = nint(rotkpt - kpts%bkf(:, nk))
 
                cdum = exp(-2*pi*img*dot_product(rotkpt, sym%tau(:, iisym)))* &
-                        exp(2*pi*img*dot_product(g, atoms%taual(:, ratom)))
+                      exp(2*pi*img*dot_product(g, atoms%taual(:, ratom)))
             END DO
          END DO
          iatom0 = iatom0 + atoms%neq(itype)
@@ -432,14 +431,14 @@ CONTAINS
       INTEGER               ::  nsymop, nrkpt
 !     - local arrays -
       INTEGER               ::  rrot(3, 3, sym%nsym)
-      INTEGER               ::  neqvkpt(kpts%nkptf), list(kpts%nkptf), parent(kpts%nkptf),&
-                                symop(kpts%nkptf)
+      INTEGER               ::  neqvkpt(kpts%nkptf), list(kpts%nkptf), parent(kpts%nkptf), &
+                               symop(kpts%nkptf)
       INTEGER, ALLOCATABLE  ::  psym(:)!,help(:)
       REAL                  ::  rotkpt(3)
 
       ! calculate rotations in reciprocal space
       DO isym = 1, sym%nsym
-         IF (isym <= sym%nop) THEN
+         IF(isym <= sym%nop) THEN
             rrot(:, :, isym) = transpose(sym%mrot(:, :, sym%invtab(isym)))
          ELSE
             rrot(:, :, isym) = -rrot(:, :, isym - sym%nop)
@@ -461,7 +460,7 @@ CONTAINS
          rotkpt = kpts%to_first_bz(rotkpt)
 
          !check if rotkpt is identical to bk(:,nk)
-         IF (maxval(abs(rotkpt - kpts%bkf(:, nk))) <= 1E-07) THEN
+         IF(maxval(abs(rotkpt - kpts%bkf(:, nk))) <= 1E-07) THEN
             ic = ic + 1
             psym(ic) = iop
          END IF
@@ -498,20 +497,20 @@ CONTAINS
             !determine number of rotkpt
             nrkpt = 0
             DO ikpt1 = 1, kpts%nkptf
-               IF (maxval(abs(rotkpt - kpts%bkf(:, ikpt1))) <= 1E-06) THEN
+               IF(maxval(abs(rotkpt - kpts%bkf(:, ikpt1))) <= 1E-06) THEN
                   nrkpt = ikpt1
                   EXIT
                END IF
             END DO
-            IF (nrkpt == 0) call judft_error('symm: Difference vector not found !')
+            IF(nrkpt == 0) call judft_error('symm: Difference vector not found !')
 
-            IF (list(nrkpt) /= 0) THEN
+            IF(list(nrkpt) /= 0) THEN
                list(nrkpt) = 0
                neqvkpt(ikpt) = neqvkpt(ikpt) + 1
                parent(nrkpt) = ikpt
                symop(nrkpt) = psym(iop)
             END IF
-            IF (all(list == 0)) EXIT
+            IF(all(list == 0)) EXIT
 
          END DO
       END DO
@@ -523,7 +522,7 @@ CONTAINS
       ! determine number of members in the EIBZ(k)
       ic = 0
       DO ikpt = 1, kpts%nkptf
-         IF (parent(ikpt) == ikpt) ic = ic + 1
+         IF(parent(ikpt) == ikpt) ic = ic + 1
       END DO
       symm_hf_nkpt_EIBZ = ic
 
