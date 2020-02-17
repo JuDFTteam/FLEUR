2c8cfc499bf6778328d8d9a48e2ce4faba9c0ef5
Matthias Redies <m.redies@fz-juelich.de>
2020-02-17 15:14:51 +0100

    new v_x io

--- io/io_hybrid.F90
+++ io/io_hybrid.F90
@@ -175,6 +175,7 @@ contains
       implicit none
       TYPE(t_mat), INTENT(IN)   :: mat
       INTEGER, INTENT(IN)           :: rec
+      integer :: id
 
       CALL write_matrix(mat, rec, id_olap)
    END subroutine write_olap
@@ -237,18 +238,25 @@ contains
 
    subroutine read_v_x(mat, rec)
       implicit none
-      TYPE(t_mat), INTENT(INOUT):: mat
-      INTEGER, INTENT(IN)           :: rec
+      TYPE(t_mat), INTENT(INOUT)  :: mat
+      INTEGER, INTENT(IN)         :: rec
+      integer                     :: id
 
-      CALL read_matrix(mat, rec, id_v_x)
+      id = open_matrix(mat%l_real, mat%matsize1, 1, 1, "v_x." // int2str(rec))
+      CALL read_matrix(mat, 1, id)
+      call close_matrix(id)
    END subroutine read_v_x
 
    subroutine write_v_x(mat, rec)
+      use m_juDFT
       implicit none
       TYPE(t_mat), INTENT(IN)   :: mat
-      INTEGER, INTENT(IN)           :: rec
+      INTEGER, INTENT(IN)       :: rec
+      integer :: id
 
-      CALL write_matrix(mat, rec, id_v_x)
+      id = open_matrix(mat%l_real, mat%matsize1, 1, 1, "v_x." // int2str(rec))
+      CALL write_matrix(mat, 1, id)
+      call close_matrix(id)
    END subroutine write_v_x
 
 end module m_io_hybinp
