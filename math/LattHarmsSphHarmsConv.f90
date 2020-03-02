447210ae465fd67a85266ab6c37ae1e8d125848f
Alexander Neukirchen <a.neukirchen@fz-juelich.de>
2020-03-02 16:33:02 +0100

    Rethinking the lh lm transformation.

--- math/LattHarmsSphHarmsConv.f90
+++ math/LattHarmsSphHarmsConv.f90
@@ -1,42 +1,68 @@
 MODULE m_lattHarmsSphHarmsConv
 
-CONTAINS
-
-SUBROUTINE lattHarmsRepToSphHarms(sym,atoms, lattHarms, iType, funcLattHarms, funcSphHarms)
-
    USE m_types
 
-   IMPLICIT NONE
-   TYPE(t_sym),    INTENT(IN)    :: sym
-   TYPE(t_atoms),  INTENT(IN)    :: atoms
-   TYPE(t_sphhar), INTENT(IN)    :: lattHarms
-   INTEGER,        INTENT(IN)    :: iType
-   REAL,           INTENT(IN)    :: funcLattHarms(:,0:) ! (iR,iLH)
-   COMPLEX,        INTENT(INOUT) :: funcSphHarms(:,:) ! (iR,lm)
+CONTAINS
 
-   INTEGER :: iAtom, iLH, ns, l, iM, m, lm, iR
+   SUBROUTINE lattHarmsRepToSphHarms(sym, atoms, lattHarms, iType, funcLattHarms, funcSphHarms)
 
-   iAtom = SUM(atoms%neq(:iType-1)) + 1
-   ns = sym%ntypsy(iAtom)
+      IMPLICIT NONE
 
-   funcSphHarms = CMPLX(0.0,0.0)
+      TYPE(t_sym),    INTENT(IN)    :: sym
+      TYPE(t_atoms),  INTENT(IN)    :: atoms
+      TYPE(t_sphhar), INTENT(IN)    :: lattHarms
+      INTEGER,        INTENT(IN)    :: iType
+      REAL,           INTENT(IN)    :: funcLattHarms(:,0:) ! (iR,iLH)
+      COMPLEX,        INTENT(INOUT) :: funcSphHarms(:,:) ! (iR,lm)
 
-   DO iLH = 0, lattHarms%nlh(ns)
-      l = lattHarms%llh(iLH,ns)
-      DO iM = 1, lattHarms%nmem(iLH,ns)
-         m = lattHarms%mlh(iM,iLH,ns)
-         lm = l*(l+1) + m + 1
-         DO iR = 1, atoms%jri(iType)
-            funcSphHarms(iR,lm) = funcSphHarms(iR,lm) + funcLattHarms(iR,iLH) * lattHarms%clnu(iM,iLH,ns)
+      INTEGER :: iAtom, iLH, ns, l, iM, m, lm, iR
+
+      iAtom = SUM(atoms%neq(:iType-1)) + 1
+      ns = sym%ntypsy(iAtom)
+
+      funcSphHarms = CMPLX(0.0,0.0)
+
+      DO iLH = 0, lattHarms%nlh(ns)
+         l = lattHarms%llh(iLH,ns)
+         DO iM = 1, lattHarms%nmem(iLH,ns)
+            m = lattHarms%mlh(iM,iLH,ns)
+            lm = l*(l+1) + m + 1
+            DO iR = 1, atoms%jri(iType)
+               funcSphHarms(iR,lm) = funcSphHarms(iR,lm) + funcLattHarms(iR,iLH) * lattHarms%clnu(iM,iLH,ns)
+            END DO
          END DO
       END DO
-   END DO
 
-END SUBROUTINE lattHarmsRepToSphHarms
+   END SUBROUTINE lattHarmsRepToSphHarms
 
-SUBROUTINE sphHarmsRepToLattHarms()
+SUBROUTINE sphHarmsRepToLattHarms(sym, atoms, lattHarms, iType, funcSphHarms, funcLattHarms)
 
    IMPLICIT NONE
+
+      TYPE(t_sym),    INTENT(IN)    :: sym
+      TYPE(t_atoms),  INTENT(IN)    :: atoms
+      TYPE(t_sphhar), INTENT(IN)    :: lattHarms
+      INTEGER,        INTENT(IN)    :: iType
+      COMPLEX,        INTENT(IN)    :: funcSphHarms(:,:) ! (iR,lm)
+      REAL,           INTENT(INOUT) :: funcLattHarms(:,0:) ! (iR,iLH)
+
+      INTEGER :: iAtom, iLH, ns, l, iM, m, lm, iR
+
+      iAtom = SUM(atoms%neq(:iType-1)) + 1
+      ns = sym%ntypsy(iAtom)
+
+      funcSphHarms = CMPLX(0.0,0.0)
+
+      DO iLH = 0, lattHarms%nlh(ns)
+         l = lattHarms%llh(iLH,ns)
+         DO iM = 1, lattHarms%nmem(iLH,ns)
+            m = lattHarms%mlh(iM,iLH,ns)
+            lm = l*(l+1) + m + 1
+            DO iR = 1, atoms%jri(iType)
+               funcLattHarms(iR,iLH) = funcLattHarms(iR,iLH) + funcSphHarms(iR,lm) * CONJG(lattHarms%clnu(iM,iLH,ns))
+            END DO
+         END DO
+      END DO
 
 END SUBROUTINE sphHarmsRepToLattHarms
 
