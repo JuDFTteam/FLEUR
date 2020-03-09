8242ed8559bad0c4755035b73e68690ba548e5fb
janssen <he.janssen@fz-juelich.de>
2020-03-09 13:40:01 +0100

    Better format for j charges in out.xml

--- cdn_mt/cdnmt.F90
+++ cdn_mt/cdnmt.F90
@@ -52,7 +52,7 @@ CONTAINS
     !     ..
     !     .. Local Arrays ..
     REAL qmtl(0:atoms%lmaxd,jspd,atoms%ntype),qmtllo(0:atoms%lmaxd),vrTmp(atoms%jmtd)
-    CHARACTER(LEN=20) :: attributes(8)
+    CHARACTER(LEN=20) :: attributes(6)
 
     !     ..
     !     .. Allocatable Arrays ..
@@ -311,18 +311,22 @@ CONTAINS
 8300           FORMAT(' -->',i3,2x,f9.5,2x,6f9.5)
                WRITE(6,*)
 
-               attributes = ''
-               WRITE(attributes(1),'(i0)') natom
-               WRITE(attributes(2),'(f12.7)') jDOS%occ(0,1,natom)
-               WRITE(attributes(3),'(f12.7)') jDOS%occ(1,1,natom)
-               WRITE(attributes(4),'(f12.7)') jDOS%occ(1,2,natom)
-               WRITE(attributes(5),'(f12.7)') jDOS%occ(2,1,natom)
-               WRITE(attributes(6),'(f12.7)') jDOS%occ(2,2,natom)
-               WRITE(attributes(7),'(f12.7)') jDOS%occ(3,1,natom)
-               WRITE(attributes(8),'(f12.7)') jDOS%occ(3,2,natom)
+               CALL openXMLElementPoly('mtJcharge',['atom'],[natom])
 
-               CALL writeXMLElementForm('JCharge',['atom','s   ','p12 ','p32 ','d32 ','d52 ','f52 ','f72 '],attributes,&
-                                        reshape([4,1,3,3,3,3,3,3,6,12,12,12,12,12,12,12],[8,2]))
+               attributes = ''
+               WRITE(attributes(1),'(f12.7)') jDOS%occ(1,1,natom)
+               WRITE(attributes(2),'(f12.7)') jDOS%occ(2,1,natom)
+               WRITE(attributes(3),'(f12.7)') jDOS%occ(3,1,natom)
+               CALL writeXMLElementForm('lowJ',['p','d','f'],attributes(:3),reshape([1,1,1,12,12,12],[3,2]))
+
+               attributes = ''
+               WRITE(attributes(1),'(f12.7)') jDOS%occ(1,2,natom)
+               WRITE(attributes(2),'(f12.7)') jDOS%occ(2,2,natom)
+               WRITE(attributes(3),'(f12.7)') jDOS%occ(3,2,natom)
+               CALL writeXMLElementForm('highJ',['p','d','f'],attributes(:3),reshape([1,1,1,12,12,12],[3,2]))
+
+               CALL closeXMLElement('mtJcharge')
+
             ENDDO
          ENDDO
       ENDIF
