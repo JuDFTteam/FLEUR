b5cd7d7f9017f490572fdb7a183d1323c04068bc
Matthias Redies <m.redies@fz-juelich.de>
2020-03-10 14:34:33 +0100

    remove more merge markers

--- init/make_sym.f90
+++ init/make_sym.f90
@@ -48,14 +48,10 @@ CONTAINS
 
       !Generated wigner symbols for LDA+U (includes DFT+HubbardI)
       IF (ALLOCATED(sym%d_wgn)) DEALLOCATE (sym%d_wgn)
-<<<<<<< HEAD
       ALLOCATE (sym%d_wgn(-lmaxU_const:lmaxU_const, -lmaxU_const:lmaxU_const, lmaxU_const, sym%nop), &
                source=CMPLX_NOT_INITALIZED)
       IF (atoms%n_u + gfinp%n .GT. 0) THEN !replace with atoms%n_u+gfinp%n
-=======
-      ALLOCATE (sym%d_wgn(-lmaxU_const:lmaxU_const, -lmaxU_const:lmaxU_const, lmaxU_const, sym%nop))
-      IF (atoms%n_u+gfinp%n.GT.0) THEN
->>>>>>> develop
+
          CALL d_wigner(sym%nop, sym%mrot, cell%bmat, lmaxU_const, sym%d_wgn)
          !For spin-offdiagonal parts, we need additional phase factors
          IF (noco%l_mperp) THEN
