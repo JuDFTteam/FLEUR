64c08f1af535be29f9464e9bb53bb11bf8112f62
Matthias Redies <m.redies@fz-juelich.de>
2020-03-12 15:33:19 +0100

    shorten error message

--- inpgen2/read_inpgen_input.f90
+++ inpgen2/read_inpgen_input.f90
@@ -101,7 +101,7 @@ CONTAINS
           CASE('gen ','sym ')
              CALL judft_error("Specifying the symmetries no longer supported in inpgen")
           CASE default
-             CALL judft_error(("Unknown input in:"//line))
+             CALL judft_error(("Unknown input in:"//trim(line)))
           END SELECT
        ELSE
           IF (aa.ne.0) THEN
