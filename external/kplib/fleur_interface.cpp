296b685afbf944d59cd1bdf9b03f023136e4a480
Daniel Wortmann <d.wortmann@fz-juelich.de>
2020-03-12 16:27:56 +0100

    Fixes for kplib

--- external/kplib/fleur_interface.cpp
+++ external/kplib/fleur_interface.cpp
@@ -1,8 +1,8 @@
 #include "kplib/src/kPointLattice.h"
 #include "kplib/src/kPointLatticeGenerator.h"
-
 static KPointLattice kp;
 
+extern "C"{
 int kplib_generate(const double primVectorsArray[3][3],
   const int latticePointOperatorsArray[][3][3],
   const int numOperators,
@@ -22,13 +22,14 @@ int kplib_generate(const double primVectorsArray[3][3],
 }
 
 
-void set_kpoints(double kpoints[][3],int weights[]){
+void kplib_setkpoints(double kpoints[][3],double weights[]){
   Tensor<double> coords = kp.getKPointCoordinates();
+  std::vector<int> w=kp.getKPointWeights();
   for (int i = 0; i < (int) coords.size(); ++i) {
     kpoints[i][0]=coords[i][0];
     kpoints[i][1]=coords[i][1];
     kpoints[i][2]=coords[i][2];
+    weights[i]=w[i];
   }
-  std::vector<int> w=kp.getKPointWeights();
-  weights=&w[0];
+}
 }
