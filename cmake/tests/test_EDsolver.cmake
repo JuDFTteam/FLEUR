609ce562a234687453e78cdb9a7b57c1acb5fb4c
janssen <he.janssen@fz-juelich.de>
2020-03-15 12:00:24 +0100

    No idea why this was there

--- cmake/tests/test_EDsolver.cmake
+++ cmake/tests/test_EDsolver.cmake
@@ -54,9 +54,9 @@ if (DEFINED CLI_FLEUR_USE_EDSOLVER)
                              We will try to download and compile the EDsolver library along with FLEUR")
             if(NOT EXISTS "${PROJECT_SOURCE_DIR}/external/edsolver-library/src")
                find_package(Git REQUIRED)
-               execute_process(COMMAND ${GIT_EXECUTABLE} submodule init -v external/edsolver-library
+               execute_process(COMMAND ${GIT_EXECUTABLE} submodule init external/edsolver-library
                                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR} RESULT_VARIABLE _res_init)
-               execute_process(COMMAND ${GIT_EXECUTABLE} submodule update -v
+               execute_process(COMMAND ${GIT_EXECUTABLE} submodule update
                                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR} RESULT_VARIABLE _res_update)
                if( ${_res_init} GREATER 0 OR ${_res_update} GREATER 0 )
                   message(FATAL_ERROR "EDsolver source could not be downloaded. \
