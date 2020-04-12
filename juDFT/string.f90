53643556e1425291fd3338db9b36cacc3e3a7dd1
Matthias Redies <m.redies@fz-juelich.de>
2020-04-12 10:49:19 +0200

    add byte_to_str function

--- juDFT/string.f90
+++ juDFT/string.f90
@@ -1,7 +1,6 @@
 module m_juDFT_string
    implicit none
-      character(len=3), parameter :: whitespaces = " "//achar(9)//achar(13) ! list of all whitespaces
-
+   character(len=3), parameter :: whitespaces = " "//achar(9)//achar(13) ! list of all whitespaces
 
    interface int2str
       module procedure int2str_int4, int2str_int8
@@ -15,12 +14,12 @@ contains
       integer                       :: front, back
 
       front = 1
-      do while(index(whitespaces, input(front:front)) /= 0)
+      do while (index(whitespaces, input(front:front)) /= 0)
          front = front + 1
       enddo
 
       back = len(input)
-      do while(index(whitespaces, input(back:back)) /= 0)
+      do while (index(whitespaces, input(back:back)) /= 0)
          back = back - 1
       enddo
 
@@ -33,9 +32,9 @@ contains
       integer                      :: int
       integer                      :: stat
 
-      read(str, *, iostat=stat) int
-      if(stat /= 0) then
-         write(*, *) "str reading failed", str
+      read (str, *, iostat=stat) int
+      if (stat /= 0) then
+         write (*, *) "str reading failed", str
          stop 9
       endif
    end function str2int
@@ -45,8 +44,8 @@ contains
       integer, intent(in)            :: num
       character(len=:), allocatable  :: ret_str
 
-      allocate(character(100) :: ret_str)
-      write(ret_str, *) num
+      allocate (character(100) :: ret_str)
+      write (ret_str, *) num
       ret_str = strip(ret_str)
    end function int2str_int4
 
@@ -55,8 +54,8 @@ contains
       integer(8), intent(in)            :: num
       character(len=:), allocatable  :: ret_str
 
-      allocate(character(100) :: ret_str)
-      write (ret_str,*) num
+      allocate (character(100) :: ret_str)
+      write (ret_str, *) num
       ret_str = strip(ret_str)
    end function int2str_int8
 
@@ -65,11 +64,11 @@ contains
       real, intent(in)               :: num
       character(len=:), allocatable  :: ret_str
 
-      allocate(character(100) :: ret_str)
-      if(num >= 1e-1 .and. num <= 1e4) then
-         write(ret_str, "(F10.5)") num
+      allocate (character(100) :: ret_str)
+      if (num >= 1e-1 .and. num <= 1e4) then
+         write (ret_str, "(F10.5)") num
       else
-         write(ret_str, "(ES12.4)") num
+         write (ret_str, "(ES12.4)") num
       endif
       ret_str = strip(ret_str)
    end function float2str
@@ -82,14 +81,14 @@ contains
       character(len=:), allocatable          :: out_str
 
       out_str = base
-      if(present(iter)) out_str = out_str // "_it=" // int2str(iter)
-      if(present(kpt))  out_str = out_str // "_kpt=" // int2str(kpt)
+      if (present(iter)) out_str = out_str//"_it="//int2str(iter)
+      if (present(kpt)) out_str = out_str//"_kpt="//int2str(kpt)
 
-      if(present(ext)) then
-         if(ext(1:1) == ".") then
-            out_str = out_str // ext
+      if (present(ext)) then
+         if (ext(1:1) == ".") then
+            out_str = out_str//ext
          else
-            out_str = out_str // "." // ext
+            out_str = out_str//"."//ext
          endif
       endif
    end function gen_filename
@@ -101,8 +100,29 @@ contains
 
       outs = s; nt = LEN_TRIM(text); nr = LEN_TRIM(rep)
       DO
-         i = INDEX(outs, text(:nt)); IF(i == 0) EXIT
+         i = INDEX(outs, text(:nt)); IF (i == 0) EXIT
          outs = outs(:i - 1)//rep(:nr)//outs(i + nt:)
       END DO
    end function Replace_Text
+
+   function get_byte_str(byte) result(str)
+      implicit none
+      real, intent(in)              :: byte
+      character(len=:), allocatable :: str
+      integer :: l10
+
+      l10 = floor(log10(byte))
+
+      if (l10 >= 12) then
+         str = float2str(1e-12*byte) //" TB"
+      elseif (l10 >= 9) then
+         str = float2str(1e-9*byte) //" GB"
+      elseif (l10 >= 6) then
+         str = float2str(1e-6*byte) //" MB"
+      elseif (l10 >= 3) then
+         str = float2str(1e-3*byte) //" kB"
+      else
+         str = float2str(byte) //" byte"
+      endif
+   end function get_byte_str
 end module m_juDFT_string
