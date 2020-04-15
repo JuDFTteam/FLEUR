1ac6ef0ab8105b78d78d16e95486066ee6e362fd
janssen <he.janssen@fz-juelich.de>
2020-04-15 12:37:03 +0200

    Fix formatting in tetra_dos

--- dos/tetra_dos.f90
+++ dos/tetra_dos.f90
@@ -28,13 +28,9 @@ MODULE m_tetrados
                         itetra,efermi,voltet,energy,nevk,&
                         ev,qal,g)
 
-!
-!     ..Scalar Arguments ..
-      INTEGER, INTENT(IN)  :: ntype,neigd,ned,lmax
-      INTEGER, INTENT(IN)  :: ntetra,nkpt
-      REAL,    INTENT(IN)  :: efermi
-!     ..
-!     .. Array Arguments ..
+      INTEGER, INTENT(IN)    :: ntype,neigd,ned,lmax
+      INTEGER, INTENT(IN)    :: ntetra,nkpt
+      REAL,    INTENT(IN)    :: efermi
       INTEGER, INTENT(IN)    :: itetra(:,:) !(4,6*nkpt)
       INTEGER, INTENT(IN)    :: nevk(:)     !(nkpt)
       REAL,    INTENT(IN)    :: voltet(:)   !(6*nkpt)
@@ -42,20 +38,17 @@ MODULE m_tetrados
       REAL,    INTENT(IN)    :: qal(:,:,:)  !(lmax*ntype+3,neigd,nkpt)
       REAL,    INTENT(INOUT) :: ev(:,:)     !(neigd,nkpt)
       REAL,    INTENT(OUT)   :: g(:,:)      !(ned,lmax*ntype+3)
-!     ..
-!     .. Local Variables ..
-      INTEGER i,j,neig,nk,ntp,ne,ns,nc,ntet
-      REAL    ener,efer,w
-!     ..
-!     .. Local Arrays ..
-      REAL weight(4),eval(4),ecmax(neigd),term(ned)
-      REAL wpar(4,ntype,neigd,nkpt),wparint(neigd,nkpt)
+
+      INTEGER :: i,j,neig,nk,ntp,ne,ns,nc,ntet
+      REAL    :: ener,efer,w
+      REAL    :: weight(4),eval(4),ecmax(neigd),term(ned)
+      REAL    :: wpar(4,ntype,neigd,nkpt),wparint(neigd,nkpt)
 
       DO nk = 1,nkpt
          ev(nevk(nk)+1:neigd,nk) = 1.0e10
       ENDDO
-      wpar(:,:,:,:) = 0.0
-      wparint=0.0
+      wpar = 0.0
+      wparint = 0.0
 
       DO neig = 1,neigd
          ecmax(neig) = -1.0e25
@@ -63,96 +56,92 @@ MODULE m_tetrados
             IF ( ev(neig,nk).GT.ecmax(neig) ) ecmax(neig) = ev(neig,nk)
          ENDDO
       ENDDO
-!
-!  check for energy degeneracies in tetrahedrons
-!
+      !
+      !  check for energy degeneracies in tetrahedrons
+      !
       DO ntet = 1,ntetra
-        DO neig = 1,neigd
-          DO i = 1,3
-            DO j = i+1,4
-              IF (abs(ev(neig,itetra(i,ntet)) -&
-                      ev(neig,itetra(j,ntet))).LT.1.0e-7) THEN
-                   ev(neig,itetra(i,ntet))=&
-                   ev(neig,itetra(i,ntet))+i*(1.e-7)*ntet
-                   ev(neig,itetra(j,ntet))=&
-                   ev(neig,itetra(j,ntet))-i*(1.e-7)*ntet
-              ENDIF
+         DO neig = 1,neigd
+            DO i = 1,3
+               DO j = i+1,4
+                  IF (abs(ev(neig,itetra(i,ntet))-ev(neig,itetra(j,ntet))).LT.1.0e-7) THEN
+                     ev(neig,itetra(i,ntet)) = ev(neig,itetra(i,ntet)) + i*(1.0e-7)*ntet
+                     ev(neig,itetra(j,ntet)) = ev(neig,itetra(j,ntet)) - i*(1.0e-7)*ntet
+                  ENDIF
+               ENDDO
             ENDDO
-          ENDDO
-        ENDDO
+         ENDDO
       ENDDO
-      WRITE (*,*) 'in tetra_dos'  ! do not remove  this statement !
-!
-! calculate partial weights
-!
+
+      WRITE (*,*) 'in tetra_dos'  ! do not remove  this statement
+
+      !
+      ! calculate partial weights
+      !
       DO nk=1,nkpt
         DO neig = 1,nevk(nk)
           DO ntp = 1,ntype
             nc = lmax*(ntp-1)
 
             DO ntet = 1,ntetra
-              IF (ALL(itetra(:,ntet).ne.nk)) CYCLE
+               IF (ALL(itetra(:,ntet).ne.nk)) CYCLE
 
-              eval(1:4) = ev(neig,itetra(1:4,ntet))
+               eval(1:4) = ev(neig,itetra(1:4,ntet))
 
-              IF (max(eval(1),eval(2),eval(3),eval(4)).LT.9999.9) THEN
-                DO i=1,4
-                  weight(i)=1.0
-                  DO j=1,4
-                    IF (i.NE.j) weight(i)=weight(i)*(eval(j)-eval(i))
+               IF (max(eval(1),eval(2),eval(3),eval(4)).LT.9999.9) THEN
+                  DO i=1,4
+                     weight(i)=1.0
+                     DO j=1,4
+                        IF (i.NE.j) weight(i)=weight(i)*(eval(j)-eval(i))
+                     ENDDO
+                     weight(i)=6.0*voltet(ntet)/weight(i)
+                     DO ns=1,4
+                        wpar(ns,ntp,neig,itetra(i,ntet)) =  wpar(ns,ntp,neig,itetra(i,ntet)) &
+                                                          + 0.25*weight(i)*qal(nc+ns,neig,nk)
+                     ENDDO
+                     IF (ntp.EQ.1) wparint(neig,itetra(i,ntet)) =  wparint(neig,itetra(i,ntet)) &
+                                                                 + 0.25*weight(i)*qal(lmax*ntype+1,neig,nk)
                   ENDDO
-                  weight(i)=6.0*voltet(ntet)/weight(i)
-                  DO ns=1,4
-                    wpar(ns,ntp,neig,itetra(i,ntet)) =&
-                    wpar(ns,ntp,neig,itetra(i,ntet)) +&
-                     0.25*weight(i)*qal(nc+ns,neig,nk)
-                  ENDDO
-                  IF (ntp.EQ.1) wparint(neig,itetra(i,ntet)) = &
-                                wparint(neig,itetra(i,ntet)) + &
-                      0.25*weight(i)*qal(lmax*ntype+1,neig,nk)
-                ENDDO
-              ENDIF
+               ENDIF
 
             ENDDO
           ENDDO
         ENDDO
       ENDDO
-!
-!---------------------------------------------------
-! evaluate d.o.s.
-!---------------------------------------------------
-!
-      g(:,:) = 0.0
+      !
+      !---------------------------------------------------
+      ! evaluate d.o.s.
+      !---------------------------------------------------
+      !
+      g = 0.0
 
       DO nk = 1,nkpt
-        DO neig = 1,neigd
+         DO neig = 1,neigd
 
-          ener = ev(neig,nk)
-          DO ntp = 1,ntype
-            DO ns = 1,lmax
-              nc = ns + lmax*(ntp-1)
-              w  = 0.5*wpar(ns,ntp,neig,nk)
-              DO ne = 1,ned
-                term(ne) = energy(ne) - ener
-                IF ( energy(ne).GT.ecmax(neig) )&
-                      term(ne) = ecmax(neig) - ener
-                IF ( term(ne).LT.0.0e0 ) term(ne) = 0.0e0
-                g(ne,nc) = g(ne,nc) + w * term(ne)**2
-              ENDDO
+            ener = ev(neig,nk)
+            DO ntp = 1,ntype
+               DO ns = 1,lmax
+                  nc = ns + lmax*(ntp-1)
+                  w  = 0.5*wpar(ns,ntp,neig,nk)
+                  DO ne = 1,ned
+                     term(ne) = energy(ne) - ener
+                     IF(energy(ne).GT.ecmax(neig)) term(ne) = ecmax(neig) - ener
+                     IF(term(ne).LT.0.0e0)         term(ne) = 0.0e0
+                     g(ne,nc) = g(ne,nc) + w * term(ne)**2
+                  ENDDO
+               ENDDO
             ENDDO
-          ENDDO
 
-          nc = lmax*ntype+1
-          w = 0.5*wparint(neig,nk)
-          DO ne = 1,ned
-            term(ne) = energy(ne) - ener
-            IF ( energy(ne).GT.ecmax(neig) ) term(ne) = ecmax(neig)-ener
-            IF ( term(ne).lt.0.0e0 ) term(ne)=0.0e0
-            g(ne,nc) = g(ne,nc) + w * term(ne)**2
-          ENDDO
+            nc = lmax*ntype+1
+            w = 0.5*wparint(neig,nk)
+            DO ne = 1,ned
+               term(ne) = energy(ne) - ener
+               IF(energy(ne).GT.ecmax(neig)) term(ne) = ecmax(neig)-ener
+               IF(term(ne).lt.0.0e0 )        term(ne) = 0.0e0
+               g(ne,nc) = g(ne,nc) + w * term(ne)**2
+            ENDDO
 
-        ENDDO
+         ENDDO
       ENDDO
 
-      END SUBROUTINE tetra_dos
+   END SUBROUTINE tetra_dos
 END MODULE m_tetrados
