63706f254772c158ae3a682311a969dc77f98ffd
Alexander Neukirchen <a.neukirchen@fz-juelich.de>
2020-04-15 19:25:24 +0200

    Minor typos are still devastating.

--- tests/tests_old.cmake
+++ tests/tests_old.cmake
@@ -7,7 +7,7 @@ set(SerialParallelTests #CuBulkXML SiLOXML  Fe_1lXML
    #FePt_film_SSFT FePt_film_SSFT_LO
    #Fe_bcc_GreensFunction GreensFunction_MultiContour
    #Fe_bcc_FlipcdnXLDA Fe_bcc_FlipcdnYGGA FeFFNLOsSOC
-   PlotDenandPot SiFilmPlotXML SiFilmSlicePlotXML vecPlot) #RelaxMTFeature
+   VecPlot)#PlotDenandPot SiFilmPlotXML SiFilmSlicePlotXML ) #RelaxMTFeature
     #Fe_bcc_SF_LDA SmAtomjDOS)
 
 #Currently disabled Tests (Hybrid+Greenfct)
