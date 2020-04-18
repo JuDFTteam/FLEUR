a39c5a8f0c19eaf3694e3bcb7b47e3644846b9ef
Matthias Redies <m.redies@fz-juelich.de>
2020-04-18 11:14:03 +0200

    try to fix issue #432

--- types/types_mpdata.f90
+++ types/types_mpdata.f90
@@ -335,7 +335,7 @@ contains
       real, allocatable :: eig_val(:), eig_vec(:, :)
 
       integer              :: n, size_iwork, info
-      real                 :: size_work
+      real                 :: size_work(1)
       integer, allocatable :: iwork(:)
       real, allocatable    :: work(:)
 
@@ -358,11 +358,11 @@ contains
                   size_work, -1, size_iwork, -1, info)
       if(info /= 0) call juDFT_error("diagonalization for size failed")
 
-      allocate(work(int(size_work)))
+      allocate(work(int(size_work(1))))
       allocate(iwork(size_iwork))
 
       call dsyevd('V', 'U', n, eig_vec, n, eig_val, &
-                  work, int(size_work), iwork, size_iwork, info)
+                  work, int(size_work(1)), iwork, size_iwork, info)
       if(info /= 0) call juDFT_error("diagonalization failed")
       call timestop("diagonalize overlap")
    end subroutine mpdata_diagonialize_olap
