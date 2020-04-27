ff9923d6c6b660bbd54e1c80b5a1d49592573479
Uliana Alekseeva <alek@iff017.iff.kfa-juelich.de>
2020-04-27 13:17:41 +0200

    HAWK updates

--- cmake/machines/HAWK.sh
+++ cmake/machines/HAWK.sh
@@ -1,5 +1,13 @@
-  if  module list 2>&1 | grep -q mpt
-  then
-     export FC=mpif90
-     export CC=mpicc
-  fi
+if  module list 2>&1 | grep -q mpt
+then
+   export FC=mpif90
+   export CC=mpicc
+fi
+
+FLEUR_LIBRARIES="-lxml2;-lmkl_scalapack_lp64;-lmkl_blacs_sgimpt_lp64"
+echo "Wannier is switched off-manually"
+CLI_USE_WANNIER=FALSE
+echo "ChASE is switched off-manually"
+CLI_USE_CHASE=FALSE
+
+
