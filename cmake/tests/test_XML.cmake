53e6bc5b874999bf2ad0ad896a70bf969579e249
Andreas Beckmann <a.beckmann@fz-juelich.de>
2020-04-29 22:56:54 +0200

    drop unused FLEUR_MPI_LIBRARIES

--- cmake/tests/test_XML.cmake
+++ cmake/tests/test_XML.cmake
@@ -14,9 +14,8 @@ try_compile(FLEUR_USE_XML ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/tests/te
             )
        if (FLEUR_USE_XML)
               set(FLEUR_LIBRARIES ${TEST_LIBRARIES} )
-	      set(FLEUR_MPI_LIBRARIES ${LIBXML2_LIBRARIES} ${FLEUR_MPI_LIBRARIES})
        endif()
-endif()       
+endif()
 
 #Try to simply add -lxml2
 if (NOT FLEUR_USE_XML)
@@ -27,7 +26,6 @@ try_compile(FLEUR_USE_XML ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/tests/te
             )
        if (FLEUR_USE_XML)
               set(FLEUR_LIBRARIES -lxml2 ${FLEUR_LIBRARIES})
-              set(FLEUR_MPI_LIBRARIES -lxml2 ${FLEUR_MPI_LIBRARIES})
        endif()
 endif()
 
@@ -49,6 +47,6 @@ message("XML Library found for C:${FLEUR_USE_XML}")
 
 
 if (FLEUR_USE_XML)
-   set(FLEUR_DEFINITIONS ${FLEUR_DEFINITIONS} "CPP_XML") 
+   set(FLEUR_DEFINITIONS ${FLEUR_DEFINITIONS} "CPP_XML")
    set(FLEUR_MPI_DEFINITIONS ${FLEUR_MPI_DEFINITIONS} "CPP_XML")
 endif()
