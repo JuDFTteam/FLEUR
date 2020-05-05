2d895d9e6dceb73cc6f958697fd82c3ebcd5f97e
janssen <he.janssen@fz-juelich.de>
2020-05-05 15:49:29 +0200

    Use the existing sorting routine -cleanup/clearer index names

--- dos/ptdos.f90
+++ dos/ptdos.f90
@@ -1,24 +1,32 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2016 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
 MODULE m_ptdos
    !-------------------------------------------------------------------------
    ! Density of states calculated by linear triangular method
    !-------------------------------------------------------------------------
    USE m_types
+   USE m_tetsrt
 
    IMPLICIT NONE
 
    CONTAINS
 
-   SUBROUTINE ptdos(jspins,ne,ndos,ntb,kpts,ev,qal,e,g)
+   SUBROUTINE ptdos(jspins,ne,ndos,nbands,kpts,ev,qal,eGrid,g)
 
-      INTEGER,       INTENT(IN)  :: ne,jspins,ndos,ntb
+      INTEGER,       INTENT(IN)  :: ne,jspins,ndos,nbands
       TYPE(t_kpts),  INTENT(IN)  :: kpts
-      REAL,          INTENT(IN)  :: qal(:,:,:)  !(ndos,ntb,nkpt)
-      REAL,          INTENT(IN)  :: e(:)
-      REAL,          INTENT(IN)  :: ev(:,:)     !(ntb,nkpt)
+      REAL,          INTENT(IN)  :: qal(:,:,:)  !(ndos,nbands,nkpt)
+      REAL,          INTENT(IN)  :: eGrid(:)    !(ne)
+      REAL,          INTENT(IN)  :: ev(:,:)     !(nbands,nkpt)
       REAL,          INTENT(OUT) :: g(:,:)      !(ne,ndos)
 
-      INTEGER :: i, j, nl, nb, n, nt(3), nc(4)
-      REAL    :: sfac, fa, ec(4)
+      INTEGER :: idos,iBand,itria,iGrid
+      INTEGER :: ind(3),k(3)
+      REAL    :: sfac,fa
+      REAL    :: ei(3)
 
       !
       !     calculate partial densities of states
@@ -29,25 +37,23 @@ MODULE m_ptdos
 
       g = 0.
 
-      DO n = 1 , kpts%ntet
-         fa = sfac*kpts%voltet(n)/kpts%ntet
-         nt(:) = kpts%ntetra(:,n)
-         DO nb = 1 , ntb
-            ec(1:3) = ev(nb,nt(:))
-            nc(1:3) = nt(:)
-            DO i = 1, 2
-               DO j = i+1, 3
-                  IF ( ec(i).GT.ec(j) ) THEN
-                     ec(4) = ec(i) ; ec(i) = ec(j) ; ec(j) = ec(4)
-                     nc(4) = nc(i) ; nc(i) = nc(j) ; nc(j) = nc(4)
-                  ENDIF
-               ENDDO
-            ENDDO
+      DO itria = 1 , kpts%ntet
+         fa = sfac*kpts%voltet(itria)/kpts%ntet
+         k = kpts%ntetra(:,itria)
+         DO iBand = 1 , nbands
+            !---------------------------
+            !eigenvalues at the corners
+            !of the current triangle
+            !---------------------------
+            ei = ev(iBand,k)
 
-            DO nl = 1 , ndos
-               DO i = 1 , ne
-                  g(i,nl) = g(i,nl) + fa* dostet( e(i),ec(1),ec(2),ec(3),&
-                              qal(nl,nb,nc(1)),qal(nl,nb,nc(2)),qal(nl,nb,nc(3)) )
+            !sort in ascending order
+            CALL tetsrt(3,ei,ind)
+
+            DO idos = 1 , ndos
+               DO iGrid = 1 , ne
+                  g(iGrid,idos) = g(iGrid,idos) + fa & 
+                                  * dostet( eGrid(iGrid),ei(ind),qal(idos,iBand,k(ind)) )
                ENDDO
             ENDDO
 
@@ -56,41 +62,47 @@ MODULE m_ptdos
 
    END SUBROUTINE ptdos
 
-   REAL FUNCTION dostet(e,e1,e2,e3,q1,q2,q3)
+   REAL FUNCTION dostet(e,ei,q)
       !
       !     partial density of states for one tetrahedron
       !     note that e1.le.e2.le.e3 is assumed
       !
+      !     ei : eigenvalues at the corners of the triangle (sorted)
+      !     q : partial charges at the corners
 
-      REAL, INTENT(IN) :: e , e1 , e2 , e3 , q1 , q2 , q3
+      REAL, INTENT(IN) :: e      !Energy point where the DOS is calculated
+      REAL, INTENT(IN) :: ei(:)  !(3)
+      REAL, INTENT(IN) :: q(:)   !(3)
 
       REAL :: e21 , e31 , e32 , ee
-      REAL, PARAMETER :: tol = 1.e-6
+      REAL, PARAMETER :: tol = 1e-6
 
-      dostet = 0.
-      IF ( e.LT.e1 ) RETURN
-      IF ( e.LE.e2 ) THEN
+      dostet = 0.0
+      IF ( e.LT.ei(1) ) RETURN
+      IF ( e.LE.ei(2) ) THEN
          !
          !     case 1: e between e1 and e2
          !
-         ee = e - e1
-         e21 = e2 - e1
+         ee = e - ei(1)
+         e21 = ei(2) - ei(1)
          IF ( e21.LT.tol ) RETURN
-         e31 = e3 - e1
+         e31 = ei(3) - ei(1)
          IF ( e31.LT.tol ) RETURN
-         dostet = ee/(e21*e31)*(q1+0.5*(ee/e21*(q2-q1)+ee/e31*(q3-q1)))
-         RETURN
+         dostet = ee/(e21*e31)*(q(1) &
+                                +0.5*(ee/e21*(q(2)-q(1)) &
+                                + ee/e31*(q(3)-q(1))))
       ELSE
-         IF ( e.GT.e3 ) RETURN
+         IF ( e.GT.ei(3) ) RETURN
          !
          !     case 2: e between e2 and e3
          !
-         e31 = e3 - e1
+         e31 = ei(3) - ei(1)
          IF ( e31.LT.tol ) RETURN
-         e32 = e3 - e2
+         e32 = ei(3) - ei(2)
          IF ( e32.LT.tol ) RETURN
-         dostet = (e3-e)/(e31*e32)&
-                  *0.5*(q1+q2+(e-e1)/e31*(q3-q1)+(e-e2)/e32*(q3-q2))
+         dostet = (ei(3)-e)/(e31*e32)*0.5*(q(1)+q(2) &
+                                        +(e-ei(1))/e31*(q(3)-q(1)) &
+                                        +(e-ei(2))/e32*(q(3)-q(2)))
          RETURN
       ENDIF
    END FUNCTION dostet
