c38b89fc934f7a7446e7a0e8d5644af214c9fe04
janssen <he.janssen@fz-juelich.de>
2020-05-05 10:46:43 +0200

    format ptdos

--- dos/ptdos.f90
+++ dos/ptdos.f90
@@ -1,30 +1,31 @@
-      MODULE m_ptdos
-!-------------------------------------------------------------------------
-! Density of states calculated by linear triangular method
-!-------------------------------------------------------------------------
-      CONTAINS
-      SUBROUTINE ptdos(emin,emax,jspins,ne,ndos,ntb,ntria,as,atr,ntriad,&
+MODULE m_ptdos
+   !-------------------------------------------------------------------------
+   ! Density of states calculated by linear triangular method
+   !-------------------------------------------------------------------------
+   IMPLICIT NONE
+
+   CONTAINS
+
+   SUBROUTINE ptdos(emin,emax,jspins,ne,ndos,ntb,ntria,as,atr,ntriad,&
                        itria,nkpt,ev,qal,e,g)
-      IMPLICIT NONE
-!
-! .. Arguments
+
       INTEGER, INTENT (IN) :: ne,ntria,jspins,ntriad,ndos,ntb,nkpt
       INTEGER, INTENT (IN) :: itria(3,ntriad)
       REAL,    INTENT (IN) :: emax,emin,as
       REAL,    INTENT (IN) :: atr(ntriad),qal(ndos,ntb,nkpt)
       REAL,    INTENT (IN) :: e(ne),ev(ntb,nkpt)
       REAL,    INTENT (OUT):: g(ne,ndos)
-!
-! .. Locals
+
       INTEGER :: i, j, nl, nb, n, nt(3), nc(4)
       REAL    :: f, fa, ec(4)
-!
-!     calculate partial densities of states
-!
+
+      !
+      !     calculate partial densities of states
+      !
       f = 2*(3-jspins)/as
-!
+
       g = 0.
-!
+
       DO n = 1 , ntria
          fa = f*atr(n)
          nt(:) = itria(:,n)
@@ -32,44 +33,43 @@
             ec(1:3) = ev(nb,nt(:))
             nc(1:3) = nt(:)
             DO i = 1, 2
-              DO j = i+1, 3
-                IF ( ec(i).GT.ec(j) ) THEN
-                  ec(4) = ec(i) ; ec(i) = ec(j) ; ec(j) = ec(4)
-                  nc(4) = nc(i) ; nc(i) = nc(j) ; nc(j) = nc(4)
-                ENDIF
-              ENDDO
+               DO j = i+1, 3
+                  IF ( ec(i).GT.ec(j) ) THEN
+                     ec(4) = ec(i) ; ec(i) = ec(j) ; ec(j) = ec(4)
+                     nc(4) = nc(i) ; nc(i) = nc(j) ; nc(j) = nc(4)
+                  ENDIF
+               ENDDO
             ENDDO
 
             DO nl = 1 , ndos
-              DO i = 1 , ne
-                g(i,nl) = g(i,nl) + fa* dostet( e(i),ec(1),ec(2),ec(3),&
-                   qal(nl,nb,nc(1)),qal(nl,nb,nc(2)),qal(nl,nb,nc(3)) )
-              ENDDO
+               DO i = 1 , ne
+                  g(i,nl) = g(i,nl) + fa* dostet( e(i),ec(1),ec(2),ec(3),&
+                              qal(nl,nb,nc(1)),qal(nl,nb,nc(2)),qal(nl,nb,nc(3)) )
+               ENDDO
             ENDDO
 
          ENDDO
       ENDDO
-      
-      END SUBROUTINE ptdos
-!-------------------------------------------------------------------------
-      REAL FUNCTION dostet(e,e1,e2,e3,q1,q2,q3)
-!
-!     partial density of states for one tetrahedron
-!     note that e1.le.e2.le.e3 is assumed
-!
-      IMPLICIT NONE
+
+   END SUBROUTINE ptdos
+
+   REAL FUNCTION dostet(e,e1,e2,e3,q1,q2,q3)
+      !
+      !     partial density of states for one tetrahedron
+      !     note that e1.le.e2.le.e3 is assumed
+      !
 
       REAL, INTENT(IN) :: e , e1 , e2 , e3 , q1 , q2 , q3
 
       REAL :: e21 , e31 , e32 , ee
       REAL, PARAMETER :: tol = 1.e-6
- 
+
       dostet = 0.
       IF ( e.LT.e1 ) RETURN
       IF ( e.LE.e2 ) THEN
-!
-!     case 1: e between e1 and e2
-!
+         !
+         !     case 1: e between e1 and e2
+         !
          ee = e - e1
          e21 = e2 - e1
          IF ( e21.LT.tol ) RETURN
@@ -79,9 +79,9 @@
          RETURN
       ELSE
          IF ( e.GT.e3 ) RETURN
-!
-!     case 2: e between e2 and e3
-!
+         !
+         !     case 2: e between e2 and e3
+         !
          e31 = e3 - e1
          IF ( e31.LT.tol ) RETURN
          e32 = e3 - e2
@@ -90,7 +90,7 @@
                   *0.5*(q1+q2+(e-e1)/e31*(q3-q1)+(e-e2)/e32*(q3-q2))
          RETURN
       ENDIF
-      END FUNCTION dostet
+   END FUNCTION dostet
 !
 !-------------------------------------------------------------------------
       END MODULE m_ptdos
