ba81a9a970c57c04068ad2e78038ca54e89313db
Daniel Wortmann <d.wortmann@fz-juelich.de>
2020-05-08 14:40:18 +0200

    Fixes partly also in develop

--- eigen/hlomat.F90
+++ eigen/hlomat.F90
@@ -16,7 +16,7 @@ MODULE m_hlomat
   ! p.kurz sept. 1996
   !***********************************************************************
 CONTAINS
-  SUBROUTINE hlomat(input,atoms,mpi,lapw,ud,tlmplm,sym,cell,noco,nococonv,isp,jsp,&
+  SUBROUTINE hlomat(input,atoms,mpi,lapw,ud,tlmplm,sym,cell,noco,nococonv,jsp,isp,&
        ntyp,na,fjgj,alo1,blo1,clo1, iintsp,jintsp,chi,hmat)
     !
     USE m_hsmt_ab
@@ -94,9 +94,9 @@ CONTAINS
        IF (sym%invsat(na) == 1) invsfct = 2
        !
        !$acc kernels present(hmat,hmat%data_c,hmat%data_c)&
-       !$acc &  copyin(atoms,lapw,ab(:,:,1),tlmplm%h_loc(:,ntyp,jsp,isp),tlmplm,lapw%nv(:),tlmplm%tdulo(:,:,:,jindex),tlmplm%tuloulo(:,:,:,jindex),atoms%rmt(ntyp))&
+       !$acc &  copyin(atoms,lapw,ab(:,:,1),tlmplm%h_loc(:,ntyp,jsp,isp),tlmplm,lapw%nv(:),tlmplm%tdulo(:,:,:,jsp,isp),tlmplm%tuloulo(:,:,:,jsp,isp),atoms%rmt(ntyp))&
        !$acc & create(ax,bx,cx)&
-       !$acc & copyin(lapw%index_lo(:,na),tlmplm%tuulo(:,:,:,jindex),atoms%llo(:,ntyp),atoms%nlo(ntyp),atoms%lnonsph(ntyp),abclo(1:3,:,:,:,1:2))&
+       !$acc & copyin(lapw%index_lo(:,na),tlmplm%tuulo(:,:,:,jsp,isp),atoms%llo(:,ntyp),atoms%nlo(ntyp),atoms%lnonsph(ntyp),abclo(1:3,:,:,:,1:2))&
        !$acc & copyin(ud%us(:,ntyp,isp),ud%uds(:,ntyp,isp),ud%dus(:,ntyp,isp),ud%dulos(:,ntyp,isp),ud,ud%duds(:,ntyp,isp))&
        !$acc & default(none)
        DO lo = 1,atoms%nlo(ntyp)
@@ -124,16 +124,16 @@ CONTAINS
                    !--->                   not their complex conjugates as in hssphn
                    !--->                   and that a,b,alo... are the complex
                    !--->                   conjugates of the a,b...-coefficients
-                   !$OMP PARALLEL DO DEFAULT(none) &
-                   !$OMP& SHARED(ax,bx,cx) &
-                   !$OMP& SHARED(lapw,ab,ab_size,iintsp) &
-                   !$OMP& SHARED(lmp,utu,dtu,utd,dtd,utulo,dtulo)
+                   !CPP_OMP PARALLEL DO DEFAULT(none) &
+                   !CPP_OMP& SHARED(ax,bx,cx) &
+                   !CPP_OMP& SHARED(lapw,ab,ab_size,iintsp) &
+                   !CPP_OMP& SHARED(lmp,utu,dtu,utd,dtd,utulo,dtulo)
                    DO kp = 1,lapw%nv(iintsp)
                       ax(kp) = ax(kp) + ab(kp,lmp,1)*utu + ab(kp,ab_size/2+lmp,1)*dtu
                       bx(kp) = bx(kp) + ab(kp,lmp,1)*utd + ab(kp,ab_size/2+lmp,1)*dtd
                       cx(kp) = cx(kp) + ab(kp,lmp,1)*utulo + ab(kp,ab_size/2+lmp,1)*dtulo
                    END DO
-                   !$OMP END PARALLEL DO
+                   !CPP_OMP END PARALLEL DO
                 END DO
              END DO
              !+t3e
