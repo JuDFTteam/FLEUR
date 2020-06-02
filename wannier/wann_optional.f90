f3d33c4ec9a9ecd489a3334bf4efe2b230efbe71
Frank Freimuth <f.freimuth@fz-juelich.de>
2020-06-02 17:29:32 +0200

    Update wann_optional.f90

--- wannier/wann_optional.f90
+++ wannier/wann_optional.f90
@@ -46,38 +46,50 @@ CONTAINS
 
     !-----generate projection-definition-file
     IF(wann%l_projgen) THEN
+          if(mpi%irank==0)then
        CALL wann_projgen(atoms%ntype,atoms%neq,atoms%nat,atoms%zatom,l_nocosoc,wann)
+              endif
        l_stopopt=.TRUE.
     ENDIF
 
     !-----generate k-point-files
     IF(wann%l_kpointgen) THEN
+          if(mpi%irank==0)then
        CALL wann_kpointgen()
+              endif
        l_stopopt=.TRUE.
     ENDIF
     IF(wann%l_w90kpointgen) THEN
+          if(mpi%irank==0)then
        CALL wann_w90kpointgen()
+              endif
        l_stopopt=.TRUE.
     ENDIF
 
     !-----find Wannier-irreducible part of BZ
     IF(wann%l_kptsreduc)THEN
+          if(mpi%irank==0)then
        CALL wann_kptsreduc(sym%nop,sym%mrot,cell%bmat,sym%tau,input%film, oneD%odi%d1,l_nocosoc)
+              endif
        l_stopopt=.TRUE.
     ENDIF
 
     !-----find Wannier-irreducible part of BZ
     IF(wann%l_kptsreduc2)THEN
+          if(mpi%irank==0)then
        CALL wann_kptsreduc2(wann%mhp, sym%nop,sym%mrot,cell%bmat,sym%tau,input%film, oneD%odi%d1,l_nocosoc)
+              endif
        l_stopopt=.TRUE.
     ENDIF
 
     !-----generate WF1.win and bkpts
     IF(wann%l_prepwan90)THEN
+          if(mpi%irank==0)then
        CALL wann_wan90prep(input,kpts, input%jspins,cell%amat,cell%bmat, atoms%nat,atoms%taual,&
             atoms%zatom,atoms%ntype, atoms%ntype,atoms%neq,wann%l_bzsym,input%film, oneD%odi%d1,&
             wann%l_ms,wann%l_sgwf,wann%l_socgwf, wann%aux_latt_const,wann%param_file,wann%l_dim, &
             wann%wan90version)
+                   endif
     ENDIF
 
     !-----calculate polarization, if not wannierize
@@ -85,16 +97,20 @@ CONTAINS
     IF(wann%l_dipole3.AND..NOT.wann%l_wannierize)THEN
        num_wann(1)=wann%band_max(1)-wann%band_min(1)+1
        num_wann(2)=wann%band_max(2)-wann%band_min(2)+1
+             if(mpi%irank==0)then
        CALL wann_dipole3(input%jspins,cell%omtil,atoms%nat,atoms%pos, cell%amat,cell%bmat,atoms%taual,&
             num_wann, atoms%ntype,atoms%neq,atoms%zatom,l_nocosoc)
+                   endif
        l_stopopt=.TRUE.
     ENDIF
 
     !-----calculate polarization, if not wannierize
     !-----if wannierize, then calculate polarization later (after wannierize)
     IF(wann%l_dipole.AND..NOT.wann%l_wannierize)THEN
+          if(mpi%irank==0)then
        CALL wann_dipole(input%jspins,cell%omtil,atoms%nat,atoms%pos, cell%amat,atoms%ntype,&
             atoms%neq,atoms%zatom)
+                   endif
        l_stopopt=.TRUE.
     ENDIF
 
@@ -120,9 +136,9 @@ CONTAINS
       wann%l_hsomtxvec_to_lmpzsoc_unf.or. &
       wann%l_hsomtxvec_to_lmpzsoc.or. &
       wann%l_hsomtxvec_unf_to_lmpzsoc)then
-
+      if(mpi%irank==0)then
          call wann_convert_fleur_w90(input%jspins,l_nocosoc,wann)
-
+       endif
          l_stopopt=.true.
     ENDIF
 
