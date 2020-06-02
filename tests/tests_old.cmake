5e3c89793de7a2a86ca66a34dfaac66d75a6ddf7
Gregor Michalicek <g.michalicek@fz-juelich.de>
2020-06-02 18:47:32 +0200

    Add test for force relaxation with BFGS
    The relaxation was not yet tested, only the forces were. There
    was a bug that was not found for a long time due to this.

--- tests/tests_old.cmake
+++ tests/tests_old.cmake
@@ -2,7 +2,7 @@ enable_testing()
 
 set(SerialParallelTests CuBulkXML SiLOXML  Fe_1lXML
    CuBandXML  CuDOSXML  Fe_bctXML  PTOXML Fe_1l_SOCXML
-   PTO-SOCXML  Fe_bct_SOCXML Fe_fccXML GaAsMultiUForceXML
+   PTO-SOCXML  Fe_bct_SOCXML Fe_fccXML GaAsMultiUForceXML H2ORelaxBFGS
    CoMCDXML  Fe_Kerker Fe_bct_LOXML SiFilmPlotXML SiFilmSlicePlotXML
    FePt_film_SSFT FePt_film_SSFT_LO
    Fe_bcc_GreensFunction GreensFunction_MultiContour Fe_1l_GreensFunction
