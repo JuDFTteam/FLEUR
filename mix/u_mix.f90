2f31be4bc7efaaa3dfafb9a028ff90e42b8e784f
janssen <he.janssen@fz-juelich.de>
2020-06-09 12:57:42 +0200

    Cleanup u_mix  -Remove dims and correct wrong allocate statement

--- mix/u_mix.f90
+++ mix/u_mix.f90
@@ -5,36 +5,37 @@
 !--------------------------------------------------------------------------------
 
 MODULE m_umix
-  USE m_juDFT
   !
   ! mix the old and new density matrix for the lda+U method
   !                                                 gb.2001
   ! --------------------------------------------------------
   ! Extension to multiple U per atom type by G.M. 2017
-CONTAINS
+  USE m_juDFT
+  USE m_types
+  USE m_constants
+  USE m_nmat_rot
+  USE m_xmlOutput
+
+  IMPLICIT NONE
+
+  CONTAINS
+
   SUBROUTINE u_mix(input,atoms,noco,n_mmp_in,n_mmp_out)
 
-    USE m_types
-    USE m_constants
-    USE m_cdn_io
-    USE m_nmat_rot
-    USE m_xmlOutput
+    TYPE(t_input),INTENT(IN)    :: input
+    TYPE(t_atoms),INTENT(IN)    :: atoms
+    TYPE(t_noco), INTENT(IN)    :: noco
+    COMPLEX,      INTENT(INOUT) :: n_mmp_out(-lmaxU_const:,-lmaxU_const:,:,:)
+    COMPLEX,      INTENT(INOUT) :: n_mmp_in (-lmaxU_const:,-lmaxU_const:,:,:)
 
-    ! ... Arguments
 
-    IMPLICIT NONE
-    TYPE(t_input),INTENT(IN)   :: input
-    TYPE(t_atoms),INTENT(IN)   :: atoms
-    TYPE(t_noco),INTENT(IN)   :: noco
-    COMPLEX, INTENT (INOUT)    :: n_mmp_out(-3:3,-3:3,atoms%n_u,MERGE(3,input%jspins,noco%l_mperp))
-    COMPLEX, INTENT (INOUT)    :: n_mmp_in (-3:3,-3:3,atoms%n_u,MERGE(3,input%jspins,noco%l_mperp))
-    !
-    ! ... Locals ...
-    INTEGER j,k,iofl,l,itype,ios,i_u,jsp
-    REAL alpha,spinf,gam,del,sum1,sum2,sum3,mix_u, uParam, jParam
-    REAL    zero(atoms%n_u)
+    INTEGER :: j,k,l,itype,i_u,jsp
+    REAL    :: alpha,spinf,gam,del,sum1,sum2,sum3,uParam,jParam
+    REAL    :: zero(atoms%n_u)
+
     CHARACTER(LEN=20)   :: attributes(6)
     COMPLEX,ALLOCATABLE :: n_mmp(:,:,:,:)
+
     !
     ! check for possible rotation of n_mmp
     !
@@ -42,9 +43,8 @@ CONTAINS
     !CALL nmat_rot(zero,-atoms%lda_u%theta,-atoms%lda_u%phi,3,atoms%n_u,input%jspins,atoms%lda_u%l,n_mmp_out)
 
     ! Write out n_mmp_out to out.xml file
-
     CALL openXMLElementNoAttributes('ldaUDensityMatrix')
-    DO jsp = 1, MERGE(3,input%jspins,noco%l_mperp)
+    DO jsp = 1, SIZE(n_mmp_out,4)
        DO i_u = 1, atoms%n_u
           l = atoms%lda_u(i_u)%l
           itype = atoms%lda_u(i_u)%atomType
@@ -65,16 +65,14 @@ CONTAINS
     CALL closeXMLElement('ldaUDensityMatrix')
 
     ! exit subroutine if density matrix does not exist
-    IF(.NOT.ANY(n_mmp_in(:,:,:,:).NE.0.0)) THEN
-       RETURN
-    END IF
+    IF(.NOT.ANY(ABS(n_mmp_in(:,:,1:atoms%n_u,:)).GT.1e-12)) RETURN
 
     IF (input%ldauLinMix) THEN
 
        ! mix here straight with given mixing factors
 
-       ALLOCATE (n_mmp(-3:3,-3:3,MAX(1,atoms%n_u),MERGE(3,input%jspins,noco%l_mperp)))
-       n_mmp = CMPLX(0.0,0.0)
+       ALLOCATE (n_mmp,mold=n_mmp_in)
+       n_mmp = cmplx_0
 
        alpha = input%ldauMixParam
        spinf = input%ldauSpinf
@@ -123,7 +121,7 @@ CONTAINS
           IF(noco%l_mperp) WRITE (oUnit,'(a23,f12.6)') 'n_mmp distance spin 3 =',sum3
        ENDIF
        n_mmp_in = n_mmp
-       DEALLOCATE (n_mmp)
+       DEALLOCATE(n_mmp)
     ELSE ! input%ldauLinMix
 
        ! only calculate distance
@@ -148,13 +146,7 @@ CONTAINS
                 END DO
              END DO
           END DO
-          !DO j=-3,3
-          !   WRITE(oUnit,'(14f12.6)') (n_mmp_in(k,j,1,2),k=-3,3)
-          !END DO
           WRITE (oUnit,'(a23,f12.6)') 'n_mmp distance spin 2 =',sum2
-          !DO j=-3,3
-          !   WRITE(oUnit,'(14f12.6)') (n_mmp_out(k,j,1,2),k=-3,3)
-          !END DO
           IF(noco%l_mperp) THEN
             !Spin off-diagonal
             sum3 = 0.0
