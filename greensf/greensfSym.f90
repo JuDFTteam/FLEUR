62941c6a20089e192a006297738c51d258ac9fd7
janssen <he.janssen@fz-juelich.de>
2020-06-29 14:08:50 +0200

    Add offdiagonal phase in greensfSym

--- greensf/greensfSym.f90
+++ greensf/greensfSym.f90
@@ -23,38 +23,40 @@ MODULE m_greensfSym
       COMPLEX,                      INTENT(IN)     :: im(-lmaxU_const:,-lmaxU_const:,:,:,:)
       TYPE(t_greensfBZintCoeffs),   INTENT(INOUT)  :: greensfBZintCoeffs
 
-      INTEGER imat,iBand
+      INTEGER imat,iBand,jspin
       COMPLEX, ALLOCATABLE :: imSym(:,:,:)
 
       !$OMP parallel default(none) &
       !$OMP shared(ikpt_i,i_elem,natom,l,l_onsite,l_sphavg)&
       !$OMP shared(spin_start,spin_end,sym,atomFactor,phase,im,greensfBZintCoeffs)&
-      !$OMP private(imat,iBand,imSym)
-      ALLOCATE(imSym(-lmaxU_const:lmaxU_const,-lmaxU_const:lmaxU_const,spin_start:spin_end),source=cmplx_0)
-      !$OMP do collapse(2)
-      DO imat = 1, SIZE(im,4)
-         DO iBand = 1, SIZE(im,3)
-            IF(l_onsite) THEN !These rotations are only available for the onsite elements
-               imSym = symMMPmat(im(:,:,iBand,imat,:),sym,natom,l)
-            ELSE
-               imSym = im(:,:,iBand,imat,:)
-            ENDIF
-            IF(l_sphavg) THEN
-               greensfBZintCoeffs%sphavg(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
-                  greensfBZintCoeffs%sphavg(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
-            ELSE IF(imat.EQ.1) THEN
-               greensfBZintCoeffs%uu(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
-                  greensfBZintCoeffs%uu(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
-            ELSE IF(imat.EQ.2) THEN
-               greensfBZintCoeffs%dd(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
-                  greensfBZintCoeffs%dd(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
-            ELSE IF(imat.EQ.3) THEN
-               greensfBZintCoeffs%ud(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
-                  greensfBZintCoeffs%ud(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
-            ELSE IF(imat.EQ.4) THEN
-               greensfBZintCoeffs%du(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
-                  greensfBZintCoeffs%du(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
-            ENDIF
+      !$OMP private(imat,iBand,imSym,jspin)
+      ALLOCATE(imSym(-lmaxU_const:lmaxU_const,-lmaxU_const:lmaxU_const),source=cmplx_0)
+      !$OMP do collapse(3)
+      DO jspin = spin_start,spin_end
+         DO imat = 1, SIZE(im,4)
+            DO iBand = 1, SIZE(im,3)
+               IF(l_onsite) THEN !These rotations are only available for the onsite elements
+                  imSym = symMMPmat(im(:,:,iBand,imat,jspin),sym,natom,l,phase=jspin==3)
+               ELSE
+                  imSym = im(:,:,iBand,imat,jspin)
+               ENDIF
+               IF(l_sphavg) THEN
+                  greensfBZintCoeffs%sphavg(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
+                     greensfBZintCoeffs%sphavg(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
+               ELSE IF(imat.EQ.1) THEN
+                  greensfBZintCoeffs%uu(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
+                     greensfBZintCoeffs%uu(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
+               ELSE IF(imat.EQ.2) THEN
+                  greensfBZintCoeffs%dd(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
+                     greensfBZintCoeffs%dd(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
+               ELSE IF(imat.EQ.3) THEN
+                  greensfBZintCoeffs%ud(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
+                     greensfBZintCoeffs%ud(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
+               ELSE IF(imat.EQ.4) THEN
+                  greensfBZintCoeffs%du(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) = &
+                     greensfBZintCoeffs%du(iBand,:,:,ikpt_i,i_elem,spin_start:spin_end) + atomFactor * phase * imSym
+               ENDIF
+            ENDDO
          ENDDO
       ENDDO
       !$OMP end do
