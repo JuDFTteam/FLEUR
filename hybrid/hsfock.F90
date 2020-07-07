5eef93e7512772b9f0099af61c9cf83687fbf9b8
Matthias Redies <m.redies@fz-juelich.de>
2020-07-07 14:48:54 +0200

    small bugfix

--- hybrid/hsfock.F90
+++ hybrid/hsfock.F90
@@ -158,9 +158,9 @@ CONTAINS
 
          call ex_to_vx(fi, nk, jsp, nsymop, psym, hybdat, lapw, z_k, ex, hybdat%v_x(nk, jsp))
          call hybdat%v_x(nk, jsp)%u2l()
-         hybdat%l_addhf = .True.
       endif
       call hybdat%v_x(nk,jsp)%bcast(0, k_pack%submpi%comm)
+      hybdat%l_addhf = .True.
       CALL timestop("total time hsfock")
    END SUBROUTINE hsfock
 END MODULE m_hsfock
