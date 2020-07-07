a3891c096b9e966416fd3bb348fb9faa760ba743
Daniel Wortmann <d.wortmann@fz-juelich.de>
2020-07-07 10:49:41 +0200

    Small changes to IO of BS

--- dos/types_eigdos.F90
+++ dos/types_eigdos.F90
@@ -178,9 +178,11 @@ subroutine write_dos(eigdos,hdf_id)
     real              :: vkr(3),vkr_prev(3)
     character(len=100)::file
     allocate(kx(kpts%nkpt))
-    DO jspin=1,eigdos%get_spins()
-      write(file,"(a,a,i0)") trim(eigdos%name_of_dos),".",jspin
+    if (eigdos%name_of_dos.ne."Local") &
+      print *,"WARNING, only very basic BS written without HDF"
 
+    DO jspin=1,eigdos%get_spins()
+      write(file,"(a,a,i0)") trim("BS.",jspin
       open(18,file=file)
       ev=eigdos%get_eig()
       kx(1) = 0.0
@@ -190,10 +192,9 @@ subroutine write_dos(eigdos,hdf_id)
         kx(k)=kx(k-1)+ sqrt(dot_product(vkr-vkr_prev,vkr-vkr_prev))
         vkr_prev=vkr
       ENDDO
-      DO i = 1, minval(eigdos%get_neig())
-         DO k = 1, kpts%nkpt
-            write(18,'(2f15.9)') kx(k),ev(i,k,jspin)!-eFermiCorrection
-         ENDDO
+      DO k = 1, kpts%nkpt
+        write(18,'(999f15.9)') kx(k),(ev(i,k,jspin),i = 1, minval(eigdos%get_neig()))
+      !-eFermiCorrection
       ENDDO
       CLOSE (18)
     enddo
