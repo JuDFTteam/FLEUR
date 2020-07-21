e9394ff74933dc58c52e996b15ed5785f4a3532b
Frank Freimuth <f.freimuth@fz-juelich.de>
2020-07-21 20:13:58 +0200

    Update fermie.F90

--- fermi/fermie.F90
+++ fermi/fermie.F90
@@ -124,6 +124,7 @@ CONTAINS
     DO jsp = 1,nspins
        DO  k = 1,kpts%nkpt
           IF (fmpi%irank == 0) THEN
+             if(input%eig66(1))CALL read_eig(eig_id,k,jsp,neig=results%neig(k,jsp),eig=results%eig(:,k,jsp))
              WRITE (oUnit,'(a2,3f10.5,f12.6)') 'at',kpts%bk(:,k),kpts%wtkpt(k)
              WRITE (oUnit,'(i5,a14)') results%neig(k,jsp),' eigenvalues :'
              WRITE (oUnit,'(8f12.6)') (results%eig(i,k,jsp),i=1,results%neig(k,jsp))
