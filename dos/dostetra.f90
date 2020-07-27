a215a144dd27d286101d36765e1a2d5480969657
Henning Janßen <he.janssen@fz-juelich.de>
2020-07-27 16:19:18 +0200

    Forgot that this was there for a reason

--- dos/dostetra.f90
+++ dos/dostetra.f90
@@ -17,19 +17,21 @@ MODULE m_dostetra
       REAL,                INTENT(IN)     :: qal(:,:,:)
       REAL,                INTENT(INOUT)  :: g(:,:)
 
-      INTEGER :: ikpt,iBand,ie,ispin,neig
+      INTEGER :: ikpt,iBand,ie,ispin,neig(kpts%nkpt)
       REAL    :: w(size(eMesh),size(qal,1))
 
       g = 0.0
       DO ispin = 1, size(qal,3)
          DO ikpt = 1, kpts%nkpt
             neig=count(eig(:,ikpt,ispin)<1E99)
+         ENDDO
+         DO ikpt = 1, kpts%nkpt
             !------------------------------------------------------
             ! Calculate the weights for the DOS on the energy Grid
             !------------------------------------------------------
-            CALL tetrahedronInit(kpts,input,ikpt,eig(:,:,ispin),neig,eMesh,&
+            CALL tetrahedronInit(kpts,input,ikpt,eig(:,:,ispin),MINVAL(neig),eMesh,&
                                  w,dos=.TRUE.)
-            DO iBand = 1, neig
+            DO iBand = 1, neig(ikpt)
                DO ie = 1, SIZE(eMesh)
                   g(ie,ispin) = g(ie,ispin) + w(ie,iBand) * 2.0/input%jspins * qal(iBand,ikpt,ispin)
                ENDDO
