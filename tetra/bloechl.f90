60e1378f97a4d3a72c57bc1541aa0f10092d48d6
Henning Janßen <he.janssen@fz-juelich.de>
2020-07-28 11:03:39 +0200

    Small fixes for bloechl correction

--- tetra/bloechl.f90
+++ tetra/bloechl.f90
@@ -16,10 +16,12 @@ MODULE m_bloechl
       LOGICAL,             INTENT(IN)     :: film
 
       INTEGER i
+      REAL dos_ef
 
+      dos_ef = dos_tetra(efermi,etetra,vol)
       bloechl = 0.0
       DO i = 1, 4
-         bloechl = bloechl + 1/40.0*dos_tetra(efermi,etetra,vol)*(etetra(i)-etetra(ind))
+         bloechl = bloechl + 1/40.0*dos_ef*(etetra(i)-etetra(ind))
       ENDDO
 
 
@@ -33,18 +35,19 @@ MODULE m_bloechl
 
       IF((energy.GT.etetra(4)).OR.(energy.LT.etetra(1))) THEN
          dos_tetra = 0.0
-      ELSE IF((energy.GT.etetra(1)).AND.(energy.LT.etetra(2))) THEN
+      ELSE IF(energy.GE.etetra(3)) THEN
+
+         dos_tetra = 3.0 * vol * (etetra(4)-energy)**2/((etetra(4)-etetra(1))*(etetra(4)-etetra(2))*(etetra(4)-etetra(3)))
+
+      ELSE IF(energy.GE.etetra(2)) THEN
+
+         dos_tetra = 3.0 * vol * 1./((etetra(3)-etetra(1))*(etetra(4)-etetra(1))) * (etetra(2) - etetra(1) + 2*(energy - etetra(2)) &
+               -(etetra(3)-etetra(1)+etetra(4)-etetra(2)) * (energy-etetra(2))**2/((etetra(3)-etetra(2))*(etetra(4)-etetra(2))))
+
+      ELSE IF(energy.GE.etetra(1)) THEN
 
          dos_tetra = 3.0 * vol * (energy-etetra(1))**2/((etetra(2)-etetra(1))*(etetra(3)-etetra(1))*(etetra(4)-etetra(1)))
 
-      ELSE IF((energy.GT.etetra(2)).AND.(energy.LT.etetra(3))) THEN
-
-         dos_tetra = 3.0 * vol * 1./((etetra(3)-etetra(1))*(etetra(4)-etetra(1))) * (etetra(2) - etetra(1) + 2*(energy - etetra(2)) -&
-               (etetra(3)-etetra(1)+etetra(4)-etetra(2)) * (energy-etetra(2))**2/((etetra(3)-etetra(2))*(etetra(4)-etetra(2))))
-
-      ELSE IF((energy.GT.etetra(3)).AND.(energy.LT.etetra(4))) THEN
-
-         dos_tetra = 3.0 * vol * (etetra(4)-energy)**2/((etetra(4)-etetra(1))*(etetra(4)-etetra(2))*(etetra(4)-etetra(3)))
       END IF
 
    END FUNCTION dos_tetra
