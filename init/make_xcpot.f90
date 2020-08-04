53670862317ac220a0c7548bcd5aa2e84cc774a1
Matthias Redies <m.redies@fz-juelich.de>
2020-08-04 12:39:00 +0200

    fomat make_xcpot

--- init/make_xcpot.f90
+++ init/make_xcpot.f90
@@ -5,47 +5,49 @@
 !--------------------------------------------------------------------------------
 
 MODULE m_make_xcpot
-  use m_juDFT
-  implicit none
-  private
-  public make_xcpot
+   use m_juDFT
+   implicit none
+   private
+   public make_xcpot
 
 contains
-  subroutine make_xcpot(xcpot,atoms,input)
-    use m_types_xcpot
-    use m_types_atoms
-    use m_types_input
-    USE m_types_xcpot_libxc
-    !USE m_types_xcpot_inbuild
-    USE m_types_xcpot_inbuild_nofunction
+   subroutine make_xcpot(xcpot, atoms, input)
+      use m_types_xcpot
+      use m_types_atoms
+      use m_types_input
+      USE m_types_xcpot_libxc
+      !USE m_types_xcpot_inbuild
+      USE m_types_xcpot_inbuild_nofunction
 
-    TYPE(t_input),    INTENT(IN)     :: input
-    TYPE(t_atoms),    INTENT(IN)     :: atoms
-    CLASS(t_xcpot),ALLOCATABLE,INTENT(INOUT) :: xcpot
+      TYPE(t_input), INTENT(IN)     :: input
+      TYPE(t_atoms), INTENT(IN)     :: atoms
+      CLASS(t_xcpot), ALLOCATABLE, INTENT(INOUT) :: xcpot
 
+      INTEGER              ::func_vxc_id_c, func_vxc_id_x, func_exc_id_c, func_exc_id_x
 
-    INTEGER              ::func_vxc_id_c,func_vxc_id_x,func_exc_id_c,func_exc_id_x
+      !Finish setup of xcpot
+      xcpot%l_libxc = (xcpot%inbuild_name == "LibXC")
+      IF (xcpot%l_libxc) THEN
+         write (*,*) "func_ids", xcpot%func_vxc_id_c, xcpot%func_vxc_id_x, xcpot%func_exc_id_c, xcpot%func_exc_id_x
 
-    !Finish setup of xcpot
-    IF (xcpot%l_libxc) THEN
-       func_vxc_id_c=xcpot%func_vxc_id_c
-       func_vxc_id_x=xcpot%func_vxc_id_x
-       func_exc_id_c=xcpot%func_exc_id_c
-       func_exc_id_x=xcpot%func_exc_id_x
-       DEALLOCATE(xcpot)
-       ALLOCATE(t_xcpot_libxc::xcpot)
-       SELECT TYPE(xcpot)
-       CLASS is (t_xcpot_libxc)!just allocated like this
-          CALL xcpot%init(func_vxc_id_x,func_vxc_id_c,func_exc_id_x,func_exc_id_c,input%jspins)
-       END SELECT
-    ELSE
-       SELECT TYPE(xcpot)
-       CLASS is (t_xcpot_inbuild_nf)
-          Call Xcpot%Init(Atoms%Ntype)
-       CLASS DEFAULT
-          CALL judft_error("Error in setup xcpot")
-       END SELECT
-    END IF
+         func_vxc_id_c = xcpot%func_vxc_id_c
+         func_vxc_id_x = xcpot%func_vxc_id_x
+         func_exc_id_c = xcpot%func_exc_id_c
+         func_exc_id_x = xcpot%func_exc_id_x
+         DEALLOCATE (xcpot)
+         ALLOCATE (t_xcpot_libxc::xcpot)
+         SELECT TYPE (xcpot)
+         CLASS is (t_xcpot_libxc)!just allocated like this
+            CALL xcpot%init(func_vxc_id_x, func_vxc_id_c, func_exc_id_x, func_exc_id_c, input%jspins)
+         END SELECT
+      ELSE
+         SELECT TYPE (xcpot)
+         CLASS is (t_xcpot_inbuild_nf)
+            Call Xcpot%Init(Atoms%Ntype)
+         CLASS DEFAULT
+            CALL judft_error("Error in setup xcpot")
+         END SELECT
+      END IF
 
-end subroutine make_xcpot
+   end subroutine make_xcpot
 end MODULE m_make_xcpot
