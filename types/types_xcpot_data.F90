c71fa4700be0b50a97ba694c9ae3813e0658e4c8
Matthias Redies <m.redies@fz-juelich.de>
2020-08-05 11:25:23 +0200

    fix format

--- types/types_xcpot_data.F90
+++ types/types_xcpot_data.F90
@@ -4,23 +4,23 @@
 ! of the MIT license as expressed in the LICENSE file in more detail.
 !--------------------------------------------------------------------------------
 MODULE m_types_xcpot_data
-  !This module contains the xcpot-type used for the in-build xc-implementations
-  IMPLICIT NONE
+   !This module contains the xcpot-type used for the in-build xc-implementations
+   IMPLICIT NONE
 
-  TYPE t_xcpot_data
-     !in the pbe case (exchpbe.F) lots of test are made
-     !in addition some constants are set
-     !to speed up this code precalculate things in init
-     LOGICAL             :: is_rpbe = .false. !Rpbe
-     LOGICAL             :: is_wc   = .false.
-     LOGICAL             :: is_hse  = .false. !hse,lhse,vhse
-     REAL                :: uk,um
-     !many logicals to determine xcpot
-     LOGICAL             :: is_pbes = .false.!is pbe-sol
-     LOGICAL             :: is_pbe0 = .false.
-     LOGICAL             :: is_bh   = .false.
-     LOGICAL             :: is_mjw  = .false.
-     REAL                :: exchange_weight
-     INTEGER             :: krla !relativistic corrections
-  END TYPE t_xcpot_data
+   TYPE t_xcpot_data
+      !in the pbe case (exchpbe.F) lots of test are made
+      !in addition some constants are set
+      !to speed up this code precalculate things in init
+      LOGICAL             :: is_rpbe = .false. !Rpbe
+      LOGICAL             :: is_wc = .false.
+      LOGICAL             :: is_hse = .false. !hse,lhse,vhse
+      REAL                :: uk, um
+      !many logicals to determine xcpot
+      LOGICAL             :: is_pbes = .false.!is pbe-sol
+      LOGICAL             :: is_pbe0 = .false.
+      LOGICAL             :: is_bh = .false.
+      LOGICAL             :: is_mjw = .false.
+      REAL                :: exchange_weight
+      INTEGER             :: krla !relativistic corrections
+   END TYPE t_xcpot_data
 END MODULE m_types_xcpot_data
