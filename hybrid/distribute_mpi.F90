70110bdce55ffb72a0295c57bc4a1cdb578a9c48
Matthias Redies <m.redies@fz-juelich.de>
2020-10-12 11:35:18 +0200

    create new distrib mpi routine

---
+++ hybrid/distribute_mpi.F90
@@ -0,0 +1,32 @@
+module m_distribute_mpi 
+   use m_types
+contains 
+   subroutine distribute_mpi(weights, glob_mpi, group_mpi, group_rank)
+      implicit none 
+      integer, intent(in)           :: weights(:)
+      type(t_hybmpi), intent(in)    :: glob_mpi
+      type(t_hybmpi), intent(inout) :: group_mpi
+      integer, intent(inout)        :: group_rank 
+
+      integer, allocatable :: nprocs(:), color(:)
+      integer              :: idx(1), i, j, cnt, n_grps
+
+
+      n_grps = size(weights)
+      allocate(nprocs(n_grps), source=0)
+      allocate(color(glob_mpi%size), source=0)
+         
+      do i = 1,glob_mpi%size 
+         idx = minloc(1.0*nprocs/weights)
+         nprocs(idx(1)) = nprocs(idx(1)) + 1
+      enddo
+
+      cnt = 1
+      do i = 1,n_grps
+         do j = 1,nprocs(i)
+            color(cnt) = i - 1
+            cnt = cnt + 1 
+         enddo 
+      enddo
+   end subroutine distribute_mpi
+end module m_distribute_mpi
