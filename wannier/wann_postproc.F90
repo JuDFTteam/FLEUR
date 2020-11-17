37d0699239156a90757d817dd4b760ab752120b6
Frank Freimuth <f.freimuth@fz-juelich.de>
2020-11-17 16:24:50 +0100

    Update wann_postproc.F90

--- wannier/wann_postproc.F90
+++ wannier/wann_postproc.F90
@@ -367,7 +367,7 @@ CONTAINS
             rvecnum,rvec,kpoints,&
             input%jspins,fullnkpts,wann%l_bzsym,input%film,oneD%odi%d1,&
             (noco%l_soc.or.noco%l_noco),wann%band_min,wann%band_max,&
-            input%neigd,.false.,atoms%ntype,atoms%neq,wann%l_ndegen,ndegen,&
+            input%neig,.false.,atoms%ntype,atoms%neq,wann%l_ndegen,ndegen,&
             wann%wan90version,wann%l_unformatted)
      ENDIF
 
