1116cd9303176e1b80b5f116f8155fe27a2487da
Henning Janßen <he.janssen@fz-juelich.de>
2020-12-03 16:11:35 +0100

    Deactivate this routine

--- greensf/greensfPostProcess.F90
+++ greensf/greensfPostProcess.F90
@@ -173,17 +173,17 @@ MODULE m_greensfPostProcess
                                   sphhar,atoms,sym,noco,nococonv,input,f,g,flo,atomType,torgue,vTot)
             ENDDO
             CALL closeXMLElement('noncollinearTorgue')
-            IF(noco%l_soc) THEN
-               CALL openXMLElementNoAttributes('spinorbitTorgue')
-               WRITE(oUnit,'(/,A)') 'Torgue Calculation (spin-orbit):'
-               WRITE(oUnit,'(/,A)') '---------------------------------'
-               DO atomType = 1, atoms%nType
-                  IF(gfinp%numTorgueElems(atomType)==0) CYCLE
-                  CALL greensfSOTorgue(greensFunction(gfinp%torgueElem(atomType,:gfinp%numTorgueElems(atomType))),&
-                                       sphhar,atoms,sym,noco,nococonv,input,enpara,mpi,f,g,flo,atomType,torgue,vTot)
-               ENDDO
-               CALL closeXMLElement('spinorbitTorgue')
-            ENDIF
+            !IF(noco%l_soc) THEN
+            !   CALL openXMLElementNoAttributes('spinorbitTorgue')
+            !   WRITE(oUnit,'(/,A)') 'Torgue Calculation (spin-orbit):'
+            !   WRITE(oUnit,'(/,A)') '---------------------------------'
+            !   DO atomType = 1, atoms%nType
+            !      IF(gfinp%numTorgueElems(atomType)==0) CYCLE
+            !      CALL greensfSOTorgue(greensFunction(gfinp%torgueElem(atomType,:gfinp%numTorgueElems(atomType))),&
+            !                           sphhar,atoms,sym,noco,nococonv,input,enpara,mpi,f,g,flo,atomType,torgue,vTot)
+            !   ENDDO
+            !   CALL closeXMLElement('spinorbitTorgue')
+            !ENDIF
             CALL timestop("Green's Function: Torgue")
          ENDIF
 
