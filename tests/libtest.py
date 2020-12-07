ed8d7721ccb48e0a3f76753e5b59198a6e5dd7ec
Matthias Redies <m.redies@fz-juelich.de>
2020-12-07 11:34:04 +0100

    limit over parallelization

--- tests/libtest.py
+++ tests/libtest.py
@@ -76,7 +76,8 @@ class TestEnv:
       logging.error(text)
 
    def run(self,arg_list):
-      OMP_NUM_THREADS = os.environ['OMP_NUM_THREADS'] if 'OMP_NUM_THREADS' in os.environ else ''
+      os.environ['OMP_NUM_THREADS'] = "1"
+      #OMP_NUM_THREADS = os.environ['OMP_NUM_THREADS'] if 'OMP_NUM_THREADS' in os.environ else ''
       self.log_info(f"Start running command:\nOMP_NUM_THREADS={OMP_NUM_THREADS}\n{arg_list}")
       with open(f"{self.workdir}/stdout", "w") as f_stdout:
          with open(f"{self.workdir}/stderr", "w") as f_stderr:
