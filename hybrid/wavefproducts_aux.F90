217c9565983fcc74d5d55f4d2898d3d3204e109d
Matthias Redies <m.redies@fz-juelich.de>
2021-02-14 13:06:50 +0100

    make hybdat intent in in FFT

--- hybrid/wavefproducts_aux.F90
+++ hybrid/wavefproducts_aux.F90
@@ -18,7 +18,7 @@ CONTAINS
       TYPE(t_nococonv), INTENT(IN)    :: nococonv
       TYPE(t_lapw), INTENT(IN)        :: lapw
       TYPE(t_mpdata), intent(in)      :: mpdata
-      TYPE(t_hybdat), INTENT(INOUT)   :: hybdat
+      TYPE(t_hybdat), INTENT(IN)      :: hybdat
       type(t_stars), intent(in)       :: stars
       type(t_mat), intent(in)         :: z_k
       type(t_mat), intent(inout)      :: z_kqpt_p, cprod
