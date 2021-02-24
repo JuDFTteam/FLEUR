aa2175ae1d33a3313405033080b47a47eb13f309
Matthias Redies <m.redies@fz-juelich.de>
2021-02-24 19:17:49 +0100

    fix serial

--- types/types_mpimat.F90
+++ types/types_mpimat.F90
@@ -400,7 +400,7 @@ CONTAINS
       character(len=*)         :: filename
       type(t_mat) :: tmp 
       integer :: ierr, irank
-
+#ifdef CPP_MPI
       CALL MPI_COMM_RANK(mat%blacsdata%mpi_com, irank, ierr)
       
       if(irank == 0) call tmp%alloc(mat%l_real, mat%global_size1, mat%global_size2)
@@ -411,6 +411,7 @@ CONTAINS
          call tmp%save_npy(filename)
          call tmp%free()
       endif
+#endif
    end subroutine mpimat_save_npy
 
    SUBROUTINE mpimat_move(mat, mat1)
