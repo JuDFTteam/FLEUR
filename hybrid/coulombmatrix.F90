5efd7f5a8e011a53d5ee09b9e736019e9d778088
Matthias Redies <m.redies@fz-juelich.de>
2021-03-01 17:35:38 +0100

    Revert "even more striped coulomb stuff"
    This reverts commit 252e0a1d9dcba1e917bfb287ac7cec1f916c286b.

--- hybrid/coulombmatrix.F90
+++ hybrid/coulombmatrix.F90
@@ -649,68 +649,65 @@ CONTAINS
             rdum = (fpi_const)**(1.5)/fi%cell%vol**2*gmat(1, 1)
 
             call timestart("double gpt loop")
-            DO igpt0 = 1, ngptm1(1)
+            DO igpt0 = 1+fmpi%n_rank, ngptm1(1), fmpi%n_size
                igpt2 = pgptm1(igpt0, 1)
                if(igpt2 /= 1) then
                   ix = hybdat%nbasp + igpt2
-                  call glob_to_loc(fmpi, ix, pe_ix, ix_loc)
-                  if(fmpi%n_rank == pe_ix) then
-                     iqnrm2 = pqnrm(igpt2, 1)
-                     igptp2 = mpdata%gptm_ptr(igpt2, 1)
-                     q2 = MATMUL(mpdata%g(:, igptp2), fi%cell%bmat)
-                     qnorm2 = norm2(q2)
+                  iqnrm2 = pqnrm(igpt2, 1)
+                  igptp2 = mpdata%gptm_ptr(igpt2, 1)
+                  q2 = MATMUL(mpdata%g(:, igptp2), fi%cell%bmat)
+                  qnorm2 = norm2(q2)
 
-                     !$OMP PARALLEL DO default(none) schedule(dynamic) &
-                     !$OMP shared(igpt2, hybdat, fi, pqnrm, mpdata, q2, qnorm2, igptp2) &
-                     !$OMP shared(striped_coul, ix_loc, rdum, sphbesmoment, iqnrm2)&
-                     !$OMP private(igpt1, iy, iqnrm1, igptp1, q1, qnorm1, rdum1, iatm1) &
-                     !$OMP private(itype1, iatm2, itype2, cdum)
-                     DO igpt1 = 2, igpt2
-                        iy = hybdat%nbasp + igpt1
-                        iqnrm1 = pqnrm(igpt1, 1)
-                        igptp1 = mpdata%gptm_ptr(igpt1, 1)
-                        q1 = MATMUL(mpdata%g(:, igptp1), fi%cell%bmat)
-                        qnorm1 = norm2(q1)
-                        rdum1 = dot_PRODUCT(q1, q2)/(qnorm1*qnorm2)
-                        do iatm1 = 1,fi%atoms%nat
-                           itype1 = fi%atoms%itype(iatm1)
-                           do iatm2 = 1,fi%atoms%nat 
-                              itype2 = fi%atoms%itype(iatm2)
-                              cdum = EXP(CMPLX(0.0, 1.0)*tpi_const* &
-                                       (-dot_PRODUCT(mpdata%g(:, igptp1), fi%atoms%taual(:, iatm1)) &
-                                          + dot_PRODUCT(mpdata%g(:, igptp2), fi%atoms%taual(:, iatm2))))
-                              striped_coul(1)%data_c(iy, ix_loc) = striped_coul(1)%data_c(iy, ix_loc) &
-                                             + rdum*cdum*( &
-                                                -sphbesmoment(1, itype1, iqnrm1) &
-                                                *sphbesmoment(1, itype2, iqnrm2)*rdum1/3 &
-                                                - sphbesmoment(0, itype1, iqnrm1) &
-                                                *sphbesmoment(2, itype2, iqnrm2)/6 &
-                                                - sphbesmoment(2, itype1, iqnrm1) &
-                                                *sphbesmoment(0, itype2, iqnrm2)/6 &
-                                                + sphbesmoment(0, itype1, iqnrm1) &
-                                                *sphbesmoment(1, itype2, iqnrm2)/qnorm2/2 &
-                                                + sphbesmoment(1, itype1, iqnrm1) &
-                                                *sphbesmoment(0, itype2, iqnrm2)/qnorm1/2)
-                           END DO
+                  !$OMP PARALLEL DO default(none) schedule(dynamic) &
+                  !$OMP shared(igpt2, hybdat, fi, pqnrm, mpdata, q2, qnorm2, igptp2) &
+                  !$OMP shared(coulomb, ix, rdum, sphbesmoment, iqnrm2)&
+                  !$OMP private(igpt1, iy, iqnrm1, igptp1, q1, qnorm1, rdum1, iatm1) &
+                  !$OMP private(itype1, iatm2, itype2, cdum)
+                  DO igpt1 = 2, igpt2
+                     iy = hybdat%nbasp + igpt1
+                     iqnrm1 = pqnrm(igpt1, 1)
+                     igptp1 = mpdata%gptm_ptr(igpt1, 1)
+                     q1 = MATMUL(mpdata%g(:, igptp1), fi%cell%bmat)
+                     qnorm1 = norm2(q1)
+                     rdum1 = dot_PRODUCT(q1, q2)/(qnorm1*qnorm2)
+                     do iatm1 = 1,fi%atoms%nat
+                        itype1 = fi%atoms%itype(iatm1)
+                        do iatm2 = 1,fi%atoms%nat 
+                           itype2 = fi%atoms%itype(iatm2)
+                           cdum = EXP(CMPLX(0.0, 1.0)*tpi_const* &
+                                    (-dot_PRODUCT(mpdata%g(:, igptp1), fi%atoms%taual(:, iatm1)) &
+                                       + dot_PRODUCT(mpdata%g(:, igptp2), fi%atoms%taual(:, iatm2))))
+                           coulomb(1)%data_c(iy, ix) = coulomb(1)%data_c(iy, ix) + rdum*cdum*( &
+                                             -sphbesmoment(1, itype1, iqnrm1) &
+                                             *sphbesmoment(1, itype2, iqnrm2)*rdum1/3 &
+                                             - sphbesmoment(0, itype1, iqnrm1) &
+                                             *sphbesmoment(2, itype2, iqnrm2)/6 &
+                                             - sphbesmoment(2, itype1, iqnrm1) &
+                                             *sphbesmoment(0, itype2, iqnrm2)/6 &
+                                             + sphbesmoment(0, itype1, iqnrm1) &
+                                             *sphbesmoment(1, itype2, iqnrm2)/qnorm2/2 &
+                                             + sphbesmoment(1, itype1, iqnrm1) &
+                                             *sphbesmoment(0, itype2, iqnrm2)/qnorm1/2)
                         END DO
                      END DO
-                     !$OMP END PARALLEL DO
-                  endif !pe_ix
+                  END DO
+                  !$OMP END PARALLEL DO
                endif
             END DO
             call timestop("double gpt loop")
 
-            call striped_coul(1)%u2l()  
+#ifdef CPP_MPI
+            call timestart("bcast dblgpt loop")
+            do igpt0 = 1, ngptm1(1)
+               root = mod(igpt0 - 1,fmpi%n_size)
+               igpt2 = pgptm1(igpt0, 1)
+               ix = hybdat%nbasp + igpt2
+               call MPI_Bcast(coulomb(1)%data_c(hybdat%nbasp+2,ix), igpt2-1, MPI_DOUBLE_COMPLEX, root, fmpi%sub_comm, ierr)
+            enddo
+            call timestop("bcast dblgpt loop")
+#endif
 
-            SELECT TYPE(striped_coul)
-            CLASS is (t_mpimat)
-               call striped_coul(1)%to_non_dist(coulomb(ikpt))
-               call coulomb(1)%bcast(0, fmpi%sub_comm)
-            CLASS is (t_mat)
-               call coulomb(1)%copy(striped_coul(ikpt), 1,1)
-            CLASS default
-               CALL judft_error("makes no sence")
-            END SELECT
+            call coulomb(1)%u2l()   
 
             ! (2) igpt1 = 1 , igpt2 > 1  (first G vector vanishes, second finite)
             call timestart("igpt1=1 loop")
