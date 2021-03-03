b1854516bc44bca2c673b35599e468b66968be62
Matthias Redies <m.redies@fz-juelich.de>
2021-03-03 16:15:25 +0100

    add more timers

--- hybrid/glob_tofrom_loc.F90
+++ hybrid/glob_tofrom_loc.F90
@@ -1,5 +1,6 @@
 module m_glob_tofrom_loc
     use m_types
+    use m_juDFT
 contains
     subroutine glob_to_loc(fmpi, glob_idx, pe, loc_idx)
         implicit none 
@@ -7,8 +8,10 @@ contains
         integer, intent(in)     :: glob_idx 
         integer, intent(inout)  :: pe, loc_idx 
 
+        call timestart("glob_to_loc")
         pe = mod((glob_idx-1), fmpi%n_size)
         loc_idx = ((glob_idx-1)/fmpi%n_size) +1
+        call timestop("glob_to_loc")
     end subroutine glob_to_loc
 
     subroutine glob_from_loc(fmpi, idx, g)
@@ -17,7 +20,9 @@ contains
         integer, intent(in)     :: idx 
         integer, intent(inout)  :: g 
 
+        call timestart("glob_from_loc")
         g = fmpi%n_size * (idx-1) + fmpi%n_rank + 1 
+        call timestop("glob_from_loc")
     end subroutine glob_from_loc 
 
     subroutine range_to_glob_to_loc(fmpi, glob_range, loc_range)
@@ -28,12 +33,14 @@ contains
 
         integer  :: idx, pe
 
+        call timestart("range_to_glob_to_loc")
         idx = glob_range + 1
         pe = -1
         do while(pe /= fmpi%n_rank)
             idx = idx -1
             call glob_to_loc(fmpi, idx, pe, loc_range)
         enddo
+        call timestop("range_to_glob_to_loc")
     end subroutine range_to_glob_to_loc
 
     subroutine range_from_glob_to_loc(fmpi, glob_from, loc_from)
@@ -44,12 +51,14 @@ contains
         integer, intent(inout)  :: loc_from 
 
         integer :: idx, pe
+
+        call timestart("range_from_glob_to_loc")
         idx = glob_from -1
         pe = -1 
         do while(pe /= fmpi%n_rank)
             idx = idx + 1
             call glob_to_loc(fmpi, idx, pe, loc_from)
         enddo 
+        call timestop("range_from_glob_to_loc")
     end subroutine range_from_glob_to_loc
-        
 end module m_glob_tofrom_loc
