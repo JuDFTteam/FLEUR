8d5c51726adecbcb03ce6db8306689f5980be796
Matthias Redies <m.redies@fz-juelich.de>
2021-03-18 12:42:05 +0100

    use faster copy

--- hybrid/calc_cmt.f90
+++ hybrid/calc_cmt.f90
@@ -107,7 +107,7 @@ contains
       ! write cmt at irreducible k-points in direct-access file cmt
       if(ik <= kpts%nkpt) then
          call timestart("copy out")
-         cmt_out = cmt
+         call zcopy(size(cmt), cmt, 1, cmt_out, 1)
          call timestop("copy out")
       else
          iop = kpts%bksym(ik)
