2cade63c57c335f224a1ef3ec4600fad75386ff9
Matthias Redies <m.redies@fz-juelich.de>
2021-05-13 13:00:55 +0200

    try to export gamma_double_gpt_loop

--- hybrid/coulombmatrix.F90
+++ hybrid/coulombmatrix.F90
@@ -40,6 +40,7 @@ MODULE m_coulombmatrix
    use m_sphbes, only: sphbes
    use m_glob_tofrom_loc
    USE m_trafo, ONLY: symmetrize_mpimat, symmetrize, bramat_trafo
+   use m_gamma_double_gpt_loop
 CONTAINS
 
    SUBROUTINE coulombmatrix(fmpi, fi, mpdata, hybdat, xcpot)
@@ -625,58 +626,9 @@ CONTAINS
             !     Add corrections from higher orders in (3b) to coulomb(:,1)
             ! (1) igpt1 > 1 , igpt2 > 1  (finite G vectors)
             call timestart("add corrections from higher orders")
+            call gamma_double_gpt_loop(fi, fmpi, hybdat, mpdata, sphbesmoment, gmat,  ngptm1, pgptm1, pqnrm, coul(1)) 
+
             rdum = (fpi_const)**(1.5)/fi%cell%vol**2*gmat(1, 1)
-
-            call timestart("double gpt loop")
-            DO igpt0 = 1, ngptm1(1)
-               igpt2 = pgptm1(igpt0, 1)
-               if(igpt2 /= 1) then
-                  ix = hybdat%nbasp + igpt2
-                  call glob_to_loc(fmpi, ix, pe_ix, ix_loc)
-                  if(pe_ix == fmpi%n_rank) then
-                     iqnrm2 = pqnrm(igpt2, 1)
-                     igptp2 = mpdata%gptm_ptr(igpt2, 1)
-                     q2 = MATMUL(mpdata%g(:, igptp2), fi%cell%bmat)
-                     qnorm2 = norm2(q2)
-
-                     !$OMP PARALLEL DO default(none) schedule(dynamic) &
-                     !$OMP shared(igpt2, hybdat, fi, pqnrm, mpdata, q2, qnorm2, igptp2) &
-                     !$OMP shared(coul, ix_loc, rdum, sphbesmoment, iqnrm2)&
-                     !$OMP private(igpt1, iy, iqnrm1, igptp1, q1, qnorm1, rdum1, iatm1) &
-                     !$OMP private(itype1, iatm2, itype2, cdum)
-                     DO igpt1 = 2, igpt2
-                        iy = hybdat%nbasp + igpt1
-                        iqnrm1 = pqnrm(igpt1, 1)
-                        igptp1 = mpdata%gptm_ptr(igpt1, 1)
-                        q1 = MATMUL(mpdata%g(:, igptp1), fi%cell%bmat)
-                        qnorm1 = norm2(q1)
-                        rdum1 = dot_PRODUCT(q1, q2)/(qnorm1*qnorm2)
-                        do iatm1 = 1,fi%atoms%nat
-                           itype1 = fi%atoms%itype(iatm1)
-                           do iatm2 = 1,fi%atoms%nat 
-                              itype2 = fi%atoms%itype(iatm2)
-                              cdum = EXP(CMPLX(0.0, 1.0)*tpi_const* &
-                                       (-dot_PRODUCT(mpdata%g(:, igptp1), fi%atoms%taual(:, iatm1)) &
-                                          + dot_PRODUCT(mpdata%g(:, igptp2), fi%atoms%taual(:, iatm2))))
-                              coul(1)%data_c(iy, ix_loc) = coul(1)%data_c(iy, ix_loc) + rdum*cdum*( &
-                                                -sphbesmoment(1, itype1, iqnrm1) &
-                                                *sphbesmoment(1, itype2, iqnrm2)*rdum1/3 &
-                                                - sphbesmoment(0, itype1, iqnrm1) &
-                                                *sphbesmoment(2, itype2, iqnrm2)/6 &
-                                                - sphbesmoment(2, itype1, iqnrm1) &
-                                                *sphbesmoment(0, itype2, iqnrm2)/6 &
-                                                + sphbesmoment(0, itype1, iqnrm1) &
-                                                *sphbesmoment(1, itype2, iqnrm2)/qnorm2/2 &
-                                                + sphbesmoment(1, itype1, iqnrm1) &
-                                                *sphbesmoment(0, itype2, iqnrm2)/qnorm1/2)
-                           END DO
-                        END DO
-                     END DO
-                     !$OMP END PARALLEL DO
-                  endif !pe_ix
-               endif
-            END DO
-            call timestop("double gpt loop")
 
             ! (2) igpt1 = 1 , igpt2 > 1  (first G vector vanishes, second finite)
             call timestart("igpt1=1 loop")
