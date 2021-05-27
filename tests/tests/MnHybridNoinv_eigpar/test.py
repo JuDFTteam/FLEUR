6d51df83037fb5bbad6a54d9c8447dfafd2aaa2c
Matthias Redies <m.redies@fz-juelich.de>
2021-05-27 13:56:04 +0200

    fix magmom

--- tests/tests/MnHybridNoinv_eigpar/test.py
+++ tests/tests/MnHybridNoinv_eigpar/test.py
@@ -24,11 +24,11 @@ try:
    te.check_value_outfile("HF total energy=", "htr", [-2317.1670995071, -2317.1757499769], 0.000001)
 
    exp_mm1 = 32*[None]
-   exp_mm1[-1] = 2.47449
+   exp_mm1[-1] = 2.47447
    te.check_value_outfile("--> mm       1", " ", exp_mm1, 0.00001)
 
    exp_mm2 = 32*[None]
-   exp_mm2[-1] =-2.47449
+   exp_mm2[-1] =-2.47447
    te.check_value_outfile("--> mm       2", " ", exp_mm2, 0.00001)
 
    # only check the last bandgap
