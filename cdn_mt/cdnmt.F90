2a62b6d3a49c2f7dd016a05b799f18921f276266
Daniel Wortmann <d.wortmann@fz-juelich.de>
2021-06-11 10:30:54 +0200

    Updates for fully-fully noco

--- cdn_mt/cdnmt.F90
+++ cdn_mt/cdnmt.F90
@@ -253,11 +253,11 @@ CONTAINS
                         + denCoeffsOffdiag%dd21(l,itype)*( g(j,1,l,2)*g(j,1,l,1) +g(j,2,l,2)*g(j,2,l,1) )
                    !rho21(j,0,itype) = rho21(j,0,itype)+ conjg(cs)/(atoms%neq(itype)*sfp_const)
                    rho21=CONJG(cs)/(atoms%neq(itype)*sfp_const)
-                   rho(j,0,itype,3)=rho(j,0,itype,3)-REAL(rho21)
-                   rho(j,0,itype,4)=rho(j,0,itype,4)+aimag(rho21)
+                   rho(j,0,itype,3)=rho(j,0,itype,3)+REAL(rho21)
+                   rho(j,0,itype,4)=rho(j,0,itype,4)-aimag(rho21)
                    IF (l.LE.input%lResMax) THEN
-                      moments%rhoLRes(j,0,llp,itype,3) = moments%rhoLRes(j,0,llp,itype,3)- REAL(conjg(cs)/(atoms%neq(itype)*sfp_const))
-                      moments%rhoLRes(j,0,llp,itype,4) = moments%rhoLRes(j,0,llp,itype,4)+ AIMAG(conjg(cs)/(atoms%neq(itype)*sfp_const))
+                      moments%rhoLRes(j,0,llp,itype,3) = moments%rhoLRes(j,0,llp,itype,3)+REAL(conjg(cs)/(atoms%neq(itype)*sfp_const))
+                      moments%rhoLRes(j,0,llp,itype,4) = moments%rhoLRes(j,0,llp,itype,4)-AIMAG(conjg(cs)/(atoms%neq(itype)*sfp_const))
                    END IF
                 ENDDO
              ENDDO
@@ -278,16 +278,15 @@ CONTAINS
                          !rho21(j,lh,itype)= rho21(j,lh,itype)+ CONJG(cs)/atoms%neq(itype)
                          rho21=CONJG(cs)/atoms%neq(itype)
                          rho(j,lh,itype,3)=rho(j,lh,itype,3)+REAL(rho21)
-                         rho(j,lh,itype,4)=rho(j,lh,itype,4)+aimag(rho21)
+                         rho(j,lh,itype,4)=rho(j,lh,itype,4)-aimag(rho21)
                          IF ((l.LE.input%lResMax).AND.(lp.LE.input%lResMax)) THEN
                             moments%rhoLRes(j,lh,llpb,itype,3)= moments%rhoLRes(j,lh,llpb,itype,3) + REAL(conjg(cs)/atoms%neq(itype))
-                            moments%rhoLRes(j,lh,llpb,itype,4)= moments%rhoLRes(j,lh,llpb,itype,4) + AIMAG(conjg(cs)/atoms%neq(itype))
+                            moments%rhoLRes(j,lh,llpb,itype,4)= moments%rhoLRes(j,lh,llpb,itype,4) - AIMAG(conjg(cs)/atoms%neq(itype))
                          END IF
                       ENDDO
                    ENDDO
                 ENDDO
              ENDDO
-
           ENDIF ! denCoeffsOffdiag%l_fmpl
        ENDIF ! noco%l_mperp
 
