016f7082e16180354107445e3fc1987055b81e87
Matthias Redies <m.redies@fz-juelich.de>
2021-06-18 14:24:31 +0200

    add ifdefs for MPI

--- hybrid/exchange_val_hf.F90
+++ hybrid/exchange_val_hf.F90
@@ -188,7 +188,9 @@ CONTAINS
          
          call timestart("q_loop")
          hybdat%max_q = size(k_pack%q_packs)
+#ifdef CPP_MPI
          call MPI_Allreduce(MPI_IN_PLACE, hybdat%max_q, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, ierr)
+#endif
          DO jq = 1, size(k_pack%q_packs)
             call timestart("initial setup")
             iq = k_pack%q_packs(jq)%ptr
@@ -387,12 +389,14 @@ CONTAINS
       !$acc end data ! exch_vv hybdat, hybdat%nbands, hybdat%nbasm, nsest, indx_sest
       call timestop("q_loop")
 
+#ifdef CPP_MPI
       call timestart("balanicing MPI_Barriers")
       do while (hybdat%max_q > 0)
          call MPI_Barrier(MPI_COMM_WORLD, ierr)
          hybdat%max_q = hybdat%max_q - 1
       enddo
       call timestop("balanicing MPI_Barriers")
+#endif
 
       if(allocated(dot_result_r)) deallocate(dot_result_r)
       if(allocated(dot_result_c)) deallocate(dot_result_c)
