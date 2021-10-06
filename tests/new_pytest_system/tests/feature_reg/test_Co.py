6ce141c179cc4d849ee1d8eab982fd5801adf0a6
Stefan Rost <s.rost@fz-juelich.de>
2021-10-06 16:04:10 +0200

    fixing indet for the CoUnfold test

--- tests/new_pytest_system/tests/feature_reg/test_Co.py
+++ tests/new_pytest_system/tests/feature_reg/test_Co.py
@@ -74,7 +74,7 @@ def test_CoUnfold(execute_fleur, grep_exists, grep_number):
     else:
         with_hdf = False
 		
-	if with_hdf:		
+	if with_hdf:
 		#    assert grep_exists(res_files['out'], "it=  1  is completed")
 		assert grep_exists(res_files['bands_sc.1'], "0.91625.*0.94323")
 		assert grep_exists(res_files['bands_sc.1'], "14.03436.*0.039764")
