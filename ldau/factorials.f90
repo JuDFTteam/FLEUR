02db3c2cc1c2b9f55651fb9764879c5690bcee04
Henning Janßen <he.janssen@fz-juelich.de>
2021-11-09 14:33:54 +0100

    Add double factorial

---
+++ ldau/factorials.f90
@@ -0,0 +1,32 @@
+module m_factorials
+
+    implicit none
+
+    contains
+
+    real function doubleFactorial(n)
+
+        !Calculates the double factorial n!!
+
+        integer, intent(in) :: n
+
+        integer upper_bound, k
+
+        if (mod(n,2) == 1) then
+            upper_bound = n+1/2
+        else
+            upper_bound = n/2
+        endif
+
+        doubleFactorial = 1
+        do k = 1, upper_bound
+            if (mod(n,2) == 1) then
+                doubleFactorial = doubleFactorial * (2*k-1)
+            else
+                doubleFactorial = doubleFactorial * 2*k
+            endif
+        enddo
+
+    end function
+
+end module
