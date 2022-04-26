457bb7257fdcc318fc18d3cff1c1584ed650d4e2
Daniel Wortmann <d.wortmann@fz-juelich.de>
2022-04-26 12:05:22 +0200

    Remove unsued loop that prevents compilation

--- juphon/jpSetupDynMat_mod.F90
+++ juphon/jpSetupDynMat_mod.F90
@@ -4209,7 +4209,7 @@ module m_jpSetupDynMat
 !  ---> put stars onto the large fft-grid "tempGrid"
 
       ! NOTE: Upper bound stars%kimax does not exist anymore and was replaced.
-      DO istar=0, (2*stars%mx1+1)* (2*stars%mx2+1)* (2*stars%mx3+1)-1
+      !DO istar=0, (2*stars%mx1+1)* (2*stars%mx2+1)* (2*stars%mx3+1)-1
       ! vEff0IR is already warped and only has to be decorated with the phase for non-symorphic systems
       !   tempGrid(stars%igfft(istar, 2)) = vEff0IR(stars%igfft(istar, 1), 1) * stars%pgfft(istar) ! changed!
       !   tempGridOvl(stars%igfft(istar, 2)) = stars%ustep(stars%igfft(istar, 1)) * stars%pgfft(istar) ! changed!
