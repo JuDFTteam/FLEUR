b044bf98d3c42abb5b557c65357e26e227795955
neukirchen <a.neukirchen@fz-juelich.de>
2022-05-13 14:15:26 +0200

    Another one.

--- juphon/dfpt_eigen.F90
+++ juphon/dfpt_eigen.F90
@@ -73,7 +73,7 @@ CONTAINS
       ! Get the required eigenvectors and values at k for occupied bands:
       bkpt = fi%kpts%bk(:, nk)
 
-      CALL lapw%init(fi%input, fi%noco, nococonv, fi%kpts, fi%atoms, fi%sym, nk, fi%cell, .FALSE., fmpi)
+      CALL lapw%init(fi%input, fi%noco, nococonv, fi%kpts, fi%atoms, fi%sym, nk, fi%cell, fmpi)
 
       noccbd = COUNT(results%w_iks(:,nk,jsp)*2.0/fi%input%jspins>1.e-8)
       nbasfcn = MERGE(lapw%nv(1)+lapw%nv(2)+2*fi%atoms%nlotot,lapw%nv(1)+fi%atoms%nlotot,fi%noco%l_noco)
