e55be1d66d59831831bad1c66823418ce830fa4e
neukirchen <a.neukirchen@fz-juelich.de>
2022-06-15 14:53:57 +0200

    Fixes and beautification in invert_HepsS.

--- juphon/invert_HepsS.F90
+++ juphon/invert_HepsS.F90
@@ -6,59 +6,60 @@
 
 MODULE m_invert_HepsS
 
-    IMPLICIT NONE
+   IMPLICIT NONE
 
 CONTAINS
-    SUBROUTINE invert_HepsS(fmpi, atoms, noco, juPhon, lapwkpr, zMatkpr, eignukpr, eignuk, nekpr, nocck, l_real, invHepsS)
-        ! Subroutine to calculate (H-\epsilon_{\nu k} S)^(-1) as
-        ! z_{k'}(\epsilon_{k'}-\epsilon_{\nu k})^(-1)z_{k'}^H, i.e.
-        ! in the spectral representation.
+   SUBROUTINE invert_HepsS(fmpi, atoms, noco, juPhon, lapwkpr, zMatkpr, eignukpr, eignuk, nekpr, nocck, l_real, invHepsS)
+      !! Subroutine to calculate \((H-\epsilon_{\nu k} S)^{-1}\) as
+      !! \(z_{k'}(\epsilon_{k'}-\epsilon_{\nu k})^(-1)z_{k'}^H\), i.e.
+      !! in the spectral representation.
 
-        USE m_types
-        USE m_types_mpimat
+      USE m_types
+      USE m_types_mpimat
 
-        TYPE(t_mpi),INTENT(IN)       :: fmpi
-        TYPE(t_atoms),   INTENT(IN) :: atoms
-        TYPE(t_noco),    INTENT(IN) :: noco
-        TYPE(t_juPhon),  INTENT(IN) :: juPhon
-        TYPE(t_lapw),    INTENT(IN) :: lapwkpr
-        CLASS(t_mat),    INTENT(IN) :: zMatkpr
+      TYPE(t_mpi),INTENT(IN)       :: fmpi
+      TYPE(t_atoms),   INTENT(IN) :: atoms
+      TYPE(t_noco),    INTENT(IN) :: noco
+      TYPE(t_juPhon),  INTENT(IN) :: juPhon
+      TYPE(t_lapw),    INTENT(IN) :: lapwkpr
+      CLASS(t_mat),    INTENT(IN) :: zMatkpr
 
-        INTEGER,         INTENT(IN) :: nekpr, nocck
-        REAL,            INTENT(IN) :: eignukpr(:), eignuk(:)
-        LOGICAL,         INTENT(IN) :: l_real
+      INTEGER,         INTENT(IN) :: nekpr, nocck
+      REAL,            INTENT(IN) :: eignukpr(:), eignuk(:)
+      LOGICAL,         INTENT(IN) :: l_real
 
-        CLASS(t_mat), ALLOCATABLE, INTENT(OUT) :: invHepsS(:)
+      CLASS(t_mat), ALLOCATABLE, INTENT(OUT) :: invHepsS(:)
 
-        INTEGER :: nbasfcn, nu, iGpr, iG, nupr
-        REAL    :: deps, invdeps
+      INTEGER :: nbasfcn, nu, iGpr, iG, nupr
+      REAL    :: deps, invdeps
 
-        IF (fmpi%n_size == 1) THEN
-          ALLOCATE (t_mat::invHepsS(nocck))
-        ELSE
-          ALLOCATE (t_mpimat::invHepsS(nocck))
-        END IF
+      IF (fmpi%n_size == 1) THEN
+         ALLOCATE (t_mat::invHepsS(nocck))
+      ELSE
+         ALLOCATE (t_mpimat::invHepsS(nocck))
+      END IF
 
-        nbasfcn = MERGE(lapwkpr%nv(1)+lapwkpr%nv(2)+2*atoms%nlotot,lapwkpr%nv(1)+atoms%nlotot,noco%l_noco)
-        DO nu = 1, nocck
-           CALL invHepsS(nu)%init(l_real, nbasfcn, nbasfcn)
-           DO nupr = 1, nekpr
-             deps = eignukpr(nupr)-eignuk(nu)
-             invdeps = 0.0
-             IF (deps.GT.juPhon%eDiffcut) invdeps = 1.0 / deps
-             DO iG = 1, nbasfcn
+      nbasfcn = MERGE(lapwkpr%nv(1)+lapwkpr%nv(2)+2*atoms%nlotot,lapwkpr%nv(1)+atoms%nlotot,noco%l_noco)
+      DO nu = 1, nocck
+         CALL invHepsS(nu)%init(l_real, nbasfcn, nbasfcn)
+         DO nupr = 1, nekpr
+            deps = eignukpr(nupr)-eignuk(nu)
+            IF (ABS(deps)<juPhon%eDiffcut) CYCLE
+            invdeps = 1.0 / deps
+            DO iG = 1, nbasfcn
                DO iGpr = 1, nbasfcn
-                        IF (l_real) THEN
-                            invHepsS(nu)%data_r(iGpr,iG) = zMatkpr%data_r(iGpr, nupr) * &
-                                                         & zMatkpr%data_r(iG, nupr) * invdeps
-                        ELSE
-                            invHepsS(nu)%data_c(iGpr,iG) = zMatkpr%data_c(iGpr, nupr) * &
-                                                   & CONJG(zMatkpr%data_c(iG, nupr)) * invdeps
-                        END IF
-                    END DO
-                END DO
+                  IF (l_real) THEN
+                     invHepsS(nu)%data_r(iGpr,iG) = invHepsS(nu)%data_r(iGpr,iG) &
+                                                  + zMatkpr%data_r(iGpr, nupr) * &
+                                                  & zMatkpr%data_r(iG, nupr) * invdeps
+                  ELSE
+                     invHepsS(nu)%data_c(iGpr,iG) = invHepsS(nu)%data_c(iGpr,iG) &
+                                                  + zMatkpr%data_c(iGpr, nupr) * &
+                                                  & CONJG(zMatkpr%data_c(iG, nupr)) * invdeps
+                  END IF
+               END DO
             END DO
-        END DO
-
-    END SUBROUTINE invert_HepsS
+         END DO
+      END DO
+   END SUBROUTINE invert_HepsS
  END MODULE m_invert_HepsS
