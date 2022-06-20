43684e3b16f111d5c0a0f090cfbdd7063e34b928
Henning Janßen <he.janssen@fz-juelich.de>
2022-06-20 15:56:49 +0200

    USERFRIEND: Do not display offdiagonal DFT+U distance if only l_mperp=T
    The input density matrix always has zeros in the offdiagonal entries,
    since these entries are dismissed during mixing.
    This means the distance in the offdiagonal is essentially meaningless
    and only confusing to users

--- mix/u_mix.f90
+++ mix/u_mix.f90
@@ -82,6 +82,7 @@ MODULE m_umix
          WRITE (oUnit,'(a,f12.6)') 'n_mmp distance =',dist(1)
       ELSE
          DO jsp = 1, SIZE(n_mmp_in,4)
+            if (jsp > 2 .and. .not.any(noco%l_spinoffd_ldau)) cycle
             WRITE (oUnit,9000) 'n_mmp distance spin ',jsp,' =',dist(jsp)
 9000        FORMAT(a,I1,a,f12.6)
          ENDDO
