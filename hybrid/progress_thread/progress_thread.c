b9419beeae060a159638e54a1040ce086ed22de0
Gregor Michalicek <g.michalicek@fz-juelich.de>
2023-06-26 11:13:30 +0200

    Try to fix deadlock
    The deadlock probably occured because while cancelling the thread an MPI call was processed.

--- hybrid/progress_thread/progress_thread.c
+++ hybrid/progress_thread/progress_thread.c
@@ -7,40 +7,28 @@
 
 const long t_pause = 150;
 
-int msleep(long msec){
-   struct timespec ts;
-   int res;
-
-   if (msec < 0){
-      errno = EINVAL;
-      return -1;
-   }
-
-   ts.tv_sec = msec / 1000;
-   ts.tv_nsec = (msec % 1000) * 1000000;
-
-   do {
-      res = nanosleep(&ts, &ts);
-   } while (res && errno == EINTR);
-   return res;
-}
+int endThread;
 
 void * check_mpi(void * arg){
    int flag;
-   while(1){
+   while(endThread==0){
       int flag;
       //MPI_Iprobe(MPI_ANY_SOURCE, 1336, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
       fortran_check_mpi();
-      msleep(t_pause);
+      
+      usleep(1000*t_pause);
    }
 }
 
 void start_prog_thread(pthread_t *threadId){
    // printf("started progress thread. Pause time: %ld\n", t_pause);
+   endThread = 0;
    pthread_create(threadId, NULL, &check_mpi, NULL);
 }
 
 void stop_prog_thread(pthread_t *threadId){
+   endThread= 1;
+   usleep(3*1000*t_pause);
    pthread_cancel(*threadId);
    // printf("stopped progress thread\n");
 }
