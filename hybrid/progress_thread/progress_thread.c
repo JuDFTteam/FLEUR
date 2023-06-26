7712c8853de5b68ad85c174ee7b67980151c6c64
Gregor Michalicek <g.michalicek@fz-juelich.de>
2023-06-26 13:20:48 +0200

    Small cleanup of previous commit

--- hybrid/progress_thread/progress_thread.c
+++ hybrid/progress_thread/progress_thread.c
@@ -13,23 +13,17 @@ void * check_mpi(void * arg){
    int flag;
    while(endThread==0){
       int flag;
-      //MPI_Iprobe(MPI_ANY_SOURCE, 1336, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
       fortran_check_mpi();
-      
       usleep(1000*t_pause);
    }
 }
 
 void start_prog_thread(pthread_t *threadId){
-   // printf("started progress thread. Pause time: %ld\n", t_pause);
    endThread = 0;
    pthread_create(threadId, NULL, &check_mpi, NULL);
 }
 
 void stop_prog_thread(pthread_t *threadId){
    endThread= 1;
-   usleep(3*1000*t_pause);
-   pthread_cancel(*threadId);
-   // printf("stopped progress thread\n");
 }
 
