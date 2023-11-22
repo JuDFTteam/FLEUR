64debdec94f8a2f9bcce95ad2e563f85da4063bc
Thomas Bornhake <bornhake@iff1146.iff.kfa-juelich.de>
2023-11-22 16:08:58 +0100

    Make Checkdopall viable for DFPT

--- global/checkdop.F90
+++ global/checkdop.F90
@@ -1,7 +1,7 @@
       MODULE m_checkdop
       CONTAINS
         SUBROUTINE checkdop(p,np,n,na,ivac,iflag,jsp,atoms,sphhar,stars,sym,&
-                            vacuum,cell,potden)
+                            vacuum,cell,potden,potdenIm)
           ! ************************************************************
           !     subroutines checks the continuity of coulomb           *
           !     potential or valence charge density                    *
@@ -31,7 +31,7 @@
           TYPE(t_vacuum),INTENT(IN)    :: vacuum
           TYPE(t_cell),INTENT(IN)      :: cell
           TYPE(t_potden),INTENT(IN)    :: potden
-
+          TYPE(t_potden),INTENT(IN), OPTIONAL    :: potdenIm
           INTEGER, INTENT (IN) :: iflag,ivac,n,na,np,jsp
           !-odim
           !+odim
@@ -39,15 +39,16 @@
           REAL,    INTENT (IN) :: p(:,:)!(3,DIMENSION%nspd)
           !     ..
           !     .. Local Scalars ..
-          REAL av,dms,rms,s,ir2,help,phi
+          REAL av,dms,rms,s,ir2,help,phi,helpIm
           INTEGER i,j,k,lh,mem,nd,lm,ll1,nopa ,gz,m
           COMPLEX ic
-          LOGICAL l_cdn
+          LOGICAL l_cdn, l_dfpt
           !     ..
           !     .. Local Arrays ..
           COMPLEX sf2(stars%ng2),sf3(stars%ng3),ylm( (atoms%lmaxd+1)**2 )
-          REAL rcc(3),v1(SIZE(p,2)),v2(SIZE(p,2)),x(3),ri(3)
-
+          REAL rcc(3),v1(SIZE(p,2)),v2(SIZE(p,2)),x(3),ri(3), v1Im(SIZE(p,2)), v2Im(SIZE(p,2))
+          l_dfpt = .FALSE.
+          l_dfpt = PRESENT(potdenIm)
           l_cdn = .FALSE. ! By default we assume that the input is a potential.
           IF (potden%potdenType.LE.0) CALL juDFT_error('unknown potden type', calledby='checkdop')
           IF (potden%potdenType.GT.1000) l_cdn = .TRUE. ! potdenTypes > 1000 are reserved for densities
@@ -63,48 +64,100 @@
              !     ---> Interstitial part
              DO j = 1,np
                 rcc=MATMUL(cell%bmat,p(:,j))/tpi_const
-                CALL starf3(sym%nop,stars%ng3,sym%symor,stars%kv3,sym%mrot,sym%tau,p(:,j),sym%invtab,sf3)!keep
-                !
+                IF (l_dfpt) THEN 
+                  CALL starf3(sym%nop,stars%ng3,sym%symor,stars%kv3,sym%mrot,sym%tau,p(:,j),sym%invtab,sf3,stars%center) ! Stars shifted by q
+                ELSE
+                  CALL starf3(sym%nop,stars%ng3,sym%symor,stars%kv3,sym%mrot,sym%tau,p(:,j),sym%invtab,sf3) 
+                END IF
+               !
                 v1(j) = 0.0
+                v1Im(j)=0.0
+                If (l_dfpt) v1Im(j) = 0.0 
                 DO k = 1,stars%ng3
                    v1(j) = v1(j) + REAL(potden%pw(k,jsp)*sf3(k))*stars%nstr(k)
+                   IF (l_dfpt) v1Im(j) = v1Im(j) + AIMAG(potden%pw(k,jsp)*sf3(k))*stars%nstr(k)
                 ENDDO
              ENDDO
              !     ---> vacuum part
              IF (l_cdn) THEN
-                WRITE (oUnit,FMT=9000) ivac
+               IF (l_dfpt) THEN
+                  WRITE (oUnit,FMT=9005) ivac
+                ELSE 
+                  WRITE (oUnit,FMT=9000) ivac
+                END IF
              ELSE
-                WRITE (oUnit,FMT=8000) ivac
+                IF (l_dfpt) THEN
+                  WRITE (oUnit,FMT=8005) ivac
+                ELSE 
+                  WRITE (oUnit,FMT=8000) ivac
+                END IF
              ENDIF
              DO  j = 1,np
-                   CALL starf2(sym%nop2,stars%ng2,stars%kv2,sym%mrot,sym%symor,sym%tau,p(1:3,j),sym%invtab,sf2)!keep
-                   v2(j) = REAL(potden%vac(1,1,ivac,jsp))
-                   DO  k = 2,stars%ng2
-                      v2(j) = v2(j) + REAL(potden%vac(1,k,ivac,jsp)*sf2(k))*stars%nstr2(k)
-                   ENDDO
+                   IF (l_dfpt) THEN
+                        CALL starf2(sym%nop2,stars%ng2,stars%kv2,sym%mrot,sym%symor,sym%tau,p(1:3,j),sym%invtab,sf2,stars%center)
+                   ELSE
+                        CALL starf2(sym%nop2,stars%ng2,stars%kv2,sym%mrot,sym%symor,sym%tau,p(1:3,j),sym%invtab,sf2)
+                   END IF 
+                   v2(j)=0.0
+                   v2Im(j)=0.0
+                   IF ((norm2(stars%center)<1e-8)) THEN 
+                     v2(j) = REAL(potden%vac(1,1,ivac,jsp))
+                     IF (l_dfpt) v2Im(j)=AIMAG(potden%vac(1,1,ivac,jsp)) !(l_dfpt Gamma Point)
+                     DO  k = 2,stars%ng2
+                       v2(j) = v2(j) + REAL(potden%vac(1,k,ivac,jsp)*sf2(k))*stars%nstr2(k)
+                       IF (l_dfpt) v2Im(j) = v2Im(j) + AIMAG(potden%vac(1,k,ivac,jsp)*sf2(k))*stars%nstr2(k)
+                     END DO 
+                   ELSE ! (l_dfpt)
+                     DO  k = 1,stars%ng2
+                        v2(j) = v2(j) + REAL(potden%vac(1,k,ivac,jsp)*sf2(k))*stars%nstr2(k)
+                        v2Im(j) = v2Im(j) + AIMAG(potden%vac(1,k,ivac,jsp)*sf2(k))*stars%nstr2(k)
+                     ENDDO
+                  END IF 
                    rcc=MATMUL(cell%amat,p(:,j))
-                   WRITE (oUnit,FMT=8020) (p(i,j),i=1,3),rcc,v1(j),v2(j)
-              ENDDO
+                   IF (l_dfpt) THEN 
+                     WRITE (oUnit,FMT=8025) (p(i,j),i=1,3),rcc, v1(j),v1Im(j),v2(j),v2Im(j)
+                   ELSE
+                     WRITE (oUnit,FMT=8020) (p(i,j),i=1,3),rcc, v1(j),v2(j)
+                   END IF
+             END DO
              CALL fitchk(v1(:np),v2(:np),av,rms,dms)
              WRITE (oUnit,FMT=8030) av,rms,dms
+             IF (l_dfpt) THEN
+               CALL fitchk(v1Im(:np),v2Im(:np),av,rms,dms)
+               WRITE (oUnit,*) "Imaginary Part:"
+               WRITE (oUnit,FMT=8030) av,rms,dms
+             END IF 
              RETURN
           ENDIF
           !      ----> interstitial part
           DO j = 1,np
              rcc=MATMUL(cell%bmat,p(:,j))/tpi_const
-
-             CALL starf3(sym%nop,stars%ng3,sym%symor,stars%kv3,sym%mrot,sym%tau,rcc,sym%invtab,sf3)!keep
+             IF (l_dfpt) THEN
+               CALL starf3(sym%nop,stars%ng3,sym%symor,stars%kv3,sym%mrot,sym%tau,rcc,sym%invtab,sf3,stars%center)
+             ELSE
+               CALL starf3(sym%nop,stars%ng3,sym%symor,stars%kv3,sym%mrot,sym%tau,rcc,sym%invtab,sf3)
+             END IF 
              !
              v1(j) = 0.0
+             IF (l_dfpt) v1Im(j) = 0.0 
              DO k = 1,stars%ng3
                 v1(j) = v1(j) + REAL(potden%pw(k,jsp)*sf3(k))*stars%nstr(k)
+                IF (l_dfpt) v1Im(j) = v1Im(j) + AIMAG(potden%pw(k,jsp)*sf3(k))*stars%nstr(k)
              ENDDO
           ENDDO
           !     ----> m.t. part
           IF (l_cdn) THEN
-             WRITE (oUnit,FMT=9010) n
+            IF (l_dfpt) THEN
+               WRITE (oUnit,FMT=9015) n
+             ELSE
+               WRITE (oUnit,FMT=9010) n
+             END IF 
           ELSE
-             WRITE (oUnit,FMT=8010) n
+             IF (l_dfpt) THEN
+               WRITE (oUnit,FMT=8015) n
+             ELSE
+               WRITE (oUnit,FMT=8010) n
+             END IF 
           ENDIF
           ir2 = 1.0
           IF (l_cdn) ir2 = 1.0 / ( atoms%rmt(n)*atoms%rmt(n) )
@@ -130,6 +183,7 @@
              ! new
              CALL ylm4(atoms%lmax(n),x,ylm)
              help = 0.0
+             helpIm= 0.0 
              DO lh = 0,sphhar%nlh(nd)
                 s = 0.0
                 ll1 = sphhar%llh(lh,nd) * ( sphhar%llh(lh,nd) + 1 ) + 1
@@ -138,28 +192,49 @@
                    s = s + REAL( sphhar%clnu(mem,lh,nd)* ylm(lm) )
                 ENDDO
                 help = help + potden%mt(atoms%jri(n),lh,n,jsp) * s
+                IF (l_dfpt) helpIm=helpIm + potdenIm%mt(atoms%jri(n),lh,n,jsp) * s
              ENDDO
              v2(j) = help * ir2
+             IF (l_dfpt) v2Im(j) = helpIm * ir2
              IF (j.LE.8) THEN
                 rcc=MATMUL(cell%bmat,p(:,j))/tpi_const
 
-                WRITE (oUnit,FMT=8020) rcc, (p(i,j),i=1,3),v1(j),v2(j)
+                IF (l_dfpt) THEN 
+                  WRITE (oUnit,FMT=8025) rcc, (p(i,j),i=1,3),v1(j),v1Im(j),v2(j),v2Im(j)
+                ELSE
+                  WRITE (oUnit,FMT=8020) rcc, (p(i,j),i=1,3),v1(j),v2(j)
+                END IF
              END IF
-          ENDDO
+         END DO 
           CALL fitchk(v1(:np),v2(:np),av,rms,dms)
           WRITE (oUnit,FMT=8030) av,rms,dms
+          IF (l_dfpt) THEN
+               CALL fitchk(v1Im(:np),v2Im(:np),av,rms,dms)
+               WRITE (oUnit,*) "Imaginary Part:"
+               WRITE (oUnit,FMT=8030) av,rms,dms
+          END IF 
 8000      FORMAT (/,'    int.-vac. boundary (potential): ivac=',i2,/,t10,&
                &       'int-coord',t36,'cart-coord',t57,' inter. ',t69,' vacuum ')
+8005      FORMAT (/,'    int.-vac. boundary (potential): ivac=',i2,/,t10,&
+               &       'int-coord',t36,'cart-coord',t64,' inter. ',t87,' vacuum ')
 8010      FORMAT (/,'    int.-m.t. boundary (potential): atom type=',i2,/,&
                &       t10,'int-coord',t36,'cart-coord',t57,' inter. ',t69,&
                &       '  m. t. ')
+8015      FORMAT (/,'    int.-m.t. boundary (potential): atom type=',i2,/,&
+               &       t10,'int-coord',t36,'cart-coord',t64,' inter. ',t87,&
+               &       '  m. t. ')
 8020      FORMAT (1x,2 (3f8.3,2x),2f12.6)
-8030      FORMAT (/,10x,'average value = ',f10.6,/,t11,'rms,dmx=',2f7.3,&
+8025      FORMAT (1x,2 (3f8.3,2x),4f12.6) !;_dfpt
+8030      FORMAT (/,10x,'average value = ',f12.6,/,t11,'rms,dmx=',2f10.3,&
                &       ' per cent')
 9000      FORMAT (/,'    int.-vac. boundary (density): ivac=',i2,/,t10,&
                &       'int-coord',t36,'cart-coord',t57,' inter. ',t69,' vacuum ')
+9005      FORMAT (/,'    int.-vac. boundary (density): ivac=',i2,/,t10,&
+               &       'int-coord',t36,'cart-coord',t64,' inter. ',t87,' vacuum ')
 9010      FORMAT (/,'    int.-m.t. boundary (density): atom type=',i2,/,t10,&
                &       'int-coord',t36,'cart-coord',t57,' inter. ',t69,'  m. t. ')
+9015      FORMAT (/,'    int.-m.t. boundary (density): atom type=',i2,/,t10,&
+               &       'int-coord',t36,'cart-coord',t64,' inter. ',t87,'  m. t. ')
           RETURN
         END SUBROUTINE checkdop
       END MODULE m_checkdop
--- global/starf.f90
+++ global/starf.f90
@@ -11,7 +11,7 @@ MODULE m_starf
     IMPLICIT NONE
 
 CONTAINS
-    SUBROUTINE starf2(nop2, ng2, kv2, mrot, symor, tau, r, invtab, sf)
+    SUBROUTINE starf2(nop2, ng2, kv2, mrot, symor, tau, r, invtab, sf, center)
 
         ! Scalar arguments:
         INTEGER, INTENT(IN) :: nop2, ng2
@@ -23,6 +23,7 @@ CONTAINS
 
         REAL,    INTENT(IN)  :: r(3), tau(3, nop2)
         COMPLEX, INTENT(OUT) :: sf(ng2)
+        REAL, INTENT(IN), OPTIONAL :: center(3)
 
         ! Local scalars:
         INTEGER :: k, n
@@ -42,10 +43,14 @@ CONTAINS
             sf(k) = 0.0
 
             DO n = 1, nop2
-                arg = tpi_const * ( kr(1, n) * r(1) + kr(2, n) * r(2) )
+                IF (.NOT. PRESENT(center)) THEN
+                    arg = tpi_const * ( (kr(1, n))* r(1) + kr(2, n) * r(2) ) 
+                ELSE
+                    arg = tpi_const * ( (kr(1, n) + center(1) )* r(1) + (kr(2, n) + center(2)) * r(2) ) 
+                END IF 
                 ! Sum up e^{i * \vec{G}_{op}^T
                 !               \cdot (\vec{r} - \vec{\tau}_{op})}
-                sf(k) = sf(k) + ph(n) * cmplx( cos(arg), sin(arg) )
+                    sf(k) = sf(k) + ph(n) * cmplx( cos(arg), sin(arg) )
             END DO
 
             sf(k) = sf(k) / nop2
@@ -54,7 +59,7 @@ CONTAINS
 
     END SUBROUTINE starf2
 
-    SUBROUTINE starf3(nop,ng3,symor,kv3,mrot,tau,r,invtab,sf)
+    SUBROUTINE starf3(nop,ng3,symor,kv3,mrot,tau,r,invtab,sf,center)
 
         ! Scalar arguments:
         INTEGER, INTENT(IN) :: nop, ng3
@@ -66,7 +71,7 @@ CONTAINS
 
         REAL,    INTENT(IN)   :: tau(3, nop), r(3)
         COMPLEX, INTENT (OUT) :: sf(ng3)
-
+        REAL, INTENT(IN), OPTIONAL :: center(3)
         ! Local scalars:
         INTEGER :: k,n
         REAL    :: arg
@@ -82,10 +87,14 @@ CONTAINS
             sf(k) = 0.0
 
             DO n = 1, nop
-                arg = tpi_const * dot_product( real(kr(:, n)), r )
+                IF (PRESENT(center)) THEN
+                    arg = tpi_const * dot_product( real(kr(:, n)) + center , r )   !! if flags + center
+                ELSE
+                    arg = tpi_const * dot_product( real(kr(:, n)) , r ) 
+                END IF 
                 ! Sum up e^{i * \vec{G}_{op}^T
                 !               \cdot (\vec{r} - \vec{\tau}_{op})}
-                sf(k) = sf(k) + ph(n) * cmplx( cos(arg), sin(arg) )
+                sf(k) = sf(k) + ph(n) * cmplx( cos(arg), sin(arg) )   
             END DO
 
             sf(k) = sf(k) / nop
