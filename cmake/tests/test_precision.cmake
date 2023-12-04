4e8f2c2f772fde1cc5bec60ea6a6e42b4b670144
Daniel Wortmann <d.wortmann@fz-juelich.de>
2023-12-04 11:40:00 +0100

    cmake is not fortran

--- cmake/tests/test_precision.cmake
+++ cmake/tests/test_precision.cmake
@@ -22,7 +22,7 @@ endif()
 
 try_run(PRECISION_RUN_OK PRECISION_COMP_OK  ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/cmake/tests/test_precision.f90 COMPILE_DEFINITIONS ${FLEUR_PRECISION_OPTION} )
 
-if (PRECISION_RUN_OK) THEN
+if (PRECISION_RUN_OK) 
    if (${PRECISION_RUN_OK} LESS "1")
          Message("Double precision found")
    else()
