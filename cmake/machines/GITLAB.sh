1965322d669c12b0f24577ca840ae46aea9d4ab9
Daniel Wortmann <d.wortmann@fz-juelich.de>
2023-12-05 09:55:31 +0100

    Fix include dir for CI

--- cmake/machines/GITLAB.sh
+++ cmake/machines/GITLAB.sh
@@ -1,6 +1,6 @@
 #NOSHOW
 export FC=mpif90
-FLEUR_INCLUDEDIR="/opt/include;-I/usr/include"
+FLEUR_INCLUDEDIR="/opt/include /usr/include"
 #FLEUR_LIBRARIES="-L/opt/lib;-lxcf03;-lxc;-ldl;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi;-llapack;-lblas"
 #FLEUR_LIBRARIES="-lwannier;-L/opt/lib;-ldl;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lxml2;-lscalapack-openmpi;-lblacsF77init-openmpi;-lblacs-openmpi"
 FLEUR_LIBRARIES="-L/opt/lib;-ldl;-L/usr/lib;-L/usr/lib/x86_64-linux-gnu;-lscalapack-openmpi;-lfftw3"
