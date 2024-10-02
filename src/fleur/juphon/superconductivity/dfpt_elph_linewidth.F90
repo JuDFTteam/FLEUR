da19e75f4dbcd1d0d2f8f0585c154885972d7831
Thomas Bornhake <bornhake@iff1146.iff.kfa-juelich.de>
2024-10-02 17:00:35 +0200

    Add further k-Integration for elph linewidth. Now able to select between approximation and smearing technique vs tetra.

--- src/fleur/juphon/superconductivity/dfpt_elph_linewidth.F90
+++ src/fleur/juphon/superconductivity/dfpt_elph_linewidth.F90
@@ -36,36 +36,62 @@ CONTAINS
         INTEGER, INTENT(IN) :: nbasfcnq_min
         REAL, ALLOCATABLE, INTENT(OUT)    :: ph_linewidth(:)
 
-        INTEGER :: iNupr,nu, ispin , gridPoint , nk , nZero , iMode
-        REAL :: emin, emax , x ,xq
+        INTEGER :: iNupr,nu, ispin , gridPoint , nk , nZero , iMode , noccbd
+        REAL :: emin, emax , x ,xq , allowed
         REAL, ALLOCATABLE :: eGrid(:),linewidth(:,:)
         COMPLEX,ALLOCATABLE:: kInt_gmat(:,:,:,:) !(nu',nu,jsp,normal_mode)
 
         REAL , ALLOCATABLE :: gauss(:) 
-        REAL :: smearing  , intOut
+        REAL :: intOut
 
 
-
-        smearing = 1e-7
         gmat(:,:,:,:,:) = ABS(gmat(:,:,:,:,:))**2 
+        
+
+        SELECT CASE(fi%juphon%i_integration)
+
+            ! 1 = Full calculation of linewidth with one delta function via Gaussian Smearing 
+            ! 2 = Double delta approximation of linewidth calculated with Gaussian Smearing
+            ! 3 = Double delta approximation of linewidth with Tetra Method 
 
 
-        SELECT CASE(fi%input%bz_integration)
+            !   For the different integration methods we need different sizes of the gmat 
+            !   For the linewidth in its original formulation we need to multiply the fermi functions
+            !   In order to not get combinatinos of nu -> unocc , nu' --> unocc we have to set these terms 0
+            !   We always scatter from a ground state property into a perturbed state 
+            !   For the tetragonal method it could happen that at a tetra we have two occupied and two unoccupied at nu
+            !   In order to get the correct interpolation we also need the contribution at the unoccupied states from nu'
 
-            CASE(BZINT_METHOD_HIST,BZINT_METHOD_GAUSS)
+            CASE(1,2)
                 
-
+                ! Cut out all contributions coming from nu-> unoccuppied 
                 DO ispin = 1 , fi%input%jspins
                     DO nk = 1 , fi%kpts%nkpt
+                        noccbd  = COUNT(results%w_iks(:,nk,ispin)*2.0/fi%input%jspins>1.e-8)
                         DO nu = 1 , size(gmat,2)
-                            x = (results%eig(nu,nk,ispin)-results%ef)/fi%input%tkb
-                            DO iNupr = 1 , size(gmat,1)
-                                xq = (resultsq%eig(iNupr,nk,ispin)-results%ef)/fi%input%tkb
-                                gmat(iNupr,nu,nk,ispin,:) = gmat(iNupr,nu,nk,ispin,:)*(sfermi(x) - sfermi(xq))
-                            END DO ! iNupr 
+                            allowed = 1. 
+                            IF (nu .GT. noccbd) allowed = 0.  
+                            gmat(:,nu,nk,ispin,:) = allowed * gmat(:,nu,nk,ispin,:) 
                         END DO  ! nu 
                     END DO ! nk 
-                END DO 
+                END DO !ispin 
+
+                ! mutliply with fermi function 
+                IF (fi%juphon%i_integration == 1 ) THEN 
+                    DO ispin = 1 , fi%input%jspins
+                        DO nk = 1 , fi%kpts%nkpt
+                            noccbd  = COUNT(results%w_iks(:,nk,ispin)*2.0/fi%input%jspins>1.e-8)
+                            DO nu = 1 , noccbd  
+                                x = (results%eig(nu,nk,ispin)-results%ef)/fi%input%tkb
+                                DO iNupr = 1 , size(gmat,1)
+                                    xq = (resultsq%eig(iNupr,nk,ispin)-results%ef)/fi%input%tkb
+                                    gmat(iNupr,nu,nk,ispin,:) = gmat(iNupr,nu,nk,ispin,:)*(sfermi(x) - sfermi(xq))
+                                END DO ! iNupr 
+                            END DO  ! nu 
+                        END DO ! nk 
+                    END DO 
+                END IF 
+
 
                 eMin = - 4 * fi%input%tkb
                 eMax =   4 * fi%input%tkb
@@ -82,25 +108,36 @@ CONTAINS
                 DO gridPoint=1,fi%banddos%ndos_points
                     eGrid(gridPoint)=emin+(emax-emin)/(fi%banddos%ndos_points-1.0)*(gridPoint-1.0)
                     IF (ABS(eGrid(gridPoint)) .LT. 1e-8) nZero = gridPoint 
-                    gauss(gridPoint) = 1/SQRT(tpi_const*smearing ) *EXP( -eGrid(gridPoint)**2/(2*smearing))
+                    gauss(gridPoint) = 1/SQRT(tpi_const*fi%juphon%smearingGauss ) *EXP( -eGrid(gridPoint)**2/(2*fi%juphon%smearingGauss))
                 END DO
 
                 DO iMode = 1 , 3*fi%atoms%ntype
                     IF (eigenVals(iMode) .GE. 0.0 ) THEN 
+                        
                         ! If omega becomes negative the deltra distribution is never satisfied as IM(eig,eigq) = 0.0 
-                        CALL dos_bin_transport(fi%input%jspins,fi%kpts%wtkpt,eGrid,results%eig(:size(gmat,2),:,:) , resultsq%eig(:nbasfcnq_min,:,:), REAL(gmat(:,:,:,:,iMode)), linewidth, -SQRT(eigenVals(iMode)),spinDeg=.FALSE.)
-                        !CALL smooth(eGrid,linewidth(:,1),fi%banddos%sig_dos,size(eGrid))
+                        IF (fi%juphon%i_integration == 1) CALL dos_bin_transport(fi%input%jspins,fi%kpts%wtkpt,eGrid,results%eig(:size(gmat,2),:,:)  &
+                        &                                                      ,resultsq%eig(:nbasfcnq_min,:,:), REAL(gmat(:nbasfcnq_min,:,:,:,iMode)), linewidth, -SQRT(eigenVals(iMode)))
+                        
+                        IF (fi%juphon%i_integration ==2) CALL dos_bin_double(fi%input%jspins,fi%kpts%wtkpt,eGrid,results%eig(:size(gmat,2),:,:)  &
+                        &                                                      ,resultsq%eig(:nbasfcnq_min,:,:), REAL(gmat(:nbasfcnq_min,:,:,:,iMode)), linewidth, results%ef)
+                        
+                        
+                        
                         DO ispin = 1 , fi%input%jspins
                             CALL intgz0(gauss*linewidth(:,ispin), eGrid(2)-eGrid(1) , size(gauss) , intOut,.FALSE.)
-                            !ph_linewidth(iMode) =  tpi_const * SQRT(eigenVals(iMode))/fi%kpts%nkpt*REAL(linewidth(nZero,1))
-                            ph_linewidth(iMode) =  ph_linewidth(iMode) +  tpi_const/fi%input%jspins * SQRT(eigenVals(iMode))/fi%kpts%nkpt*intOut
+                            ! factor two for spin deg. is calculated in dos_bin 
+                            ph_linewidth(iMode) =  ph_linewidth(iMode) +  pi_const * SQRT(eigenVals(iMode))/fi%kpts%nkpt*intOut
                         END DO 
                     ELSE
-                        CALL juDFT_warn('linewidth: Eigenvalue imaginary --> Phonon linewidth set to zero')
+                        write(*,*) '-------------------------'
+                        write(*,*) 'linewidth: Eigenvalue imaginary --> Phonon linewidth set to zero'
+                        write(*,*) '-------------------------'
                     END IF 
                 END DO 
             
-            CASE(BZINT_METHOD_TETRA)
+
+
+            CASE(3)
 
                 CALL timestart("k-Integration el-ph")
                 CALL dfpt_tetra_int(fi,results,resultsq, results1, gmat,nbasfcnq_min,kInt_gmat)
@@ -127,16 +164,20 @@ CONTAINS
         IF ( iQ .EQ. fi%juPhon%startq ) THEN 
             open( 110, file="linewidth", status='replace', action='write', form='formatted')
             write(110,*) "q-Point", qpts%bk(:,iQ) 
+            write(*,*) '-------------------------'
             write(*,*) "Linewidth q-Point", qpts%bk(:,iQ) 
             write(110,*) ph_linewidth(:) 
             write(*,*) ph_linewidth(:) 
+            write(*,*) '-------------------------'
         
         ELSE
             open( 110, file="linewidth", status="old", position="append", action="write")
             write(110,*) "q-Point", qpts%bk(:,iQ) 
+            write(*,*) '-------------------------'
             write(*,*) "Linewidth q-Point", qpts%bk(:,iQ) 
             write(110,*) ph_linewidth(:) 
             write(*,*) ph_linewidth(:) 
+            write(*,*) '-------------------------'
         END IF 
 
                     
--- src/fleur/propcalc/dos/dos_bin.f90
+++ src/fleur/propcalc/dos/dos_bin.f90
@@ -35,10 +35,11 @@ CONTAINS
       END DO
    END SUBROUTINE dos_bin
 
-   SUBROUTINE dos_bin_transport(jspins,wtkpt,egrid,eig,eigq,matrix_element,g,energyShift,spinDeg)
+   SUBROUTINE dos_bin_transport(jspins,wtkpt,egrid,eig,eigq,matrix_element,g,energyShift)
       !! This subroutine generates the idos,
       !! Almost identical to upper subroutine
       !! However, we have have e1 and e2 
+      !! Evaluation of delta-distribution is $\delta(\epsilon_{\nu , k }  - shift - \epsilon_{\nu' , k+q })$
       IMPLICIT NONE 
 
       INTEGER, INTENT(IN)  :: jspins
@@ -46,19 +47,19 @@ CONTAINS
       REAL, INTENT(IN)     :: eig(:,:,:) , eigq(:,:,:) , matrix_element(:,:,:,:) !(nu',nu,kpts,jspin)
       REAL, INTENT(OUT)    :: g(:,:) 
       REAL, OPTIONAL, INTENT(IN) :: energyShift
-      LOGICAL , OPTIONAL, INTENT(IN) :: spinDeg
+      !LOGICAL , OPTIONAL, INTENT(IN) :: spinDeg
 
 
       REAL :: de, wk , emin, shift  , degen
       INTEGER :: jsp, nk , nu , iNupr , i 
-      LOGICAL :: l_deg
+      !LOGICAL :: l_deg
 
 
-      degen = 2.0
-      l_deg = .TRUE. 
+      !degen = 2.0
+      !l_deg = .TRUE. 
       
-      IF (PRESENT(spinDeg)) l_deg = spinDeg
-      IF(.NOT.l_deg) degen = 1.0 
+      !IF (PRESENT(spinDeg)) l_deg = spinDeg
+      !IF(.NOT.l_deg) degen = 1.0 
       
 
       de = abs(egrid(2)-egrid(1))
@@ -74,7 +75,7 @@ CONTAINS
                      ! make sure if we hit emin we are at the first bin
                      i = NINT((eig(nu,nk,jsp) -eigq(iNupr,nk,jsp) -shift - emin)/de) + 1
                      IF ( (i.LE.size(g,1)) .AND. (i.GE.1) ) THEN 
-                        g(i,jsp) = g(i,jsp) + wk*matrix_element(iNupr,nu,nk,jsp)* degen/jspins
+                        g(i,jsp) = g(i,jsp) + wk*matrix_element(iNupr,nu,nk,jsp)* 2.0/jspins
                      END IF 
                   END DO ! iNupr
                END DO ! nu 
@@ -83,4 +84,43 @@ CONTAINS
 
    END SUBROUTINE dos_bin_transport
 
+   SUBROUTINE dos_bin_double(jspins,wtkpt,egrid,eig,eigq,matrix_element,g,energyShift)
+      !! This subroutine calculates the idos 
+      !! Here we have to evalulation we have to take, one for k and one for k'
+      !! Only accept contribution if they belong in the same bin --> discuss with daniel again
+      IMPLICIT NONE 
+
+      INTEGER, INTENT(IN) :: jspins 
+      REAL,    INTENT(IN)  :: wtkpt(:),egrid(:)
+      REAL, INTENT(IN)     :: eig(:,:,:) , eigq(:,:,:) , matrix_element(:,:,:,:) !(nu',nu,kpts,jspin)
+      REAL, INTENT(OUT)    :: g(:,:)
+      REAL, OPTIONAL, INTENT(IN) :: energyShift
+
+
+      REAL :: de, wk , emin, shift  
+      INTEGER :: jsp, nk , nu , iNupr , i , j
+
+
+      de = abs(egrid(2)-egrid(1))
+      g = 0.0
+      shift = 0.0 
+      IF (PRESENT(energyShift)) shift=energyShift
+      emin = MINVAL(egrid)
+      DO jsp = 1, SIZE(matrix_element,4)
+         DO nk = 1 , size(matrix_element,3)
+            wk = wtkpt(nk) / de 
+               DO nu = 1 , SIZE(matrix_element,2)
+                  ! make sure if we hit emin we are at the first bin
+                  i = NINT((eig(nu,nk,jsp) -shift - emin)/de) + 1
+                  DO iNupr = 1 , size(matrix_element,1)
+                     j = NINT((eigq(iNupr,nk,jsp) -shift - emin)/de) + 1
+                     IF ( (i.LE.size(g,1)) .AND. (i.GE.1) .AND. (i .EQ. j) ) THEN 
+                        g(i,jsp) = g(i,jsp) + wk*matrix_element(iNupr,nu,nk,jsp)* 2.0/jspins
+                     END IF 
+                  END DO ! iNupr
+               END DO ! nu 
+         END DO  ! k 
+      END DO  ! jsp 
+
+   END SUBROUTINE dos_bin_double
 END MODULE m_dosbin
