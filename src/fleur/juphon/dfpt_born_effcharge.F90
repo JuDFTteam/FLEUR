fd38d912573c2b2630c9b2dc33df917422eb71eb
Friedrich Hanrath <hanrath@iff1835.iff.kfa-juelich.de>
2025-01-14 09:43:15 +0100

    Make files for Born-effective charge

---
+++ src/fleur/juphon/dfpt_born_effcharge.F90
@@ -0,0 +1,10 @@
+module m_dfpt_born_effcharge
+    implicit none 
+
+    contains 
+        subroutine born_effective_charge_integrals
+            write(*,*) 'Determine integrals here'
+
+        end subroutine
+        
+end module 
