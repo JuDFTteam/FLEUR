650f563332848d8f820620e8382c99c94f05da7e
Gregor Michalicek <g.michalicek@fz-juelich.de>
2025-04-17 16:55:40 +0200

    Move and adapt b_field call to make it compatible with bulk noco calculations

--- src/fleur/vgen/b_field.F90
+++ src/fleur/vgen/b_field.F90
@@ -22,8 +22,8 @@ CONTAINS
     TYPE(t_field),INTENT(IN)::field
     TYPE(t_potden),INTENT(INOUT)::vtot
     TYPE(t_nococonv):: nococonv
-    INTEGER :: n
-    COMPLEX :: bComplexTemp
+    INTEGER :: iType
+    COMPLEX :: bOffdiag
     REAL :: bExternal(4)
 
     
@@ -33,33 +33,35 @@ CONTAINS
     !IF (noco%l_noco) CALL judft_error("B-fields not implemented in noco case")
     
     !Interstitial
-    vTot%pw_w(:,1)=vTot%pw_w(:,1)-((field%b_field/2.0)*stars%ustep(:)*stars%nstr(:))
-    vTot%pw_w(:,2)=vTot%pw_w(:,2)+((field%b_field/2.0)*stars%ustep(:)*stars%nstr(:))
+    vTot%pw_w(:,1)=vTot%pw_w(:,1)-(field%b_field/2.0)*stars%ustep(:)
+    vTot%pw_w(:,2)=vTot%pw_w(:,2)+(field%b_field/2.0)*stars%ustep(:)
 
     !MT-spheres
-    DO n=1,atoms%ntype
+    DO iType = 1, atoms%ntype
+       bExternal = 0.0
+       bOffdiag = 0.0
        bExternal(1) = -field%b_field / 2
        bExternal(2) = field%b_field / 2
 
        IF (noco%l_noco) THEN
-          CALL nococonv%rotdenmat(nococonv%alph(n), nococonv%beta(n), bExternal(1), bExternal(2), bComplexTemp, .false.)
-          bExternal(3) = REAL(bComplexTemp)
-          bExternal(4) = AIMAG(bComplexTemp)
+          CALL nococonv%rotdenmat(nococonv%alph(iType), nococonv%beta(iType), bExternal(1), bExternal(2), bOffdiag, .FALSE.)
+          bExternal(3) = REAL(bOffdiag)
+          bExternal(4) = AIMAG(bOffdiag)
        END IF
 
-       vTot%mt(:atoms%jri(n),0,n,1) = vTot%mt(:atoms%jri(n),0,n,1) + sfp_const * (bExternal(1) * 2.0 - field%b_field_mt(n)) / 2.0
-       vTot%mt(:atoms%jri(n),0,n,2) = vTot%mt(:atoms%jri(n),0,n,2) + sfp_const * (bExternal(2) * 2.0 + field%b_field_mt(n)) / 2.0
+       vTot%mt(:atoms%jri(iType),0,iType,1) = vTot%mt(:atoms%jri(iType),0,iType,1) + sfp_const * (bExternal(1) * 2.0 - field%b_field_mt(iType)) / 2.0
+       vTot%mt(:atoms%jri(iType),0,iType,2) = vTot%mt(:atoms%jri(iType),0,iType,2) + sfp_const * (bExternal(2) * 2.0 + field%b_field_mt(iType)) / 2.0
 
        IF (noco%l_noco) THEN
-          vTot%mt(:atoms%jri(n),0,n,3) = vTot%mt(:atoms%jri(n),0,n,3) + sfp_const * (bExternal(3))
-          vTot%mt(:atoms%jri(n),0,n,4) = vTot%mt(:atoms%jri(n),0,n,4) + sfp_const * (bExternal(4))
+          vTot%mt(:atoms%jri(iType),0,iType,3) = vTot%mt(:atoms%jri(iType),0,iType,3) + sfp_const * bExternal(3)
+          vTot%mt(:atoms%jri(iType),0,iType,4) = vTot%mt(:atoms%jri(iType),0,iType,4) + sfp_const * bExternal(4)
        END IF
     ENDDO
 
     !Vacuum
     IF (input%film) THEN
-       vTot%vac(:,1,:,1)=vTot%vac(:,1,:,1)-field%b_field/2.
-       vTot%vac(:,1,:,2)=vTot%vac(:,1,:,2)+field%b_field/2.
+       vTot%vac(:,1,:,1)=vTot%vac(:,1,:,1)-field%b_field/2.0
+       vTot%vac(:,1,:,2)=vTot%vac(:,1,:,2)+field%b_field/2.0
     END IF
   END SUBROUTINE bfield
 END MODULE m_bfield
