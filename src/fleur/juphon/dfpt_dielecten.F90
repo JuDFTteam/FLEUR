4edb6372aae70d1ad0085598e7c60f9a9be68971
Friedrich Hanrath <hanrath@iff1835.iff.kfa-juelich.de>
2025-04-24 15:26:52 +0200

    missing rho

--- src/fleur/juphon/dfpt_dielecten.F90
+++ src/fleur/juphon/dfpt_dielecten.F90
@@ -13,18 +13,19 @@ module m_dfpt_dielecten
 
     contains
 
-        subroutine dfpt_dielecten_HF_int(fi,stars,starsq,sphhar,fmpi,denIn1,denIn1Im,results,results1,dieltensor_row,iDir_den,q_sign)
+        subroutine dfpt_dielecten_HF_int(fi,stars,starsq,sphhar,fmpi,denIn1,denIn1Im,results,results1,dieltensor_row,rho,iDir_den,q_sign)
 
 
             
             type(t_fleurinput), intent(in)     :: fi
             type(t_sphhar),    intent(in)      :: sphhar
             TYPE(t_stars),      INTENT(IN)     :: stars, starsq
-            type(t_potden), intent(in)         :: denIn1,denIn1Im
+            type(t_potden), intent(inout)         :: denIn1,denIn1Im
             type(t_results), intent(in)        :: results, results1
             TYPE(t_mpi),        intent(in)     :: fmpi
             complex, intent(inout)             :: dieltensor_row(:)
             integer, intent(in)                :: q_sign
+            type(t_potden), intent(inout)         :: rho
 
 
 
