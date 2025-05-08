526052f119a42b2ec998e0da9516dbf80775b9cd
Gregor Michalicek <g.michalicek@fz-juelich.de>
2025-05-08 15:55:03 +0200

    Circumvent intel internal compiler error in src/fleur/juphon/dfpt_vgen_finalize.F90

--- src/fleur/juphon/dfpt_vgen_finalize.F90
+++ src/fleur/juphon/dfpt_vgen_finalize.F90
@@ -42,7 +42,9 @@ CONTAINS
         COMPLEX, ALLOCATABLE :: v1full(:)
 
         iPhonon = 0
-        IF (juphon%l_phonon) iPhonon = 1
+        IF (juphon%l_phonon.EQ..TRUE.) THEN ! This strange formulation was chosen to circumvent an internal compiler error with some version of the Intel compiler.
+           iPhonon = 1
+        END IF
 
         ifft3 = 27*stars%mx1*stars%mx2*stars%mx3 !TODO: What if starsq/=stars in that regard?
 
