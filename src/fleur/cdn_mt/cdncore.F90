5b98eeba2409ad88387b81eb2b42bb964ca22b3b
Gregor Michalicek <g.michalicek@fz-juelich.de>
2025-06-06 19:05:41 +0200

    Eliminate rhoTemp in src/fleur/cdn_mt/cdncore.F90

--- src/fleur/cdn_mt/cdncore.F90
+++ src/fleur/cdn_mt/cdncore.F90
@@ -52,7 +52,6 @@ SUBROUTINE cdncore(fmpi ,input,vacuum,noco,nococonv,sym,&
    LOGICAL                          :: l_coreDenPresent
    LOGICAL                          :: l_useOtherCoreSolver
 
-   REAL                             :: rhoTemp(atoms%jmtd,0:sphhar%nlhd,atoms%ntype,input%jspins)
    REAL                             :: rh(atoms%msh,atoms%ntype,input%jspins)
    REAL                             :: qint(atoms%ntype,input%jspins)
    REAL                             :: tec(atoms%ntype,input%jspins)
@@ -116,20 +115,15 @@ SUBROUTINE cdncore(fmpi ,input,vacuum,noco,nococonv,sym,&
          END DO
       ELSE
          IF(PRESENT(EnergyDen)) call juDFT_error("Energyden not implemented for relativistic core calculations")
-         rhoTemp = outDen%mt
          DO iType = 1, atoms%ntype
             l_useOtherCoreSolver = .FALSE.
             CALL coredr(input,atoms,iType,seig, outDen%mt,sphhar,vr0,qint,rh,l_useOtherCoreSolver)
             results%seigc = results%seigc + seig
             IF (l_useOtherCoreSolver) THEN
                DO jspin = 1,input%jspins
-                  CALL cored(input,jspin,iType,atoms,rhoTemp,sphhar,l_CoreDenPresent,vr0(:,:,jspin),qint,rh,tec,seig,l_useOtherCoreSolver=l_useOtherCoreSolver)
+                  CALL cored(input,jspin,iType,atoms,outDen%mt,sphhar,l_CoreDenPresent,vr0(:,:,jspin),qint,rh,tec,seig,l_useOtherCoreSolver=l_useOtherCoreSolver)
                   results%seigc = results%seigc + seig
                END DO
-               IF (l_useOtherCoreSolver) THEN
-                  outDen%mt(1:atoms%jmtd,0:sphhar%nlhd,iType,1) = rhoTemp(1:atoms%jmtd,0:sphhar%nlhd,iType,1)
-                  outDen%mt(1:atoms%jmtd,0:sphhar%nlhd,iType,input%jspins) = rhoTemp(1:atoms%jmtd,0:sphhar%nlhd,iType,input%jspins)
-               END IF
             END IF
          END DO
       END IF
