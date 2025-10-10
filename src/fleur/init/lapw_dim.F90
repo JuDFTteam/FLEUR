5ed78cfc12b13e04d0980bd14553cd3eade83fe2
Friedrich Hanrath <hanrath@iff1835.iff.kfa-juelich.de>
2025-10-10 13:17:04 +0200

    introduce efield q's+ correct nbasfcn calculation

--- src/fleur/init/lapw_dim.F90
+++ src/fleur/init/lapw_dim.F90
@@ -6,137 +6,148 @@
 
 MODULE m_lapwdim
 CONTAINS
-  SUBROUTINE lapw_dim(kpts,cell,input,noco,nococonv,forcetheo,atoms,nbasfcn,juPhon)
-    !
-    !*********************************************************************
-    !     determines dimensions of the lapw basis set with |k+G|<rkmax.
-    !  Generalization of the old apws_dim routine
-    !*********************************************************************
-    USE m_boxdim
-    USE m_types_fleurinput
-    USE m_types_forcetheo_extended
-    IMPLICIT NONE
-    TYPE(t_kpts),INTENT(IN)      :: kpts
-    TYPE(t_cell),INTENT(IN)      :: cell
-    TYPE(t_input),INTENT(IN)     :: input
-    TYPE(t_nococonv),INTENT(IN)  :: nococonv
-    TYPE(t_noco),INTENT(IN)      :: noco
-     
-    CLASS(t_forcetheo),INTENT(IN):: forcetheo
-    TYPE(t_atoms),INTENT(IN)     :: atoms
-    INTEGER, INTENT(OUT)         :: nbasfcn
+   SUBROUTINE lapw_dim(kpts,cell,input,noco,nococonv,forcetheo,atoms,nbasfcn,juPhon)
+      !
+      !*********************************************************************
+      !     determines dimensions of the lapw basis set with |k+G|<rkmax.
+      !  Generalization of the old apws_dim routine
+      !*********************************************************************
+      USE m_boxdim
+      USE m_types_fleurinput
+      USE m_types_forcetheo_extended
+      IMPLICIT NONE
+      TYPE(t_kpts),INTENT(IN)      :: kpts
+      TYPE(t_cell),INTENT(IN)      :: cell
+      TYPE(t_input),INTENT(IN)     :: input
+      TYPE(t_nococonv),INTENT(IN)  :: nococonv
+      TYPE(t_noco),INTENT(IN)      :: noco
+      
+      CLASS(t_forcetheo),INTENT(IN):: forcetheo
+      TYPE(t_atoms),INTENT(IN)     :: atoms
+      INTEGER, INTENT(OUT)         :: nbasfcn
 
-    TYPE(t_juPhon), INTENT(IN)   :: juPhon
+      TYPE(t_juPhon), INTENT(IN)   :: juPhon
 
-    !local variable for init
-    INTEGER               :: nvd,nv2d,addx,addy,addz
-    TYPE(t_lapw) :: lapw
+      !local variable for init
+      INTEGER               :: nvd,nv2d,addx,addy,addz
+      TYPE(t_lapw) :: lapw
 
-    INTEGER j1,j2,j3,mk1,mk2,mk3,iofile,ksfft,q,nk,nv,nv2
-    INTEGER ispin,nvh(2),nv2h(2)
+      INTEGER j1,j2,j3,mk1,mk2,mk3,iofile,ksfft,q,nk,nv,nv2,qpho
+      INTEGER ispin,nvh(2),nv2h(2)
 
-    REAL arltv1,arltv2,arltv3,rkm,rk2,r2,s(3),gmaxp,qss(3)
-    REAL,ALLOCATABLE:: q_vectors(:,:)
-    REAL            :: bkpt(3)
-    ! ..
-    !
-    !------->          ABBREVIATIONS
-    !
+      REAL arltv1,arltv2,arltv3,rkm,rk2,r2,s(3),gmaxp,qss(3),q_dfpt(3)
+      REAL,ALLOCATABLE:: q_vectors(:,:),q_dfpt_vectors(:,:)
+      REAL            :: bkpt(3)
+      ! ..
+      !
+      !------->          ABBREVIATIONS
+      !
 
-    !   iofile      : device number for in and output
-    !   gmax        : cut-off wavevector for charge density
-    !   rkmax       : cut-off for |g+k|
-    !   gmaxp       : gmaxp = gmax/rkmax, ideal: gmaxp=2
-    !   arltv(i)    : length of reciprical lattice vector along
-    !                 direction (i)
-    !
-    !---> Determine rkmax box of size mk1, mk2, mk3,
-    !     for which |G(mk1,mk2,mk3) + (k1,k2,k3)| < rkmax
-    !
-    CALL boxdim(cell%bmat,arltv1,arltv2,arltv3)
+      !   iofile      : device number for in and output
+      !   gmax        : cut-off wavevector for charge density
+      !   rkmax       : cut-off for |g+k|
+      !   gmaxp       : gmaxp = gmax/rkmax, ideal: gmaxp=2
+      !   arltv(i)    : length of reciprical lattice vector along
+      !                 direction (i)
+      !
+      !---> Determine rkmax box of size mk1, mk2, mk3,
+      !     for which |G(mk1,mk2,mk3) + (k1,k2,k3)| < rkmax
+      !
+      CALL boxdim(cell%bmat,arltv1,arltv2,arltv3)
 
-    !     (add 1+1 due to integer rounding, strange k_vector in BZ)
-    mk1 = int(input%rkmax/arltv1) + 2
-    mk2 = int(input%rkmax/arltv2) + 2
-    mk3 = int(input%rkmax/arltv3) + 2
+      !     (add 1+1 due to integer rounding, strange k_vector in BZ)
+      mk1 = int(input%rkmax/arltv1) + 2
+      mk2 = int(input%rkmax/arltv2) + 2
+      mk3 = int(input%rkmax/arltv3) + 2
 
-    rkm = input%rkmax
-    rk2 = rkm*rkm
+      rkm = input%rkmax
+      rk2 = rkm*rkm
 
-    !Determine the q-vector(s) to use
-    IF (juPhon%l_dfpt) THEN
-       ALLOCATE(q_vectors(3,SIZE(juPhon%qvec,2)+1))
-       q_vectors = 0.0 ! with this we force the gamma point to be within the dim search 
-       q_vectors(:,:size(juPhon%qvec,2))=juPhon%qvec
-    ELSE
-       SELECT TYPE(forcetheo)
-       TYPE IS (t_forcetheo_ssdisp)
-          ALLOCATE(q_vectors(3,SIZE(forcetheo%qvec,2)))
-          q_vectors=forcetheo%qvec
-       TYPE IS (t_forcetheo_dmi)
-          ALLOCATE(q_vectors(3,SIZE(forcetheo%qvec,2)))
-          q_vectors=forcetheo%qvec
-       TYPE IS (t_forcetheo_jij)
-          ALLOCATE(q_vectors(3,SIZE(forcetheo%qvec,2)))
-          q_vectors=forcetheo%qvec
-       CLASS IS (t_forcetheo) ! DEFAULT
-          ALLOCATE(q_vectors(3,1))
-          q_vectors(:,1)=nococonv%qss
-       END SELECT
-    END IF
+      !Determine the q-vector(s) to use
+      IF (juPhon%l_dfpt) THEN
+         IF (juPhon%l_efield .OR. juPhon%l_borneffcharge) THEN
+            ALLOCATE(q_dfpt_vectors(3,SIZE(juPhon%qvec_efield,2)+1))
+            q_dfpt_vectors = 0.0
+            q_dfpt_vectors(:,:size(juPhon%qvec_efield,2))=juPhon%qvec_efield
+         ElSE
+            ALLOCATE(q_dfpt_vectors(3,SIZE(juPhon%qvec,2)+1))
+            q_dfpt_vectors = 0.0
+            q_dfpt_vectors(:,:size(juPhon%qvec,2))=juPhon%qvec
+         END IF
+         ALLOCATE(q_vectors(3,1))
+         q_vectors=0.0
+      ELSE
+         ALLOCATE(q_dfpt_vectors(3,1))
+         q_dfpt_vectors = 0.0
+         SELECT TYPE(forcetheo)
+         TYPE IS (t_forcetheo_ssdisp)
+            ALLOCATE(q_vectors(3,SIZE(forcetheo%qvec,2)))
+            q_vectors=forcetheo%qvec
+         TYPE IS (t_forcetheo_dmi)
+            ALLOCATE(q_vectors(3,SIZE(forcetheo%qvec,2)))
+            q_vectors=forcetheo%qvec
+         TYPE IS (t_forcetheo_jij)
+            ALLOCATE(q_vectors(3,SIZE(forcetheo%qvec,2)))
+            q_vectors=forcetheo%qvec
+         CLASS IS (t_forcetheo) ! DEFAULT
+            ALLOCATE(q_vectors(3,1))
+            q_vectors(:,1)=nococonv%qss
+         END SELECT
+      END IF
 
-    if (.not. juPhon%l_dfpt .AND. any(abs(nococonv%qss-q_vectors(:,1))>1E-4)) CALL judft_warn("q-vector for self-consistency should be first in list for force-theorem")
+      if (.not. juPhon%l_dfpt .AND. any(abs(nococonv%qss-q_vectors(:,1))>1E-4)) CALL judft_warn("q-vector for self-consistency should be first in list for force-theorem")
 
 
-    nvd = 0 ; nv2d = 0
-    DO q=1,SIZE(q_vectors,2)
-       qss=q_vectors(:,q)
-       DO nk=1,kpts%nkpt
-          bkpt=kpts%bk(:,nk)
-          !---> obtain vectors
-          !---> in a spin-spiral calculation different basis sets are used for
-          !---> the two spin directions, because the cutoff radius is defined
-          !---> by |G + k +/- qss/2| < rkmax.
-          DO ispin = 1,2
-             addX = abs(NINT((bkpt(1) + (2*ispin - 3)/2.0*qss(1))/arltv1))
-             addY = abs(NINT((bkpt(2) + (2*ispin - 3)/2.0*qss(2))/arltv2))
-             addZ = abs(NINT((bkpt(3) + (2*ispin - 3)/2.0*qss(3))/arltv2))
-             nv = 0
-             nv2 = 0
-             DO j1 = -mk1-addX,mk1+addX
-                s(1) = bkpt(1) + j1 + (2*ispin - 3)/2.0*qss(1)
-                DO j2 = -mk2-addY,mk2+addY
-                   s(2) = bkpt(2) + j2 + (2*ispin - 3)/2.0*qss(2)
-                   !--->          nv2 for films
-                   s(3) = 0.0
-                   !r2 = dotirp(s,s,cell%bbmat)
-                   r2 = dot_product(matmul(s,cell%bbmat),s)
-                   IF (r2.LE.rk2) nv2 = nv2 + 1
-                   DO j3 = -mk3-addz,mk3+addz
-                      s(3) = bkpt(3) + j3 + (2*ispin - 3)/2.0*qss(3)
-                      !r2 = dotirp(s,s,cell%bbmat)
-                      r2 = dot_product(matmul(s,cell%bbmat),s)
-                      IF (r2.LE.rk2) THEN
-                         nv = nv + 1
-                      END IF
-                   END DO
-                END DO
-             END DO
-        
-             nvh(ispin)  = nv
-             nv2h(ispin) = nv2
-          END DO
-          nvd=MAX(nvd,MAX(nvh(1),nvh(2)))
-          nv2d=MAX(nv2d,MAX(nv2h(1),nv2h(2)))
+      nvd = 0 ; nv2d = 0
+      DO qpho=1,SIZE(q_dfpt_vectors,2)
+         q_dfpt=q_dfpt_vectors(:,qpho)
+         DO q=1,SIZE(q_vectors,2)
+            qss=q_vectors(:,q)
+            DO nk=1,kpts%nkpt
+               bkpt=kpts%bk(:,nk)
+               !print*,"shape(bkpt)",shape(bkpt)
+               !---> obtain vectors
+               !---> in a spin-spiral calculation different basis sets are used for
+               !---> the two spin directions, because the cutoff radius is defined
+               !---> by |G + k +/- qss/2| < rkmax.
+               DO ispin = 1,2
+                  addX = abs(NINT((bkpt(1) + (2*ispin - 3)/2.0*qss(1)+q_dfpt(1))/arltv1))
+                  addY = abs(NINT((bkpt(2) + (2*ispin - 3)/2.0*qss(2)+q_dfpt(2))/arltv2))
+                  addZ = abs(NINT((bkpt(3) + (2*ispin - 3)/2.0*qss(3)+q_dfpt(3))/arltv2))
+                  nv = 0
+                  nv2 = 0
+                  DO j1 = -mk1-addX,mk1+addX
+                     s(1) = bkpt(1) + j1 + (2*ispin - 3)/2.0*qss(1) + q_dfpt(1)
+                     DO j2 = -mk2-addY,mk2+addY
+                        s(2) = bkpt(2) + j2 + (2*ispin - 3)/2.0*qss(2) + q_dfpt(2)
+                        !--->          nv2 for films
+                        s(3) = 0.0
+                        !r2 = dotirp(s,s,cell%bbmat)
+                        r2 = dot_product(matmul(s,cell%bbmat),s)
+                        IF (r2.LE.rk2) nv2 = nv2 + 1
+                        DO j3 = -mk3-addz,mk3+addz
+                           s(3) = bkpt(3) + j3 + (2*ispin - 3)/2.0*qss(3)+q_dfpt(3)
+                           !r2 = dotirp(s,s,cell%bbmat)
+                           r2 = dot_product(matmul(s,cell%bbmat),s)
+                           IF (r2.LE.rk2) THEN
+                              nv = nv + 1
+                           END IF
+                        END DO
+                     END DO
+                  END DO
+                  nvh(ispin)  = nv
+                  nv2h(ispin) = nv2
+               END DO
+               nvd=MAX(nvd,MAX(nvh(1),nvh(2)))
+               nv2d=MAX(nv2d,MAX(nv2h(1),nv2h(2)))
 
-       ENDDO !k-loop
-    ENDDO !q-loop
-
-    nbasfcn = nvd + atoms%nat*atoms%nlod*(2*atoms%llod+1)
-    IF (noco%l_noco) nbasfcn = 2*nbasfcn
-    call lapw%init_dim(nvd,nv2d,nbasfcn)
-
-  END SUBROUTINE lapw_dim
+            ENDDO !k-loop
+         ENDDO !q-loop
+      ENDDO !qpho-loop
+      nbasfcn = nvd + atoms%nat*atoms%nlod*(2*atoms%llod+1)
+      IF (noco%l_noco) nbasfcn = 2*nbasfcn
+      call lapw%init_dim(nvd,nv2d,nbasfcn)
+   END SUBROUTINE lapw_dim
 
   
 END MODULE m_lapwdim
