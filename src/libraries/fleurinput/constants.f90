e38a6f7f005d81fd663244d732854b71facfd4cd
Daniel Wortmann <d.wortmann@fz-juelich.de>
2025-10-31 11:08:15 +0100

    avoid multiply in data statements

--- src/libraries/fleurinput/constants.f90
+++ src/libraries/fleurinput/constants.f90
@@ -155,12 +155,13 @@ MODULE m_constants
   integer, parameter, dimension(3, 3) :: id3x3   = reshape([dirvecx, dirvecy, dirvecz], [3, 3])
 
   real,private,parameter:: sqrt32=sqrt(3.0/2.0)
+  real,private,parameter:: nsqrt32=-1.*sqrt(3.0/2.0)
   complex, dimension(3,3,5) :: mat2ord  !initialization below due to problems with nvhpc
   
   DATA mat2ord / &
        ! Tensor 1
        (sqrt32, 0.0),  (0.0, sqrt32),  (0.0, 0.0), &
-       (0.0, sqrt32),  (-1.*sqrt32, 0.0), (0.0, 0.0), &
+       (0.0, sqrt32),  (nsqrt32, 0.0), (0.0, 0.0), &
        (0.0, 0.0),            (0.0, 0.0),            (0.0, 0.0), &
        ! Tensor 2
        (0.0, 0.0),            (0.0, 0.0),            (sqrt32, 0.0), &
@@ -171,12 +172,12 @@ MODULE m_constants
        (0.0, 0.0),            (-1.0, 0.0),           (0.0, 0.0), &
        (0.0, 0.0),            (0.0, 0.0),            (2.0, 0.0), &
        ! Tensor 4
-       (0.0, 0.0),            (0.0, 0.0),            (-1.*sqrt32, 0.0), &
+       (0.0, 0.0),            (0.0, 0.0),            (nsqrt32, 0.0), &
        (0.0, 0.0),            (0.0, 0.0),            (0.0, sqrt32), &
-       (-1.*sqrt32, 0.0), (0.0, sqrt32),  (0.0, 0.0), &
+       (nsqrt32, 0.0), (0.0, sqrt32),  (0.0, 0.0), &
        ! Tensor 5
-       (sqrt32, 0.0),  (0.0, -1.*sqrt32), (0.0, 0.0), &
-       (0.0, -1.*sqrt32), (-1.*sqrt32, 0.0), (0.0, 0.0), &
+       (sqrt32, 0.0),  (0.0, nsqrt32), (0.0, 0.0), &
+       (0.0, nsqrt32), (nsqrt32, 0.0), (0.0, 0.0), &
        (0.0, 0.0),            (0.0, 0.0),            (0.0, 0.0) /
                                                   
 CONTAINS
