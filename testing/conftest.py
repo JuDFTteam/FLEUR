8c5febb499ffeceddf2c541dd8107aa501b123a6
Gregor Michalicek <g.michalicek@fz-juelich.de>
2025-11-28 16:14:49 +0100

    Increase total energy test sensitivity
    Some physics takes place on a very fine energy scale. Our tests should be sensitive to that scale.

--- testing/conftest.py
+++ testing/conftest.py
@@ -1110,7 +1110,7 @@ def check_all_outxml(test_logger,check_outxml):
         checks=[
             ["FermiEnergy","value",-1,0.0001,None],
             ["bandgap","value",-1,0.001,None],
-            ["totalEnergy","value",-1,0.0001,None],
+            ["totalEnergy","value",-1,0.00003,None],
             ["sumValenceSingleParticleEnergies","value",-1,0.0001,None],
             ["chargeDensity","distance",-1,0.001,None], #last spind only
             ["mtCharge","total",-1,0.001,None],
