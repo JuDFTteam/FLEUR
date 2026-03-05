e33a60f58912b2230026d18890c2902eabb5d7a5
Friedrich Hanrath <hanrath@iff1835.iff.kfa-juelich.de>
2026-03-05 16:25:04 +0100

    cleaning LO-TO files

--- src/fleur/juphon/dfpt_NAC.F90
+++ src/fleur/juphon/dfpt_NAC.F90
@@ -14,53 +14,29 @@ module m_dfpt_NAC
 
 contains
 
-    subroutine dfpt_NAC(fi,iDtype,iDir_den,dyn_mat_NAC_row)
+    subroutine dfpt_NAC(fi,dyn_mat_NAC_row)
 
     type(t_fleurinput), intent(in)     :: fi
-    integer, intent(in)                :: iDtype,iDir_den 
     complex, intent(inout)             :: dyn_mat_NAC_row(:,:)
-    real                    :: borneffcharge_dumb(2,3,3),dielten(3,3),dielten_dumb(3,3),tempval_denom,tempval_BEC_fixed,tempval_NAC
+    real                    :: dielten(3,3),tempval_denom,tempval_BEC_fixed,tempval_NAC
     real, allocatable       :: borneffcharge(:,:,:)
     integer                 :: qnorm_vec(3), iDir,iDir1,iType,iType_row,iDir_row
 
-    borneffcharge_dumb(:,:,:) =0.0
-    dielten_dumb(:,:) =0.0
-    !get data for SiC:
-    do iDir=1,3
-        dielten_dumb(iDir,iDir) = 7.053
-        borneffcharge_dumb(1,iDir,iDir)=2.738
-        borneffcharge_dumb(2,iDir,iDir)=-2.738
-    end do
     allocate(borneffcharge(fi%atoms%ntype,3,3))
-    !print*,"dielten",dielten
-    !print*,"borneffcharge1",borneffcharge(1,:,:)
-    !print*,"borneffcharge2",borneffcharge(2,:,:)
+
     call read_BEC(fi,borneffcharge)
-    !borneffcharge= borneffcharge_dumb
+        
     qnorm_vec(:) = [1,0,0]
     tempval_denom = 0.0
-    !print*,"diff", 7.053*borneffcharge-borneffcharge_dumb
+
     call read_dielten(fi,dielten)
-    !call correct_BEC(dielten,borneffcharge)
 
-    !print*,"borneffcharge1",borneffcharge(1,:,:)
-    !print*,"borneffcharge2",borneffcharge(2,:,:)
-    !print*,"dielten",dielten(:,:)
-    !stop
-
-    !dielten_dumb = dielten
-    !print*,"diff", dielten-dielten_dumb
-
-
-    !stop
     do iDir=1,3
         do iDir1=1,3
             tempval_denom = tempval_denom +qnorm_vec(iDir)*dielten(iDir,iDir1)*qnorm_vec(iDir1)
-            !Isn't this actually completely redundant?
         end do
     end do
-    !print*,"tempval_denom",tempval_denom
-    !tempval_BEC_fixed = sum(borneffcharge(iDtype,iDir_den,:)*qnorm_vec(:))
+
     do itype=1,fi%atoms%ntype
         do iDir=1,3
             do iType_row=1,fi%atoms%ntype
@@ -72,9 +48,7 @@ contains
             end do
         end do
     end do
-    !dyn_mat_NAC_row(:) =0.0
 
-    !stop
 
 
     end subroutine dfpt_NAC
@@ -90,17 +64,14 @@ contains
         open(111, file="born_eff_charge", status='old', action='read', form='formatted')
 
         do iDType = 1, fi%atoms%ntype
-            ! Skip header line completely
             read(111,'(A)') dummy_line
-            ! Read 3x3 block
+
             do iDir = 1, 3
                 read(111,*) borneffcharge(iDType, iDir, 1:3)
-                !print*,borneffcharge(iDType, iDir, 1:3)
             end do
         end do
 
         close(111)
-        !print*,"borneffcharge",borneffcharge(1,:,:)
     
     end subroutine read_BEC
 
@@ -119,29 +90,8 @@ contains
         end do
 
         close(110)
-        !print*,"dielten",dielten(:,:)
     
     end subroutine read_dielten
 
-    subroutine correct_BEC(dielten,borneffcharge)
-
-        !type(t_fleurinput), intent(in)     :: fi
-        real, intent(in)   :: dielten(:,:)
-        real, intent(inout)  :: borneffcharge(:,:,:)
-        integer                            :: iDir
-
-        !dielten(:,:)=0.0
-        !open(110, file="diel_tensor", status='old', action='read', form='formatted')
-
-        do iDir = 1, 3
-            borneffcharge(:,:,iDir) = borneffcharge(:,:,iDir)*dielten(iDir,iDir) !last factor technically not correct --> discuss this
-            !read(110,*) dielten( iDir, 1:3)
-            !print*,dielten( iDir, 1:3)
-        end do
-
-        !close(110)
-        !print*,"dielten",dielten(:,:)
-    
-    end subroutine correct_BEC
 
 end module 
