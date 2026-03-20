a5692c1846ac442f12dbfe0ae6e6d73e06b75682
Thomas Bornhake <bornhake@iff1146.iff.kfa-juelich.de>
2026-03-20 12:05:26 +0100

    Next Refactor Idea. Introduce Abstract Class.
    This is the first commit of multiple. This is an idea to refactor the different dfpt subroutine to have a more modular code. The specific perturbation is given by the child that extends the abstract dfpt class.
    This code has not been compiled yet and only acts as a guideline.

---
+++ src/libraries/fleurinput/types_phonon.F90
@@ -0,0 +1,233 @@
+!--------------------------------------------------------------------------------
+! Copyright (c) 2026 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! This file is part of FLEUR and available as free software under the conditions
+! of the MIT license as expressed in the LICENSE file in more detail.
+!--------------------------------------------------------------------------------
+
+
+module m_types_phonon
+
+    use m_juDFT
+    use m_types_dfpt
+
+    implicit none 
+
+
+       
+    private
+    public :: t_phonon
+
+    type, extends(t_dfpt) :: t_phonon
+        private
+        complex , allocatable :: dynMat(:,:,:) ! (q-Point , 3*natoms, 3*natoms)
+        complex, allocatable  :: Eii2(:,:) ! q = 0 of ion-ion interaction (3*natoms, 3*natoms)
+
+    contains
+        procedure :: set_Eii2, get_Eii2
+        procedure :: set_dynMat, get_dynMat
+
+        ! maybe add an init that allocates the private variables --> should do
+        procedure :: init_child => init_child_phonon 
+        procedure :: q_indepent_properties => q_indepent_properties_phonon
+        procedure :: postprocessing_scf => postprocessing_scf_phonon
+        procedure :: postprocessing_qpoint => postprocessing_qpoint_phonon
+
+    end type t_phonon
+
+
+    contains
+
+    subroutine set_dynMat(this,inDynMat)
+
+        type(t_phonon), intent(inout) :: this
+        complex, intent(in)           :: inDynMat(:,:,:)
+        ! maybe add a check if sizes match --> after init the size should be relatively fixxed
+        this%dynMat(:,:,:) = inDynMat(:,:,:)
+    end subroutine set_dynMat
+
+
+    subroutine get_dynMat(this,outDynMat)
+        type(t_phonon), intent(in) :: this
+        complex, allocatable, intent(out) :: outDynmat(:,:,:)
+
+        if (allocated(outDynmat)) deallocate(outDynmat)
+        allocate(outDynmat,mold=this%dynMat)
+
+        outDynmat = this%dynMat        
+    end subroutine get_dynMat
+
+
+    subroutine set_Eii2(this,inEii2)
+        type(t_phonon), intent(inout) :: this
+        complex, intent(in)           :: inEii2(:,:)
+        ! maybe add a check if sizes match --> after init the size should be relatively fixxed
+        this%Eii2(:,:) = inEii2(:,:)
+    end subroutine set_Eii2
+
+    
+    subroutine get_Eii2(this,outEii2)
+        type(t_phonon), intent(in) :: this
+        complex, allocatable, intent(out) :: outEii2(:,:)
+
+        if (allocated(outEii2)) deallocate(outEii2)
+        allocate(outEii2,mold=this%Eii2)
+
+        outEii2 = this%Eii2        
+    end subroutine get_Eii2
+
+    subroutine init_child_phonon(this,fi,nqpts)
+        use m_types
+        class(t_phonon), intent(inout) :: this
+        type(t_fleurinput), intent(in) :: fi 
+        integer, intent(in)  :: nqpts
+        
+
+        allocate(this%dynMat(nqpts,3*fi%atoms%nat,3*fi%atoms%nat))
+        allocate(this%Eii2(3*fi%atoms%nat,3*fi%atoms%nat))
+
+        this%dynMat = cmplx(0.0,0.0)
+        this%Eii2 = cmplx(0.0,0.0)
+
+    end subroutine init_child_phonon
+
+
+
+    subroutine q_indepent_properties_phonon(this,fi,fmpi,sphhar,hybdat,xcpot,nococonv,stars,rho,vTot,grRho3,grVtot3,grVC3,grVext3,grgrVext3x3)
+        
+        use m_types
+    
+        class(t_phonon), intent(inout) :: this
+        type(t_fleurinput), intent(in)  :: fi 
+        type(t_mpi), intent(in)         :: fmpi
+        type(t_stars),intent(in)      :: stars
+        type(t_sphhar),intent(in)     :: sphhar
+        class(t_xcpot),intent(in)     :: xcpot
+        type(t_nococonv),intent(in)   :: nococonv
+        type(t_hybdat),intent(inout)     :: hybdat
+        type(t_potden),intent(in)     :: rho 
+        type(t_potden),intent(in)     :: vTot
+        type(t_potden), intent(in) :: grRho3(3), grVtot3(3), grVC3(3), grVext3(3),grgrVext3x3(3,3)
+
+        ! local variables 
+        type(t_potden) :: potdummy
+        complex :: e2_vm(fi%atoms%nat,3,3)
+        complex :: E2ndOrdII(3*fi%atoms%nat,fi%atoms%nat) 
+        integer :: iDir, iDir2, iDtype
+
+
+        E2ndOrdII = cmplx(0.0,0.0)
+        e2_vm = cmplx(0.0,0.0)
+
+        
+        call potdummy%copyPotDen(rho)
+        call potdummy%resetPotDen()
+
+        call timestart("Eii2 q=0")
+        do iDir = 1, 3 
+            do iDir2 = 1, 3 
+                call dfpt_e2_madelung(fi%atoms,fi%input%jspins,imagrhodummy%mt(:,0,:,:),grgrVext3x3(iDir2,iDir)%mt(:,0,:,1),e2_vm(:,iDir2,iDir))
+            end do !iDir2
+        end do !iDir 
+        call timestop("Eii2 q=0")
+
+        ! map the q=0 part of Eii(2) to the corresponding element in the dynMat shape 
+        do iDtype = 1, fi%atoms%ntype
+            do iDir2 = 1, 3
+                do iDir = 1, 3
+                    E2ndOrdII(3*(iDtype-1)+iDir2,3*(iDtype-1)+iDir) = e2_vm(iDtype,iDir2,iDir)
+                end do 
+            end do 
+        end do 
+
+        ! write to type 
+        call this%set_Eii2(E2ndOrdII)
+    
+    end subroutine q_indepent_properties_phonon
+
+
+    subroutine postprocessing_scf_phonon(this,fi,stars,starsq,sphhar,xcpot,nococonv,hybdat,fmpi,qpts,q_list,iQ,iDtype,iDir,eig_id,dfpt_eig_id, &
+                                          dfpt_eig_id2,enpara,results,results1,l_real,rho,vTot,grRho3,grVext3,grVc3,den1,vTot1,den1Im,vTot1Im,vC1)
+        
+        
+        use m_types
+        use m_dfpt_dynmat
+
+        class(t_phonon),intent(inout) :: this
+        type(t_fleurinput), intent(in)  :: fi 
+        type(t_stars),intent(in)      :: stars
+        type(t_stars),intent(in)      :: starsq
+        type(t_sphhar),intent(in)     :: sphhar
+        class(t_xcpot),intent(in)     :: xcpot
+        type(t_nococonv),intent(in)   :: nococonv
+        type(t_hybdat),intent(inout)     :: hybdat
+        type(t_mpi), intent(in)         :: fmpi
+        type(t_kpts), intent(in)       :: qpts
+        integer, allocatable,intent(in) :: q_list(:)
+        integer, intent(in)            :: iQ,iDtype,iDir,eig_id,dfpt_eig_id,dfpt_eig_id2
+        type(t_enpara),intent(inout)     :: enpara
+        type(t_results),intent(inout)    :: results, results1
+        logical,intent(in)               :: l_real
+        type(t_potden),intent(in)        :: rho,vTot,grRho3(3),grVext3,grVC3,den1,vTot1,den1Im,vTot1Im,vC1
+
+        
+        complex,allocatable :: E2ndOrdII(:,:)
+        complex,allocatable :: dyn_mat(:,:,:)
+
+        
+        call this%get_Eii2(E2ndOrdII)
+        call this%get_dynMat(dyn_mat)
+
+        call timestart("dynmat")
+        call dfpt_dynmat_row(fi, stars, starsq, sphhar, xcpot, nococonv, hybdat, fmpi, qpts, q_list(iQ), iDtype, iDir, &
+                                eig_id, dfpt_eig_id, dfpt_eig_id2, enpara, results, results1, l_real,&
+                                rho, vTot, grRho3, grVext3, grVC3, &
+                                den1, vTot1, den1Im, vTot1Im, vC1, vC1Im, dyn_mat(iQ,3 *(iDtype-1)+iDir,:), E2ndOrdII)
+
+        call timestop("Dynmat")
+        
+        dyn_mat(iQ,3 *(iDtype-1)+iDir,:) = dyn_mat(iQ,3 *(iDtype-1)+iDir,:) + conjg(E2ndOrdII(3 *(iDtype-1)+iDir,:))
+
+        call this%set_dynMat(dyn_mat)
+
+
+    end subroutine postprocessing_scf_phonon
+
+    subroutine postprocessing_qpoint_phonon(this,fi,fmpi,qpts,iQ)
+
+        use m_types
+        use m_dfpt_dynmat_eig
+
+        class(t_phonon), intent(inout) :: this
+        type(t_fleurinput),intent(in)  :: fi 
+        type(t_mpi),intent(in)         :: fmpi
+        type(t_kpts),intent(in)        :: qpts
+        integer,intent(in)             :: iQ
+
+        complex,allocatable :: dyn_mat(:,:,:)
+
+        real, allocatable    :: eigenVals(:)
+        complex, allocatable :: eigenFreqs(:), eigenVecs(:,:),dyn_mat_NAC(:,:)
+
+
+        call this%get_dynMat(dyn_mat)
+
+        if (fmpi%irank==0) then 
+
+            ! you have to add the NAC stuff here 
+
+            call timestart("Dynmat diagonalization")
+            call DiagonalizeDynMat(fi%atoms, qpts%bk(:,q_list(iQ)), fi%juPhon%calcEigenVec, dyn_mat(iQ,:,:), eigenVals, eigenVecs, q_list(iQ),.TRUE.,"raw",fi%juphon%l_sumrule)
+            call timestop("Dynmat diagonalization")
+
+            call timestart("Frequency calculation")
+            call CalculateFrequencies(fi%atoms, q_list(iQ), eigenVals, eigenFreqs,"raw",qpts%bk(:,q_list(iQ)))
+            call timestop("Frequency calculation")
+        end if 
+
+    end subroutine postprocessing_qpoint_phonon
+
+
+
+
+
+end module m_types_phonon
