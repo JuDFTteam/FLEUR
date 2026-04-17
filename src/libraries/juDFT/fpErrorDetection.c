82776c4a2c72a131b66611b11c69e942cbcd9ea6
Daniel Wortmann <d.wortmann@fz-juelich.de>
2026-04-17 16:47:43 +0200

    Refactor floating-point error detection: enhance macOS support and update conditional compilation for clarity

--- src/libraries/juDFT/fpErrorDetection.c
+++ src/libraries/juDFT/fpErrorDetection.c
@@ -1,7 +1,33 @@
-#ifdef CPP_DEBUG // This is commented out because at the moment this symbol is not defined for the compilation of c files.
 
 #include <fenv.h>
 
+#if defined(__APPLE__)
+/* macOS does not provide feenableexcept/fedisableexcept (Linux/glibc extensions). */
+#if defined(__x86_64__) || defined(__i386__)
+#include <xmmintrin.h>
+
+int startFPErrorDetection()
+{
+   _MM_SET_EXCEPTION_MASK(_MM_GET_EXCEPTION_MASK() &
+      ~(_MM_MASK_INVALID | _MM_MASK_DIV_ZERO | _MM_MASK_OVERFLOW));
+   return 0;
+}
+
+int stopFPErrorDetection()
+{
+   _MM_SET_EXCEPTION_MASK(_MM_GET_EXCEPTION_MASK() |
+      (_MM_MASK_INVALID | _MM_MASK_DIV_ZERO | _MM_MASK_OVERFLOW));
+   return 0;
+}
+
+#else
+/* ARM64 macOS: fenv exception trapping not reliably supported */
+int startFPErrorDetection() { return -1; }
+int stopFPErrorDetection()  { return -1; }
+#endif
+
+#else
+/* Linux and other platforms with glibc */
 int startFPErrorDetection()
 {
    return feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW);
@@ -11,5 +37,5 @@ int stopFPErrorDetection()
 {
    return fedisableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW);
 }
-
 #endif
+
--- src/libraries/juDFT/sysinfo.F90
+++ src/libraries/juDFT/sysinfo.F90
@@ -1,5 +1,5 @@
 !--------------------------------------------------------------------------------
-! Copyright (c) 2025 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
+! Copyright (c) 2026 Peter Grünberg Institut, Forschungszentrum Jülich, Germany
 ! This file is part of FLEUR and available as free software under the conditions
 ! of the MIT license as expressed in the LICENSE file in more detail.
 !--------------------------------------------------------------------------------
@@ -20,7 +20,7 @@ CONTAINS
 subroutine fp_error_check(onoff)
    LOGICAL,INTENT(IN) :: onoff
    Integer::errorStatus
-#ifdef CPP_DEBUG
+#if defined(CPP_DEBUG)
       interface
          function startFPErrorDetection() bind(C, name="startFPErrorDetection")
             use iso_c_binding
