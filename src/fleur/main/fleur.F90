cf970cb8ed8c5dd58aa3dacc8d144b7c2f221e5f
Thomas Bornhake <bornhake@iff1146.iff.kfa-juelich.de>
2026-07-01 10:29:17 +0200

    Fix diagonalization error in wannier_h_interpolate
    Use the solver type to fix diag problems. Previous implementation used single precession diag.

--- src/fleur/main/fleur.F90
+++ src/fleur/main/fleur.F90
@@ -491,7 +491,10 @@ CONTAINS
                CALL timestop("wannierlib")
                ! lets do a test interpolation here 
                ! sub in the hamiltinan routine
+               if (fmpi%irank==0) print *, "Starting interpolate bandstructure "
+               call timestart("Wannier Interpolation")
                if (fmpi%irank==0) call interpolate_bandstructure(fi,results , fi%wannierlib%kpts_fine)
+               call timestop("Wannier Interpolation")
                IF (.NOT. fi%dfpt%l_dfpt) CALL juDFT_end("Wannierization done. Fleur ends.", fmpi%irank)
             END IF
 
--- src/fleur/wannierlib/wannier_h_interpolate.F90
+++ src/fleur/wannierlib/wannier_h_interpolate.F90
@@ -9,11 +9,14 @@ module m_wannier_interpolate
     use m_types
     use m_constants
     use m_matrix_interpolation
-
+    use m_npy
     implicit none
 
 contains
     subroutine interpolate_bandstructure(fi, results, kpts_fine)
+
+        use m_available_solvers
+        use m_types_solver
 
         type(t_fleurinput), intent(in) :: fi
         type(t_results),    intent(in) :: results
@@ -25,12 +28,15 @@ contains
         complex, allocatable :: H_bloch(:,:,:,:)
         complex, allocatable :: H_interpol(:,:,:,:)
         complex, allocatable :: H_interpol_spin(:,:,:,:)
-        complex, allocatable :: hmat_tmp(:,:)
         complex, allocatable :: work(:)
         complex, allocatable :: U_full(:,:,:,:)
-        real :: kpath_coord, dk(3)
-        integer :: iout , nfine 
+        real :: kpath_coord, dk(3) , dk_cart(3)
+        integer :: iout , nfine , ioutEv
 
+        class(t_mat),allocatable :: hmat_tmp
+        class(t_mat),allocatable :: zMat_tmp
+        class(t_solver), allocatable   :: solver, transform 
+        logical :: l_real 
 
         if (.not. fi%wannierlib%l_wannierize) return
         if (.not. allocated(results%U_mat)) &
@@ -40,6 +46,8 @@ contains
         num_bands = fi%wannierlib%max_band - fi%wannierlib%min_band + 1
         num_wann  = fi%wannierlib%num_wann
         nfine = size(kpts_fine,2)
+
+        l_real = fi%sym%invs.AND.(.NOT.fi%noco%l_soc).AND.(.NOT.fi%noco%l_noco).AND.fi%atoms%n_hia==0
 
 
         if (num_bands /= num_wann .and. .not. allocated(results%U_dis)) &
@@ -79,38 +87,60 @@ contains
             else
                 U_full(:,:,:,1) = results%U_mat(:,:,:,jspin)
             end if
-
+            call timestart("Wannier Transformation")
             call wannier_matrix_interpolate(fi, H_bloch(:,:,:,jspin:jspin), &
                                             U_full,                          &
                                             fi%kpts, kpts_fine,              &
                                             H_interpol_spin)
+            call timestop("Wannier Transformation")
             H_interpol(:,:,:,jspin) = H_interpol_spin(:,:,:,1)
             deallocate(H_interpol_spin)
         end do
-
+        call save_npy("Hmat_interpolate.npy",H_interpol)
         deallocate(U_full)
 
         ! ---------------------------------------------------------------
         ! Step 3: diagonalize interpolated Hamiltonian at each fine k-point
         ! ---------------------------------------------------------------
-        lwork = max(1, 2*num_wann - 1)
-        allocate(work(lwork))
-        allocate(rwork(max(1, 3*num_wann - 2)))
-        allocate(hmat_tmp(num_wann, num_wann))
+      
         allocate(eig_interpol(num_wann, nfine, fi%input%jspins))
 
-        do jspin = 1, fi%input%jspins
-            do ikpt = 1, nfine 
-                hmat_tmp = H_interpol(:,:,ikpt,jspin)
-                call cheev('N', 'U', num_wann, hmat_tmp, num_wann, &
-                           eig_interpol(:,ikpt,jspin), work, lwork, rwork, info)
-                if (info /= 0) &
-                    call juDFT_error('cheev failed in interpolate_bandstructure', &
-                                     calledby='interpolate_bandstructure')
-            end do
-        end do
+        ! do jspin = 1, fi%input%jspins
+        !     do ikpt = 1, nfine 
+        !         hmat_tmp = H_interpol(:,:,ikpt,jspin)
+        !         !hmat_tmp = 0.5 * (hmat_tmp + conjg(transpose(hmat_tmp)))
+                
+        !         call cheev('N', 'U', num_wann, hmat_tmp, num_wann, &
+        !                    eig_interpol(:,ikpt,jspin), work, lwork, rwork, info)
+        !         if (info /= 0) &
+        !             call juDFT_error('cheev failed in interpolate_bandstructure', &
+        !                              calledby='interpolate_bandstructure')
+        !     end do
+        ! end do
 
-        deallocate(H_interpol, hmat_tmp, work, rwork)
+        !call zMat_tmp%init(l_real,num_wann,num_wann)
+        allocate (t_mat::hmat_tmp)
+        call hmat_tmp%init(l_real,num_wann,num_wann)
+        call select_solver(.false.,diag_solver=solver,diag_transform=transform)
+
+        do jspin = 1 , fi%input%jspins
+            do ikpt  = 1 , nfine 
+                if (hmat_tmp%l_real) then 
+                    hmat_tmp%data_r = real(H_interpol(:,:,ikpt,jspin))    
+                else 
+                    hmat_tmp%data_c = H_interpol(:,:,ikpt,jspin)
+                end if 
+                call timestart("Diogonalization Wannier")
+                call solver%solve_std_dp(hmat_tmp,num_wann,eig_interpol(:,ikpt,jspin),zMat_tmp)
+                call timestop("Diogonalization Wannier")
+                if (allocated(zMat_tmp)) then
+                    call zMat_tmp%free()
+                    deallocate(zMat_tmp)
+                end if 
+            end do 
+        end do !jspins
+
+        !deallocate(H_interpol)
 
         ! ---------------------------------------------------------------
         ! Write interpolated eigenvalues to wann_bands.dat
@@ -119,19 +149,25 @@ contains
         open(newunit=iout, file='wann_bands.dat', status='replace')
         write(iout,'(a)') '# kpath_coord   eig_1  eig_2  ...'
 
+        open(newunit=ioutEv, file='wann_bands_ev.dat', status='replace')
+        write(ioutEV,'(a)') '# kpath_coord   eig_1  eig_2  ...'
+
         do jspin = 1, fi%input%jspins
             if (fi%input%jspins > 1) write(iout,'(a,i1)') '# spin ', jspin
             kpath_coord = 0.0
             do ikpt = 1, nfine 
                 if (ikpt > 1) then
                     dk = kpts_fine(:,ikpt) - kpts_fine(:,ikpt-1)
-                    kpath_coord = kpath_coord + sqrt(dot_product(dk, dk))
+                    dk_cart = matmul(fi%cell%bmat,dk)
+                    kpath_coord = kpath_coord + sqrt(dot_product(dk_cart, dk_cart))
                 end if
                 write(iout,'(f12.6,*(2x,f14.8))') kpath_coord, eig_interpol(:,ikpt,jspin)
+                write(ioutEv,'(f12.6,*(2x,f14.8))') kpath_coord, hartree_to_ev_const*eig_interpol(:,ikpt,jspin)
             end do
         end do
 
         close(iout)
+        close(ioutEV)
         deallocate(eig_interpol)
 
     end subroutine interpolate_bandstructure
